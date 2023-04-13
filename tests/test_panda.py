a43d46a1bcdb34c683b531a41646bc277ec713fc
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-04-13 16:08:38 +0100

    added tests which include a test_panda db file. For some reason the tests fail normally but pass if you debug them.

---
+++ tests/test_panda.py
@@ -0,0 +1,65 @@
+"""Test file specifying how we want to eventually interact with the panda..."""
+import bluesky.plan_stubs as bps
+import numpy as np
+import pytest
+from bluesky import RunEngine
+from ophyd.v2.core import DeviceCollector
+
+from ophyd_epics_devices.panda import PandA, SeqTable, SeqTrigger
+
+
+@pytest.fixture
+async def sim_panda():
+    async with DeviceCollector(sim=True):
+        sim_panda = PandA("PANDAQSRV")
+        # Signals connected here
+
+    assert sim_panda.name == "sim_panda"
+    # units = cast(ChannelSim, sim_motor.units.read_channel)
+    # units.set_value("mm")
+    # precision = cast(ChannelSim, sim_motor.precision.read_channel)
+    # precision.set_value(3)
+    # velocity = cast(ChannelSim, sim_motor.velocity.read_channel)
+    # velocity.set_value(1)
+    yield sim_panda
+
+
+def test_panda_names_correct(pva, sim_panda: PandA):
+    assert sim_panda.seq[1].name == "sim_panda-seq1"
+    assert sim_panda.pulse[1].name == "sim_panda-pulse1"
+
+
+async def test_panda_children_connected(pva, sim_panda: PandA):
+    # try to set and retrieve from simulated values...
+    table = SeqTable(
+        repeats=np.array([1, 1, 1, 32]).astype(np.uint16),
+        trigger=(
+            SeqTrigger.POSA_GT,
+            SeqTrigger.POSA_LT,
+            SeqTrigger.IMMEDIATE,
+            SeqTrigger.IMMEDIATE,
+        ),
+        position=np.array([3222, -565, 0, 0]).astype(np.int32),
+        time1=np.array([5, 0, 10, 10]).astype(np.uint32),
+        outa1=np.array([1, 0, 0, 1]).astype(np.bool_),
+        outb1=np.array([0, 0, 1, 1]).astype(np.bool_),
+        outc1=np.array([0, 1, 1, 0]).astype(np.bool_),
+        outd1=np.array([1, 1, 0, 1]).astype(np.bool_),
+        oute1=np.array([1, 0, 1, 0]).astype(np.bool_),
+        outf1=np.array([1, 0, 0, 0]).astype(np.bool_),
+        time2=np.array([0, 10, 10, 11]).astype(np.uint32),
+        outa2=np.array([1, 0, 0, 1]).astype(np.bool_),
+        outb2=np.array([0, 0, 1, 1]).astype(np.bool_),
+        outc2=np.array([0, 1, 1, 0]).astype(np.bool_),
+        outd2=np.array([1, 1, 0, 1]).astype(np.bool_),
+        oute2=np.array([1, 0, 1, 0]).astype(np.bool_),
+        outf2=np.array([1, 0, 0, 0]).astype(np.bool_),
+    )
+    await sim_panda.pulse[1].delay.set(20.0)
+    await sim_panda.seq[1].table.set(table)
+
+    readback_pulse = await sim_panda.pulse[1].delay.get_value()
+    readback_seq = await sim_panda.seq[1].table.get_value()
+
+    assert readback_pulse == 20.0
+    assert readback_seq == table
