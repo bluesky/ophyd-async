0122af4cc681e1543250dc9475519091c8c75f6f
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-04-14 11:06:28 +0100

    reverted back to old Dockerfile, replaced subprocess in tests with call to epicscorelibs.ioc instead

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import asyncio
 import subprocess
+import sys
 import time
+from pathlib import Path
+
+import pytest
+
+RECORD = str(Path(__file__).parent / "db" / "panda.db")
 
 import pytest
 from bluesky.run_engine import RunEngine, TransitionError
@@ -26,15 +32,37 @@ def RE(request):
     return RE
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module", params=["pva", "ca"])
 def pva():
     process = subprocess.Popen(
-        ["softIocPVA", "-d", "tests/db/panda.db"],
+        [
+            sys.executable,
+            "-m",
+            "epicscorelibs.ioc",
+            "-d",
+            RECORD,
+        ],
+        stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        universal_newlines=True,
     )
     time.sleep(2)
     assert not process.poll(), process.stdout.read().decode("utf-8")
     yield process
 
     process.terminate()
+
+
+# @pytest.fixture(scope="session")
+# def pva():
+#     process = subprocess.Popen(
+#         ["softIocPVA", "-d", "tests/db/panda.db"],
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.STDOUT,
+#     )
+#     time.sleep(2)
+#     assert not process.poll(), process.stdout.read().decode("utf-8")
+#     yield process
+
+#     process.terminate()
