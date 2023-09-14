141aae8a50318705e99975bdcff8e063ffd2058b
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-09-14 14:18:28 +0100

    Fix linting

--- tests/core/_device/test_device_collector.py
+++ tests/core/_device/test_device_collector.py
@@ -1,11 +1,14 @@
-from ophyd_async.core import Device, DeviceCollector
 import pytest
+
+from ophyd_async.core import Device, DeviceCollector
+
 
 class Dummy(Device):
     def connect(self, sim: bool = False):
         raise AttributeError()
 
+
 def test_device_collector_propagates_error(RE):
     with pytest.raises(AttributeError):
         with DeviceCollector():
-            dummy = Dummy("somename")
+            _ = Dummy("somename")
