8d207fa9c4e4bccda3210216e7e211f8543baa78
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-09-14 16:41:39 +0100

    Add children method to Device and DeviceVector

--- src/ophyd_async/core/_device/device.py
+++ src/ophyd_async/core/_device/device.py
@@ -1,6 +1,7 @@
 """Base device"""
+from __future__ import annotations
 
-from typing import Generator, Optional, Tuple
+from typing import Iterator, Optional, Tuple
 
 from bluesky.protocols import HasName
 
@@ -15,7 +16,7 @@ class Device(HasName):
 
     _name: str = ""
     #: The parent Device if it exists
-    parent: Optional["Device"] = None
+    parent: Optional[Device] = None
 
     def __init__(self, name: str = "") -> None:
         self.set_name(name)
@@ -24,6 +25,11 @@ class Device(HasName):
     def name(self) -> str:
         """Return the name of the Device"""
         return self._name
+    
+    def children(self) -> Iterator[Tuple[str, Device]]:
+        for attr_name, attr in self.__dict__.items():
+            if attr_name != "parent" and isinstance(attr, Device):
+                yield attr_name, attr
 
     def set_name(self, name: str):
         """Set ``self.name=name`` and each ``self.child.name=name+"-child"``.
@@ -34,7 +40,10 @@ class Device(HasName):
             New name to set
         """
         self._name = name
-        name_children(self, name)
+        for attr_name, child in self.children():
+            child_name = f"{name}-{attr_name.rstrip('_')}" if name else ""
+            child.set_name(child_name)
+            child.parent = self
 
     async def connect(self, sim: bool = False):
         """Connect self and all child Devices.
@@ -44,35 +53,8 @@ class Device(HasName):
         sim:
             If True then connect in simulation mode.
         """
-        await connect_children(self, sim)
-
-
-async def connect_children(device: Device, sim: bool):
-    """Call ``child.connect(sim)`` on all child devices in parallel.
-
-    Typically used to implement `Device.connect` like this::
-
-        async def connect(self, sim=False):
-            await connect_children(self, sim)
-    """
-
-    coros = {
-        name: child_device.connect(sim)
-        for name, child_device in get_device_children(device)
-    }
-    if coros:
-        await wait_for_connection(**coros)
-
-
-def name_children(device: Device, name: str):
-    """Call ``child.set_name(child_name)`` on all child devices in series."""
-    for attr_name, child in get_device_children(device):
-        child_name = f"{name}-{attr_name.rstrip('_')}" if name else ""
-        child.set_name(child_name)
-        child.parent = device
-
-
-def get_device_children(device: Device) -> Generator[Tuple[str, Device], None, None]:
-    for attr_name, attr in device.__dict__.items():
-        if attr_name != "parent" and isinstance(attr, Device):
-            yield attr_name, attr
+        coros = {
+            name: child_device.connect(sim) for name, child_device in self.children()
+        }
+        if coros:
+            await wait_for_connection(**coros)
