1742e4c72daf56cd1858dd10023735b68cad97d1
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2023-10-13 14:22:33 +0100

    remove pilatus detector

--- src/ophyd_async/epics/areadetector/__init__.py
+++ src/ophyd_async/epics/areadetector/__init__.py
@@ -1,10 +1,8 @@
 from .areadetector import AreaDetector
-from .pilatus import HDFStatsPilatus
 from .single_trigger_det import SingleTriggerDet
 from .utils import FileWriteMode, ImageMode, ad_r, ad_rw
 
 __all__ = [
-    "HDFStatsPilatus",
     "SingleTriggerDet",
     "FileWriteMode",
     "ImageMode",
