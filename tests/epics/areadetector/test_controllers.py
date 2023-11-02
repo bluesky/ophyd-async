513c20d7ea3bd77492f31f5cb330f011c9fa88af
Rose Syrett <90774497+rosesyrett@users.noreply.github.com>
2023-11-02 16:13:56 +0000

    Change mode-> trigger for pilatus controller (#58)

--- tests/epics/areadetector/test_controllers.py
+++ tests/epics/areadetector/test_controllers.py
@@ -52,7 +52,7 @@ async def test_ad_controller(RE, ad: ADSimController):
 
 async def test_pilatus_controller(RE, pilatus: PilatusController):
     with patch("ophyd_async.core.signal.wait_for_value", return_value=None):
-        await pilatus.arm(mode=DetectorTrigger.constant_gate)
+        await pilatus.arm(trigger=DetectorTrigger.constant_gate)
 
     driver = pilatus.driver
     assert await driver.num_images.get_value() == 0
