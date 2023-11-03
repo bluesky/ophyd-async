c71942efe85447fad22ed6d2fdebdd0d6b353565
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-11-03 09:44:55 +0000

    Change test name for device collector

--- tests/core/test_device_collector.py
+++ tests/core/test_device_collector.py
@@ -1,14 +1,17 @@
+from ophyd_async.core import NotConnected
 import pytest
 
 from ophyd_async.core import Device, DeviceCollector
 
 
 class Dummy(Device):
-    def connect(self, sim: bool = False):
+    async def connect(self, sim: bool = False):
         raise AttributeError()
 
 
-def test_device_collector_propagates_error(RE):
-    with pytest.raises(AttributeError):
+def test_device_collector_does_not_propagate_error(RE):
+    with pytest.raises(NotConnected) as exc:
         with DeviceCollector():
             _ = Dummy("somename")
+
+    assert not exc.value.__cause__
