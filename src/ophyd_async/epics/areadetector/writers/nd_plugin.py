c13c529859ebeaa6d45c7069dd3a8edcbea82907
Rose Syrett <90774497+rosesyrett@users.noreply.github.com>
2023-11-13 14:16:47 +0000

    Flesh out NDPluginBase to include nd array port and enable callback pvs (#70)
    ophyd_async.epics.areadetector.writers.nd_plugin NDPluginBase now has pv's for an array port and to enable callbacks.

--- src/ophyd_async/epics/areadetector/writers/nd_plugin.py
+++ src/ophyd_async/epics/areadetector/writers/nd_plugin.py
@@ -1,7 +1,14 @@
+from enum import Enum
+
 from ophyd_async.core import Device
 from ophyd_async.epics.signal import epics_signal_rw
 
-from ..utils import ad_r
+from ..utils import ad_r, ad_rw
+
+
+class Callback(str, Enum):
+    Enable = "Enable"
+    Disable = "Disable"
 
 
 class NDArrayBase(Device):
@@ -12,7 +19,10 @@ class NDArrayBase(Device):
 
 
 class NDPluginBase(NDArrayBase):
-    pass
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self.nd_array_port = ad_rw(str, prefix + "NDArrayPort")
+        self.enable_callback = ad_rw(Callback, prefix + "EnableCallbacks")
+        super().__init__(prefix, name)
 
 
 class NDPluginStats(NDPluginBase):
