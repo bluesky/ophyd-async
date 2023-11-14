fed40c5ce622cc1271106ebd40eabe8bcffbe7cb
Rose Syrett <90774497+rosesyrett@users.noreply.github.com>
2023-11-14 09:45:37 +0000

    Add two extra PVs to NDFileHDF (#79)
    * Add two extra PVs to NDFileHDF
    
    * Add NDArrayAddress to NDPluginBase, make NDFileHDF subclass it

--- src/ophyd_async/epics/areadetector/writers/nd_file_hdf.py
+++ src/ophyd_async/epics/areadetector/writers/nd_file_hdf.py
@@ -1,12 +1,26 @@
-from ophyd_async.core import Device
+from enum import Enum
 
 from ...signal.signal import epics_signal_rw
 from ..utils import FileWriteMode, ad_r, ad_rw
+from .nd_plugin import NDPluginBase
 
 
-class NDFileHDF(Device):
-    def __init__(self, prefix: str) -> None:
+class Compression(str, Enum):
+    none = "None"
+    nbit = "N-bit"
+    szip = "szip"
+    zlib = "zlib"
+    blosc = "Blosc"
+    bslz4 = "BSLZ4"
+    lz4 = "LZ4"
+    jpeg = "JPEG"
+
+
+class NDFileHDF(NDPluginBase):
+    def __init__(self, prefix: str, name="") -> None:
         # Define some signals
+        self.position_mode = ad_rw(bool, prefix + "PositionMode")
+        self.compression = ad_rw(Compression, prefix + "Compression")
         self.num_extra_dims = ad_rw(int, prefix + "NumExtraDims")
         self.file_path = ad_rw(str, prefix + "FilePath")
         self.file_name = ad_rw(str, prefix + "FileName")
@@ -22,3 +36,4 @@ class NDFileHDF(Device):
         self.flush_now = epics_signal_rw(bool, prefix + "FlushNow")
         self.array_size0 = ad_r(int, prefix + "ArraySize0")
         self.array_size1 = ad_r(int, prefix + "ArraySize1")
+        super().__init__(prefix, name)
