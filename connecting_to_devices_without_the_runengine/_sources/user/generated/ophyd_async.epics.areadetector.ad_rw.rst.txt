fef2dd6717df9216e3dbad3a457063c93cbfe3e5
evalott100 <evalott100@users.noreply.github.com>
2024-02-15 13:18:42 +0000

    deploy: c73c0af125bbbd0cb782bac9438be360b8c5e2d6

---
+++ connecting_to_devices_without_the_runengine/_sources/user/generated/ophyd_async.epics.areadetector.ad_rw.rst.txt
@@ -0,0 +1,6 @@
+ophyd\_async.epics.areadetector.ad\_rw
+======================================
+
+.. currentmodule:: ophyd_async.epics.areadetector
+
+.. autofunction:: ad_rw
---
+++ connecting_to_devices_without_the_runengine/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.2.1.dev12+gc73c0af1',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
