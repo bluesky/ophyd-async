31d18b34badffe7beff5b9d995bc69b880ef8db7
olliesilvester <olliesilvester@users.noreply.github.com>
2024-03-01 15:29:09 +0000

    deploy: 51d5b9fa23bdf3e53d9dfb614ef267d987b35b4d

---
+++ add_hdf_writer/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.2.1.dev29+g51d5b9fa',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
