f7f734e6b4c75d2fb9f04bda743a0cf9ebeef641
olliesilvester <olliesilvester@users.noreply.github.com>
2024-03-13 15:50:31 +0000

    deploy: c3579fdb365dad62bfe43a329b80e799c505438c

--- add_hdf_writer/_static/documentation_options.js
+++ add_hdf_writer/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.1.dev35+gd265ef776',
+    VERSION: '0.2.1.dev37+gc3579fdb3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
