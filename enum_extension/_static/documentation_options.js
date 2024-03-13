2a19d85dd54bb9dce0b6de59af49187939025ee6
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2024-03-13 13:10:52 +0000

    deploy: 2161fee9b4407cc239250b09fd7030afad3a4971

--- enum_extension/_static/documentation_options.js
+++ enum_extension/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.2.dev19+ge80a008fa',
+    VERSION: '0.2.2.dev19+g2161fee9b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
