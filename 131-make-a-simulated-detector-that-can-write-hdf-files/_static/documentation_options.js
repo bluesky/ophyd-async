b2fd0d2e8691c5936191d65357870aea7c98a7b1
stan-dot <stan-dot@users.noreply.github.com>
2024-03-22 11:44:58 +0000

    deploy: bf8974916fcd012e0e3e999ffcd441af67d551ec

--- 131-make-a-simulated-detector-that-can-write-hdf-files/_static/documentation_options.js
+++ 131-make-a-simulated-detector-that-can-write-hdf-files/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.2.dev34+gf8af0dd41',
+    VERSION: '0.2.2.dev35+gbf8974916',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
