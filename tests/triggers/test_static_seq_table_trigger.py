b2bf1130b01ab5ea094542c21bee86b8816b226a
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-04-04 17:07:28 +0000

    black format

--- tests/triggers/test_static_seq_table_trigger.py
+++ tests/triggers/test_static_seq_table_trigger.py
@@ -1,15 +1,17 @@
 import pytest
 
 
-
 def test_trigger_logic_contains_seq_block():
     assert 1 == 0
+
 
 def test_trigger_logic_prepare_sets_seq_table():
     assert 1 == 0
 
+
 def test_trigger_logic_can_be_start():
     assert 1 == 0
+
 
 def test_trigger_logic_can_stop():
     assert 1 == 0
