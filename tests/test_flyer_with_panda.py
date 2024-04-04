e52013f06fe6438ef87abb24823b148fab34eb71
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-04-04 17:06:30 +0000

    Add begining of test for new TriggerLogic and Prepare

---
+++ tests/test_flyer_with_panda.py
@@ -0,0 +1,209 @@
+import time
+from enum import Enum
+from typing import AsyncGenerator, AsyncIterator, Dict, Optional, Sequence
+from unittest.mock import Mock
+
+import bluesky.plan_stubs as bps
+import pytest
+from bluesky.protocols import Descriptor, StreamAsset
+from bluesky.run_engine import RunEngine
+from event_model import ComposeStreamResourceBundle, compose_stream_resource
+
+from ophyd_async.core import (
+    DEFAULT_TIMEOUT,
+    DetectorControl,
+    DetectorWriter,
+    HardwareTriggeredFlyable,
+    SignalRW,
+    SimSignalBackend,
+)
+from ophyd_async.core.detector import StandardDetector
+from ophyd_async.core.device import DeviceCollector
+from ophyd_async.core.signal import observe_value
+from ophyd_async.panda import PandA
+from ophyd_async.triggers.static_seq_table_trigger import StaticSeqTableTriggerLogic
+from planstubs.prepare_trigger_and_dets import (
+    prepare_static_seq_table_flyer_and_detectors_with_same_trigger,
+)
+
+
+class DummyWriter(DetectorWriter):
+    def __init__(self, name: str, shape: Sequence[int]):
+        self.dummy_signal = SignalRW(backend=SimSignalBackend(int, source="test"))
+        self._shape = shape
+        self._name = name
+        self._file: Optional[ComposeStreamResourceBundle] = None
+        self._last_emitted = 0
+        self.index = 0
+
+    async def open(self, multiplier: int = 1) -> Dict[str, Descriptor]:
+        return {
+            self._name: Descriptor(
+                source="sim://some-source",
+                shape=self._shape,
+                dtype="number",
+                external="STREAM:",
+            )
+        }
+
+    async def observe_indices_written(
+        self, timeout=DEFAULT_TIMEOUT
+    ) -> AsyncGenerator[int, None]:
+        num_captured: int
+        async for num_captured in observe_value(self.dummy_signal, timeout):
+            yield num_captured
+
+    async def get_indices_written(self) -> int:
+        return self.index
+
+    async def collect_stream_docs(
+        self, indices_written: int
+    ) -> AsyncIterator[StreamAsset]:
+        if indices_written:
+            if not self._file:
+                self._file = compose_stream_resource(
+                    spec="AD_HDF5_SWMR_SLICE",
+                    root="/",
+                    data_key=self._name,
+                    resource_path="",
+                    resource_kwargs={
+                        "path": "",
+                        "multiplier": 1,
+                        "timestamps": "/entry/instrument/NDAttributes/NDArrayTimeStamp",
+                    },
+                )
+                yield "stream_resource", self._file.stream_resource_doc
+
+            if indices_written >= self._last_emitted:
+                indices = dict(
+                    start=self._last_emitted,
+                    stop=indices_written,
+                )
+                self._last_emitted = indices_written
+                self._last_flush = time.monotonic()
+                yield "stream_datum", self._file.compose_stream_datum(indices)
+
+    async def close(self) -> None:
+        self._file = None
+
+
+@pytest.fixture
+async def detector_list(RE: RunEngine) -> tuple[StandardDetector, StandardDetector]:
+    writers = [DummyWriter("testa", (1, 1)), DummyWriter("testb", (1, 1))]
+    await writers[0].dummy_signal.connect(sim=True)
+    await writers[1].dummy_signal.connect(sim=True)
+
+    async def dummy_arm_1(self=None, trigger=None, num=0, exposure=None):
+        return writers[0].dummy_signal.set(1)
+
+    async def dummy_arm_2(self=None, trigger=None, num=0, exposure=None):
+        return writers[1].dummy_signal.set(1)
+
+    detector_1 = StandardDetector(
+        Mock(spec=DetectorControl, get_deadtime=lambda num: num, arm=dummy_arm_1),
+        writers[0],
+        name="detector_1",
+        writer_timeout=3,
+    )
+    detector_2 = StandardDetector(
+        Mock(spec=DetectorControl, get_deadtime=lambda num: num, arm=dummy_arm_2),
+        writers[1],
+        name="detector_2",
+        writer_timeout=3,
+    )
+    return (detector_1, detector_2)
+
+
+@pytest.fixture
+async def panda():
+    async with DeviceCollector(sim=True):
+        sim_panda = PandA("PANDAQSRV:", "sim_panda")
+
+    assert sim_panda.name == "sim_panda"
+    yield sim_panda
+
+
+async def test_hardware_triggered_flyable_with_static_seq_table_logic(
+    RE: RunEngine,
+    detector_list: tuple[StandardDetector],
+    panda,
+):
+    names = []
+    docs = []
+
+    def append_and_print(name, doc):
+        names.append(name)
+        docs.append(doc)
+
+    RE.subscribe(append_and_print)
+
+    shutter_time = 0.004
+    exposure = 1
+    deadtime = max(det.controller.get_deadtime(1) for det in detector_list)
+
+    trigger_logic = StaticSeqTableTriggerLogic(panda.seq[1])
+    flyer = HardwareTriggeredFlyable(trigger_logic, [], name="flyer")
+
+    def flying_plan():
+        yield from bps.stage_all(*detector_list, flyer)
+
+        yield from prepare_static_seq_table_flyer_and_detectors_with_same_trigger(
+            flyer,
+            detector_list,
+            num=2,
+            width=exposure,
+            deadtime=deadtime,
+            shutter_time=shutter_time,
+        )
+
+        for detector in detector_list:
+            detector.controller.disarm.assert_called_once  # type: ignore
+
+        yield from bps.open_run()
+        yield from bps.declare_stream(*detector_list, name="main_stream", collect=True)
+
+        yield from bps.kickoff(flyer)
+        for detector in detector_list:
+            yield from bps.kickoff(detector)
+
+        yield from bps.complete(flyer, wait=False, group="complete")
+        for detector in detector_list:
+            yield from bps.complete(detector, wait=False, group="complete")
+
+        # Manually incremenet the index as if a frame was taken
+        for detector in detector_list:
+            detector.writer.index += 1
+
+        done = False
+        while not done:
+            try:
+                yield from bps.wait(group="complete", timeout=0.5)
+            except TimeoutError:
+                pass
+            else:
+                done = True
+            yield from bps.collect(
+                *detector_list,
+                return_payload=False,
+                name="main_stream",
+            )
+            yield from bps.sleep(0.01)
+        yield from bps.wait(group="complete")
+        yield from bps.close_run()
+
+        yield from bps.unstage_all(flyer, *detector_list)
+        for detector in detector_list:
+            assert detector.controller.disarm.called  # type: ignore
+
+    # fly scan
+    RE(flying_plan())
+
+    assert names == [
+        "start",
+        "descriptor",
+        "stream_resource",
+        "stream_datum",
+        "stream_resource",
+        "stream_datum",
+        "stop",
+    ]
