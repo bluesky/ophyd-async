6dd821ad6eadae0b6304d7dc5b94acf1ce76400d
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-04-04 15:08:16 +0000

    add trigger_info to hdfscan test

--- tests/epics/areadetector/test_scans.py
+++ tests/epics/areadetector/test_scans.py
@@ -105,6 +105,7 @@ def test_hdf_writer_fails_on_timeout_with_flyscan(RE: RunEngine, writer: HDFWrit
     trigger_logic = DummyTriggerLogic()
 
     flyer = HardwareTriggeredFlyable(trigger_logic, [], name="flyer")
+    trigger_info = TriggerInfo(num=1, trigger=DetectorTrigger.constant_gate, deadtime=2, livetime=2)
 
     def flying_plan():
         """NOTE: the following is a workaround to ensure tests always pass.
@@ -115,7 +116,7 @@ def test_hdf_writer_fails_on_timeout_with_flyscan(RE: RunEngine, writer: HDFWrit
             # Prepare the flyer first to get the trigger info for the detectors
             yield from bps.prepare(flyer, 1, wait=True)
             # prepare detector second.
-            yield from bps.prepare(detector, flyer.trigger_info, wait=True)
+            yield from bps.prepare(detector, trigger_info, wait=True)
             yield from bps.open_run()
             yield from bps.kickoff(flyer)
             yield from bps.kickoff(detector)
