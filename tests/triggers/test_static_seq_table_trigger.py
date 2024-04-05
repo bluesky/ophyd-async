3292a1955459da9b63e49ce38b6650f5ede0e2db
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-04-05 16:40:57 +0000

    Add basic test for trigger_logic

--- tests/triggers/test_static_seq_table_trigger.py
+++ tests/triggers/test_static_seq_table_trigger.py
@@ -1,14 +1,22 @@
-def test_trigger_logic_contains_seq_block():
-    assert 1 == 0
+import pytest
+
+from ophyd_async.core.device import DeviceCollector
+from ophyd_async.panda import PandA
+from ophyd_async.triggers.static_seq_table_trigger import StaticSeqTableTriggerLogic
 
 
-def test_trigger_logic_prepare_sets_seq_table():
-    assert 1 == 0
+@pytest.fixture
+async def panda():
+    async with DeviceCollector(sim=True):
+        sim_panda = PandA("PANDAQSRV:", "sim_panda")
+
+    assert sim_panda.name == "sim_panda"
+    yield sim_panda
 
 
-def test_trigger_logic_can_be_start():
-    assert 1 == 0
-
-
-def test_trigger_logic_can_stop():
-    assert 1 == 0
+def test_trigger_logic_has_given_methods(panda: PandA):
+    trigger_logic = StaticSeqTableTriggerLogic(panda.seq[1])
+    assert hasattr(trigger_logic, "prepare")
+    assert hasattr(trigger_logic, "kickoff")
+    assert hasattr(trigger_logic, "complete")
+    assert hasattr(trigger_logic, "stop")
