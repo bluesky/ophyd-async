c2336f3fdaeeec5b6850804276dd58c1cd6f56eb
Stanisław Malinowski <56644812+stan-dot@users.noreply.github.com>
2024-04-15 11:31:05 +0100

    131 make a simulated detector that can write hdf files (#144)
    * add the sim module
    
    * day progress
    
    * more exploration
    
    * day's work
    
    * incremental changes
    
    * an attempt to put features together
    
    * incremental changes
    
    * try SimSample
    
    * make pattern generator do sensible things
    
    * make lint happy
    
    * attempt at tests
    
    * WIP setting up pytest with fake paths
    
    * write more test outlines
    
    * work on tests
    
    * WIP fix async tests
    
    * WIP make the tests work better
    
    * raw refactor
    
    * refactoring proof of concept
    
    * delete refactoring attempts
    
    * tests improvement
    
    * add basic streaming plan
    
    * rename to sim driver
    
    * change into SimDriver
    
    * fix formatting
    
    * work on the tests
    
    * correct 19 out of 32 errors
    
    * down to 10 errors
    
    * down to 4 errors, all due to observe_value decision and the NDfileHDF
    
    * swmr error
    
    * fix lint
    
    * setup for asyncio
    
    * still error with async
    
    * closing writer and file handle issue
    
    * move back to pattern generator
    
    * got to descriptor validation error
    
    * async setup for all IO failed
    
    * fix writer descriptor test. also component not found async error
    
    * fix the test
    
    * pass all tests
    
    * ran black
    
    * make lint happy
    
    * missed one
    
    * make mypy happy
    
    * fix black
    
    * add declare to the failing plan
    
    * update pyproject for wrong pyside6
    
    * fix the flyer test
    
    * delete mistaken test file
    
    * rollback some unintended changes
    
    * make lint happy
    
    * delete sim sample to lower the test lines
    
    * delete empty test
    
    * mistake double test
    
    * day work (not done yet)
    
    * add snake case@
    
    * lock pyside6 to 6.6.2 to fix the svg indent issue in upstream
    
    * pyproject syntax error fix

---
+++ tests/sim/test_sim_detector.py
@@ -0,0 +1,52 @@
+import pytest
+
+from ophyd_async.core.device import DeviceCollector
+from ophyd_async.epics.motion import motor
+from ophyd_async.sim.sim_pattern_generator import SimPatternDetector
+
+
+@pytest.fixture
+async def sim_motor():
+    async with DeviceCollector(sim=True):
+        sim_motor = motor.Motor("test")
+    return sim_motor
+
+
+async def test_sim_pattern_detector_initialization(
+    sim_pattern_detector: SimPatternDetector,
+):
+    assert (
+        sim_pattern_detector.pattern_generator
+    ), "PatternGenerator was not initialized correctly."
+
+
+async def test_detector_creates_controller_and_writer(
+    sim_pattern_detector: SimPatternDetector,
+):
+    assert sim_pattern_detector.writer
+    assert sim_pattern_detector.controller
+
+
+async def test_writes_pattern_to_file(
+    sim_pattern_detector: SimPatternDetector, sim_motor: motor.Motor, tmp_path
+):
+    sim_pattern_detector = SimPatternDetector(
+        config_sigs=[*sim_motor._read_signals], path=tmp_path
+    )
+
+    images_number = 2
+    await sim_pattern_detector.controller.arm(num=images_number)
+    # assert that the file is created and non-empty
+    assert sim_pattern_detector.writer
+
+    # assert that the file contains data in expected dimensions
+
+
+async def test_set_x_and_y(sim_pattern_detector):
+    assert sim_pattern_detector.pattern_generator.x == 0
+    sim_pattern_detector.pattern_generator.set_x(200)
+    assert sim_pattern_detector.pattern_generator.x == 200
+
+
+async def test_initial_blob(sim_pattern_detector: SimPatternDetector):
+    assert sim_pattern_detector.pattern_generator.STARTING_BLOB.any()
