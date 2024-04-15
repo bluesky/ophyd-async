5f3df2cf78f95c45758f56f0d9d1f99827d34081
Jakub Wlodek <jwlodek@bnl.gov>
2024-04-15 10:33:57 -0400

    Adding driver and controller classes for Kinetix and Vimba cameras

---
+++ src/ophyd_async/epics/areadetector/controllers/vimba_controller.py
@@ -0,0 +1,65 @@
+import asyncio
+from typing import Optional, Set
+
+from ophyd_async.core import AsyncStatus, DetectorControl, DetectorTrigger
+from ophyd_async.epics.areadetector.drivers.ad_base import (
+    DEFAULT_GOOD_STATES,
+    DetectorState,
+    start_acquiring_driver_and_ensure_status,
+)
+
+from ..drivers.vimba_driver import VimbaDriver, ExposeOutMode, OnOff, TriggerSource
+from ..utils import ImageMode, stop_busy_record
+
+TRIGGER_MODE = {
+    DetectorTrigger.internal: OnOff.off,
+    DetectorTrigger.constant_gate: OnOff.on,
+    DetectorTrigger.variable_gate: OnOff.on,
+    DetectorTrigger.edge_trigger: OnOff.on,
+}
+
+EXPOSE_OUT_MODE = {
+    DetectorTrigger.internal: ExposeOutMode.timed,
+    DetectorTrigger.constant_gate: ExposeOutMode.trigger_width,
+    DetectorTrigger.variable_gate: ExposeOutMode.trigger_width,
+    DetectorTrigger.edge_trigger: ExposeOutMode.timed,
+}
+
+
+class VimbaController(DetectorControl):
+    def __init__(
+        self,
+        driver: VimbaDriver,
+        good_states: Set[DetectorState] = set(DEFAULT_GOOD_STATES),
+    ) -> None:
+        self.driver = driver
+        self.good_states = good_states
+
+    def get_deadtime(self, exposure: float) -> float:
+        return 0.001
+
+    async def arm(
+        self,
+        num: int,
+        trigger: DetectorTrigger = DetectorTrigger.internal,
+        exposure: Optional[float] = None,
+    ) -> AsyncStatus:
+        await asyncio.gather(
+            self.driver.trigger_mode.set(TRIGGER_MODE[trigger]),
+            self.driver.expose_out_mode.set(EXPOSE_OUT_MODE[trigger]),
+            self.driver.num_images.set(999_999 if num == 0 else num),
+            self.driver.image_mode.set(ImageMode.multiple),
+        )
+        if exposure is not None and trigger not in [
+            DetectorTrigger.variable_gate,
+            DetectorTrigger.constant_gate,
+        ]:
+            await self.driver.acquire_time.set(exposure)
+        if trigger != DetectorTrigger.internal:
+            self.driver.trigger_source = TriggerSource.line1
+        return await start_acquiring_driver_and_ensure_status(
+            self.driver, good_states=self.good_states
+        )
+
+    async def disarm(self):
+        await self.driver.acquire.set(0)
---
+++ src/ophyd_async/epics/areadetector/drivers/vimba_driver.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+from ..utils import ad_rw
+from .ad_base import ADBase
+
+
+class PixelFormat(str, Enum):
+    internal = "Mono8"
+    ext_enable = "Mono12"
+    ext_trigger = "Ext. Trigger"
+    mult_trigger = "Mult. Trigger"
+    alignment = "Alignment"
+
+
+class ConvertFormat(str, Enum):
+    none = "None"
+    mono8 = "Mono8"
+    mono16 = "Mono16"
+    rgb8 = "RGB8"
+    rgb16 = "RGB16"
+
+class TriggerSource(str, Enum):
+    freerun = "Freerun"
+    line1 = "Line1"
+    line2 = "Line2"
+    fixed_rate = "FixedRate"
+    software = "Software"
+    action0 = "Action0"
+    action1 = "Action1"
+
+class Overlap(str, Enum):
+    off = "Off"
+    prev_frame = "PreviousFrame"
+
+class OnOff(str, Enum):
+    on = "On"
+    off = "Off"
+
+class ExposeOutMode(str, Enum):
+    timed = "Timed"  # Use ExposureTime PV
+    trigger_width = "TriggerWidth"  # Expose for length of high signal
+
+class VimbaDriver(ADBase):
+    def __init__(self, prefix: str) -> None:
+        # self.pixel_format = ad_rw(PixelFormat, prefix + "PixelFormat")
+        self.convert_format = ad_rw(ConvertFormat, prefix + "ConvertPixelFormat")
+        self.trigger_source = ad_rw(TriggerSource, prefix + "TriggerSource")
+        self.trigger_mode = ad_rw(OnOff, prefix + "TriggerMode")
+        self.overlap = ad_rw(Overlap, prefix + "TriggerOverlap")
+        self.expose_out_mode = ad_rw(ExposeOutMode, prefix + "ExposureMode")
+        super().__init__(prefix)
