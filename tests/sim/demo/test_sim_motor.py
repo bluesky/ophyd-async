82ea5b65c0bdb3774f09813ea16a5a128d357b02
Giles Knap <giles.knap@diamond.ac.uk>
2024-04-17 13:44:06 +0000

    remove move, make _move synchronous

--- tests/sim/demo/test_sim_motor.py
+++ tests/sim/demo/test_sim_motor.py
@@ -3,16 +3,19 @@ import time
 
 from bluesky.plans import spiral_square
 from bluesky.run_engine import RunEngine
+from ophyd_async.core.device import DeviceCollector
 from ophyd_async.sim.demo.sim_motor import SimMotor
 
 
 async def test_move_sim_in_plan():
     RE = RunEngine()
 
-    m1 = SimMotor("M1", "sim_motor1")
-    m2 = SimMotor("M2", "sim_motor2")
-    await m1.connect()
-    await m2.connect()
+    async with DeviceCollector():
+        m1 = SimMotor("M1", "sim_motor1")
+        m2 = SimMotor("M2", "sim_motor2")
+
+    await m1.velocity.set(2)
+    await m2.velocity.set(2)
 
     my_plan = spiral_square([], m1, m2, 0, 0, 4, 4, 10, 10)
 
@@ -23,25 +26,24 @@ async def test_move_sim_in_plan():
 
 
 async def test_slow_move():
-    _ = RunEngine()
+    async with DeviceCollector():
+        m1 = SimMotor("M1", "sim_motor1", instant=False)
 
-    m1 = SimMotor("M1", "sim_motor1", instant=False)
-    await m1.connect()
     await m1.velocity.set(20)
 
     start = time.monotonic()
-    m1.move(10)
+    await m1.set(10)
     elapsed = time.monotonic() - start
 
+    assert await m1.user_readback.get_value() == 10
     assert elapsed >= 0.5
     assert elapsed < 1
-    assert await m1.user_readback.get_value() == 10
 
 
 async def test_stop():
-    _ = RunEngine()
+    async with DeviceCollector():
+        m1 = SimMotor("M1", "sim_motor1", instant=False)
 
-    m1 = SimMotor("M1", "sim_motor1", instant=False)
     await m1.connect()
     await m1.velocity.set(2)
 
