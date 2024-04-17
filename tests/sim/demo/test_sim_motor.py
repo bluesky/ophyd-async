10f3269b7e1a82bd828ba9d29335475698b5dffa
Giles Knap <giles.knap@diamond.ac.uk>
2024-04-17 09:26:17 +0000

    switch to pure simulation

--- tests/sim/demo/test_sim_motor.py
+++ tests/sim/demo/test_sim_motor.py
@@ -3,7 +3,7 @@ import time
 from bluesky.plans import spiral_square
 from bluesky.run_engine import RunEngine
 from ophyd_async.core import set_sim_value
-from ophyd_async.epics.motion.sim_motor import SimMotor
+from ophyd_async.sim.demo.sim_motor import SimMotor
 
 
 async def test_move_sim_in_plan():
