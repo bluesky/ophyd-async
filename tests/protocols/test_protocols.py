352b81694d1b99e81aba92d9b9708cebe1698754
Oliver Copping <ryi58813@diamtl04.diamond.ac.uk>
2024-04-18 08:30:36 +0100

    Removed test_mypy

--- tests/protocols/test_protocols.py
+++ tests/protocols/test_protocols.py
@@ -15,36 +15,3 @@ def test_readable():
 
 def test_pausable():
     assert isinstance(sim.det1, bs_protocols.AsyncPausable)
-
-
-# I think the commented out tests pass because __getattr__ is implemented, but not sure
-@pytest.mark.skip(reason="ophyd missing py.typed to communicate type hints to mypy")
-@pytest.mark.parametrize(
-    "type_, hardware, pass_",
-    [
-        ("Readable", "ABDetector(name='hi')", True),
-        ("Readable", "SynAxis(name='motor1')", True),
-        ("Readable", "TrivialFlyer()", False),
-        ("Configurable", "ABDetector(name='hi')", True),
-        ("Pausable", "ABDetector(name='hi')", True),
-    ],
-)
-def test_mypy(type_, hardware, pass_):
-    template = f"""
-from ophyd_async import protocols as bs_protocols
-from ophyd import sim
-
-var: bs_protocols.{type_} = sim.{hardware}
-"""
-
-    with tempfile.NamedTemporaryFile("wt") as f:
-        f.write(template)
-        f.seek(0)
-        stdout, stderr, exit = mypy.api.run([f.name])
-        # pass true means exit 0, pass false means nonzero exit
-        try:
-            assert exit != pass_
-        except AssertionError:
-            print(stdout)
-            print(stderr)
-            raise
