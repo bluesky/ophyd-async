f20b63b57b82599bda8ffc96bc3bcf2cc9dc8ef5
RobertSchaffer1 <87657015+RobertSchaffer1@users.noreply.github.com>
2024-04-22 09:46:58 -0400

    Fixes broken link on docs page (#253)
    * Fixes broken link on docs page
    
    * STY: fix ruff/pre-commit errors
    
    ```
    lint with ruff...........................................................Failed
    - hook id: ruff
    - exit code: 1
    
    docs/conf.py:155:17: F541 [*] f-string without any placeholders
    Found 1 error.
    [*] 1 fixable with the `--fix` option.
    ```
    
    ---------
    
    Co-authored-by: Max Rakitin <mrakitin@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -152,7 +152,7 @@ copybutton_prompt_is_regexp = True
 html_theme = "pydata_sphinx_theme"
 github_repo = "ophyd-async"
 github_user = "bluesky"
-switcher_json = f"https://{github_user}.github.io/{github_repo}/switcher.json"
+switcher_json = "https://blueskyproject.io/ophyd-async/switcher.json"
 switcher_exists = requests.get(switcher_json).ok
 if not switcher_exists:
     print(
