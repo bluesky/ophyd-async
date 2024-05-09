dc9e179ec2ea7c5d26237e18455e321d94fd3931
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-05-09 10:48:53 +0100

    Remove staging and open/close run

--- src/ophyd_async/planstubs/fly.py
+++ src/ophyd_async/planstubs/fly.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import bluesky.plan_stubs as bps
 
 from ophyd_async.core.detector import StandardDetector
@@ -16,24 +14,23 @@ def fly(
     number_of_frames: int,
     exposure: int,
     shutter_time: float,
-    repeats: Optional[int],
-    period: Optional[float],
+    repeats: int = 1,
+    period: float = 0.0,
 ):
     """Run a scan wth a flyer and multiple detectors.
 
-    The standard basic flow for a flyscan.
+    The standard basic flow for a flyscan:
 
-    1. Set up and prepare a trigger
+    - Set up the flyer with a static sequence table and detectors with a trigger
+    - Declare the stream and kickoff the scan
+    - Collect while completing
 
-    2. fly and collect
+    This needs to be used in a plan that instantates detectors and a flyer,
+    stages/unstages the devices and opens and closes the run.
 
     """
-    deadtime = max(det.controller.get_deadtime(1) for det in detector_list)
-
-    # sort repeats and period
-
     # Set up scan and prepare trigger
-    yield from bps.stage_all(*detector_list, flyer)
+    deadtime = max(det.controller.get_deadtime(1) for det in detector_list)
     yield from prepare_static_seq_table_flyer_and_detectors_with_same_trigger(
         flyer,
         detector_list,
@@ -44,20 +41,13 @@ def fly(
         repeats=repeats,
         period=period,
     )
-    yield from bps.open_run()
     yield from bps.declare_stream(*detector_list, name=stream_name, collect=True)
-
-    # fly and collect
     yield from bps.kickoff_all(flyer, *detector_list)
+
+    # collect_while_completing
     yield from bps.complete_all(flyer, *detector_list, group="complete")
 
     done = False
     while not done:
-        yield from bps.wait(group="complete", timeout=0.5)
+        done = yield from bps.wait(group="complete", timeout=0.5)
         yield from bps.collect(*detector_list, name=stream_name)
-
-    yield from bps.wait(group="complete")
-
-    # Finish
-    yield from bps.close_run()
-    yield from bps.unstage_all(flyer, *detector_list)
