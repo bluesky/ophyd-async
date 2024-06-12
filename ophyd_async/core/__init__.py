c57ac7860a49e134f1daaaa37dca9d85258561b1
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-12 11:30:29 +0100

    Remove util imports for now

--- ophyd_async/core/__init__.py
+++ ophyd_async/core/__init__.py
@@ -1,14 +1,11 @@
 from ._device import Device, DeviceCollector, DeviceVector
-from ._utils import DEFAULT_TIMEOUT, NotConnected, wait_for_connection
+
 from._detector import StandardDetector, DetectorControl, TriggerInfo, DetectorTrigger
 
 __all__ = [
     "Device",
     "DeviceCollector",
     "DeviceVector",
-    "DEFAULT_TIMEOUT",
-    "NotConnected", 
-    "wait_for_connection",
     "StandardDetector",
     "DetectorControl",
     "TriggerInfo",
