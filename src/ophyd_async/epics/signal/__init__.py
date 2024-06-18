338efbfc31af418e8ebf5bbcf151508a766b2c45
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-18 13:51:48 +0100

    Add common method to signal init

--- src/ophyd_async/epics/signal/__init__.py
+++ src/ophyd_async/epics/signal/__init__.py
@@ -1,8 +1,11 @@
+from ._common import get_supported_values
 from ._p4p import PvaSignalBackend
 from ._signal import (epics_signal_r, epics_signal_rw, epics_signal_rw_rbv,
                       epics_signal_w, epics_signal_x)
 
 __all__ = [
+    "get_supported_values",
+
     "PvaSignalBackend",
 
     "epics_signal_r",
