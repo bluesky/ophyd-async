9cf24498b48495a2975aae984635e764a2b08dab
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-18 11:01:40 +0100

    Add adbase imports for sim detector

--- src/ophyd_async/epics/adcore/__init__.py
+++ src/ophyd_async/epics/adcore/__init__.py
@@ -1,11 +1,13 @@
-from ._ad_base import (ADBase, ADBaseShapeProvider,
-                       start_acquiring_driver_and_ensure_status)
+from ._ad_base import (DEFAULT_GOOD_STATES, ADBase, ADBaseShapeProvider,
+                       DetectorState, start_acquiring_driver_and_ensure_status)
 from .writers.hdf_writer import HDFWriter
 from .writers.nd_file_hdf import NDFileHDF
 
 __all__ = [
+    "DEFAULT_GOOD_STATES",
     "ADBase",
     "ADBaseShapeProvider",
+    "DetectorState",
     "start_acquiring_driver_and_ensure_status",
 
     "HDFWriter",
