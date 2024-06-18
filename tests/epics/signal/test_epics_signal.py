55d675769fc14d17e1a3efaff3104fad91de520f
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-18 14:32:05 +0100

    Add EpicsTransport and _make_backend to epics signal init

--- tests/epics/signal/test_epics_signal.py
+++ tests/epics/signal/test_epics_signal.py
@@ -21,10 +21,10 @@ from bluesky.protocols import Reading
 
 from ophyd_async.core import (NotConnected, SignalBackend, T, get_dtype,
                               load_from_yaml, save_to_yaml)
-from ophyd_async.epics.signal import (epics_signal_r, epics_signal_rw,
+from ophyd_async.epics.signal import (EpicsTransport, _make_backend,
+                                      epics_signal_r, epics_signal_rw,
                                       epics_signal_rw_rbv, epics_signal_w,
                                       epics_signal_x)
-from ophyd_async.epics.signal._epics_transport import EpicsTransport
 
 RECORDS = str(Path(__file__).parent / "test_records.db")
 PV_PREFIX = "".join(random.choice(string.ascii_lowercase) for _ in range(12))
