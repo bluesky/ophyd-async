90f3cd481a6dcbf755953bdef87e3287dcd9a82d
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-18 10:58:15 +0100

    Update epics motor signal import

--- src/ophyd_async/epics/motor.py
+++ src/ophyd_async/epics/motor.py
@@ -6,8 +6,8 @@ from ophyd_async.core import (DEFAULT_TIMEOUT, CalculatableTimeout,
                               CalculateTimeout, ConfigSignal, HintedSignal,
                               StandardReadable, WatchableAsyncStatus,
                               WatcherUpdate, observe_value)
-
-from ..signal.signal import epics_signal_r, epics_signal_rw, epics_signal_x
+from ophyd_async.epics.signal import (epics_signal_r, epics_signal_rw,
+                                      epics_signal_x)
 
 
 class Motor(StandardReadable, Movable, Stoppable):
