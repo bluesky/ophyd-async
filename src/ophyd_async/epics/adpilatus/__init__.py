aae99635272cf62d5bb9e1f59d5240b5eeadc7c9
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-18 13:59:39 +0100

    Add extra Pilatus imports to pilatus init

--- src/ophyd_async/epics/adpilatus/__init__.py
+++ src/ophyd_async/epics/adpilatus/__init__.py
@@ -1,5 +1,13 @@
-from ._pilatus import PilatusDetector
+from ._pilatus import PilatusDetector, PilatusReadoutTime
+from ._pilatus_controller import PilatusController
+from ._pilatus_driver import PilatusDriver, PilatusTriggerMode
 
 __all__ = [
     "PilatusDetector",
+    "PilatusReadoutTime",
+
+    "PilatusController",
+
+    "PilatusDriver",
+    "PilatusTriggerMode",
 ]
