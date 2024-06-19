c1362e992ad2cf61452e985edc705bc66f08f99a
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-06-19 13:05:11 +0100

    Move epics utils to adcore

--- src/ophyd_async/epics/__init__.py
+++ src/ophyd_async/epics/__init__.py
@@ -1,15 +1,1 @@
-from .utils import (
-    FileWriteMode,
-    ImageMode,
-    NDAttributeDataType,
-    NDAttributesXML,
-    stop_busy_record,
-)
 
-__all__ = [
-    "FileWriteMode",
-    "ImageMode",
-    "NDAttributeDataType",
-    "NDAttributesXML",
-    "stop_busy_record",
-]
--- src/ophyd_async/epics/adcore/__init__.py
+++ src/ophyd_async/epics/adcore/__init__.py
@@ -1,11 +1,8 @@
-from ._ad_base import (
-    DEFAULT_GOOD_STATES,
-    ADBase,
-    ADBaseShapeProvider,
-    DetectorState,
-    start_acquiring_driver_and_ensure_status,
-)
+from ._ad_base import (DEFAULT_GOOD_STATES, ADBase, ADBaseShapeProvider,
+                       DetectorState, start_acquiring_driver_and_ensure_status)
 from ._single_trigger_det import SingleTriggerDet
+from ._utils import (FileWriteMode, ImageMode, NDAttributeDataType,
+                     NDAttributesXML, stop_busy_record)
 from .writers import HDFWriter, NDFileHDF, NDPluginStats
 
 __all__ = [
@@ -15,6 +12,11 @@ __all__ = [
     "DetectorState",
     "start_acquiring_driver_and_ensure_status",
     "SingleTriggerDet",
+    "FileWriteMode",
+    "ImageMode",
+    "NDAttributeDataType",
+    "NDAttributesXML",
+    "stop_busy_record",
     "HDFWriter",
     "NDFileHDF",
     "NDPluginStats",
