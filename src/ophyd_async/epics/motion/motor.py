280ba104ae2af44ff50abe3b8a919618c9143f9c
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-06-28 16:55:53 +0100

    Add prepare to motor

--- src/ophyd_async/epics/motion/motor.py
+++ src/ophyd_async/epics/motion/motor.py
@@ -1,6 +1,7 @@
 import asyncio
 
-from bluesky.protocols import Movable, Stoppable
+from bluesky.protocols import Flyable, Movable, Preparable, Stoppable
+from pydantic import BaseModel
 
 from ophyd_async.core import (
     ConfigSignal,
@@ -8,6 +9,7 @@ from ophyd_async.core import (
     StandardReadable,
     WatchableAsyncStatus,
 )
+from ophyd_async.core.async_status import AsyncStatus
 from ophyd_async.core.signal import observe_value
 from ophyd_async.core.utils import (
     DEFAULT_TIMEOUT,
@@ -19,7 +21,26 @@ from ophyd_async.core.utils import (
 from ..signal.signal import epics_signal_r, epics_signal_rw, epics_signal_x
 
 
-class Motor(StandardReadable, Movable, Stoppable):
+class MotorLimitsException(Exception):
+    pass
+
+
+class FlyMotorInfo(BaseModel):
+    """Minimal set of information required to fly a motor"""
+
+    # Absolute position of the motor once it finishes accelerating to desired velocity,
+    # in millimetres
+    start_position: float
+
+    # Absolute position of the motor once it begins decelerating from desired velocity,
+    # in millimetres
+    end_position: float
+
+    # Time taken for the motor to get from start_position to end_position, in seconds
+    time_for_move: float
+
+
+class Motor(StandardReadable, Movable, Stoppable, Flyable, Preparable):
     """Device that moves a motor record"""
 
     def __init__(self, prefix: str, name="") -> None:
@@ -49,6 +70,15 @@ class Motor(StandardReadable, Movable, Stoppable):
         super().set_name(name)
         # Readback should be named the same as its parent in read()
         self.user_readback.set_name(name)
+
+    def prepare(self, value: FlyMotorInfo):
+        """Calculate required velocity and run-up distance, then if motor limits aren't
+        breached, move to start position minus run-up distance"""
+        return AsyncStatus(self._prepare(value))
+
+    def kickoff(): ...
+
+    def complete(): ...
 
     @WatchableAsyncStatus.wrap
     async def set(
@@ -95,3 +125,33 @@ class Motor(StandardReadable, Movable, Stoppable):
         # Put with completion will never complete as we are waiting for completion on
         # the move above, so need to pass wait=False
         await self.motor_stop.trigger(wait=False)
+
+    async def _prepare(self, value: FlyMotorInfo) -> None:
+        # Velocity at which motor travels from start_position to end_position, in mm/s
+        self.fly_velocity = abs(
+            (value.start_position - value.end_position) / value.time_for_move
+        )
+        velocity_max = await self.max_velocity.get_value()
+        if self.fly_velocity > velocity_max:
+            raise MotorLimitsException(
+                f"Velocity of {self.fly_velocity}mm/s was requested for a motor with vmax of {velocity_max}mm/s"
+            )
+        await self.velocity.set(self.fly_velocity)
+
+        # Distance required for motor to accelerate to fly_velocity before reaching start_position, and distance required
+        # for motor to decelerate from fly_velocity to zero after end_position
+        self.run_up_distance = (
+            0.5 * (await self.acceleration_time.get_value()) * self.fly_velocity
+        )
+        motor_lower_limit = await self.low_limit_travel.get_value()
+        motor_upper_limit = await self.high_limit_travel.get_value()
+        if (
+            value.start_position - self.run_up_distance < motor_lower_limit
+            or value.end_position + self.run_up_distance > motor_upper_limit
+        ):
+            raise MotorLimitsException(
+                f"Requested a motor trajectory of {value.start_position}mm to {value.end_position}mm and motor limits are {value.start_position} <= x <= {value.end_position}"
+            )
+
+        await self.user_setpoint.set(value.start_position - self.run_up_distance)
+        return await self.set(value.start_position - self.run_up_distance)
