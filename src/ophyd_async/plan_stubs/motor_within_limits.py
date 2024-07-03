3d720415ec75639624f8cf09497b852bc015afdf
olliesilvester <122091460+olliesilvester@users.noreply.github.com>
2024-07-03 19:00:26 +0100

    Delete src/ophyd_async/plan_stubs/motor_within_limits.py

--- src/ophyd_async/plan_stubs/motor_within_limits.py
+++
@@ -1,3 +0,0 @@
-# def motor_position_within_limits(motor: Motor, position: float):
-#     low, high = yield bps.locate(motor.low_limit_travel, motor.high_limit_travel)
-#     return low.readback <= position <= high.readback
