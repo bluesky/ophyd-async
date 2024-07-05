bde2c838cd14a9d452e63fe594746b04bd9ef26c
Tom Trafford <tom.trafford@diamond.ac.uk>
2024-07-05 14:25:59 +0000

    Initial trajectory implementation

---
+++ tests/epics/pmac/test_pmac.py
@@ -0,0 +1,193 @@
+import pytest
+from scanspec.regions import Circle
+from scanspec.specs import Line, fly
+
+from ophyd_async.core import DeviceCollector, set_mock_value
+from ophyd_async.epics.motion import motor
+from ophyd_async.epics.pmac import PmacTrajectory
+
+
+@pytest.fixture
+async def sim_x_motor():
+    async with DeviceCollector(mock=True):
+        sim_motor = motor.Motor("BLxxI-MO-TABLE-01:X", name="sim_x_motor")
+
+    set_mock_value(sim_motor.motor_egu, "mm")
+    set_mock_value(sim_motor.precision, 3)
+    set_mock_value(sim_motor.velocity, 1)
+    set_mock_value(sim_motor.acceleration_time, 1)
+    yield sim_motor
+
+
+@pytest.fixture
+async def sim_y_motor():
+    async with DeviceCollector(mock=True):
+        sim_motor = motor.Motor("BLxxI-MO-TABLE-01:Y", name="sim_y_ motor")
+
+    set_mock_value(sim_motor.motor_egu, "mm")
+    set_mock_value(sim_motor.precision, 3)
+    set_mock_value(sim_motor.velocity, 1)
+    set_mock_value(sim_motor.acceleration_time, 1)
+    yield sim_motor
+
+
+async def test_sim_pmac_trajectory(sim_x_motor, sim_y_motor) -> None:
+    # Test the generated Trajectory profile from a scanspec
+    async with DeviceCollector(mock=True):
+        prefix = "BLxxI-MO-STEP-01"
+        motors = [[sim_x_motor, "x"], [sim_y_motor, "y"]]
+        traj = PmacTrajectory(prefix, 2, motors, name="sim_pmac")
+        grid = Line("y", 10, 20, 11) * ~Line("x", 1, 5, 5)
+        spec = fly(grid, 0.4) & Circle("x", "y", 3.0, 15, radius=3)
+        stack = spec.calculate()
+        await traj.prepare(stack)
+    assert traj.profile == {
+        "duration": [
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+            0.4,
+        ],
+        "x": [
+            3,
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1,
+            3,
+        ],
+        "x_velocity": [
+            5.0,
+            -2.5,
+            -2.5,
+            -2.5,
+            -2.5,
+            0.0,
+            2.5,
+            2.5,
+            2.5,
+            2.5,
+            0.0,
+            -2.5,
+            -2.5,
+            -2.5,
+            -2.5,
+            0.0,
+            2.5,
+            2.5,
+            2.5,
+            2.5,
+            0.0,
+            -2.5,
+            -2.5,
+            -2.5,
+            -2.5,
+            5.0,
+            0,
+        ],
+        "y": [
+            12,
+            13,
+            13,
+            13,
+            13,
+            13,
+            14,
+            14,
+            14,
+            14,
+            14,
+            15,
+            15,
+            15,
+            15,
+            15,
+            16,
+            16,
+            16,
+            16,
+            16,
+            17,
+            17,
+            17,
+            17,
+            17,
+            18,
+        ],
+        "y_velocity": [
+            2.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            2.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            2.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            2.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            2.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            2.5,
+            0,
+        ],
+    }
+
+    assert traj.initial_pos["x"] == 0.5
+    assert traj.initial_pos["y"] == 10.75
