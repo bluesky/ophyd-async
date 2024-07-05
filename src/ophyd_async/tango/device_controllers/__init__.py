06a9911f6aa3806a6ca14d11e5e74dd20d7f0f29
Devin Burke <devin.burke@desy.de>
2024-07-05 13:30:51 +0200

    Removed unused TangoMover

--- src/ophyd_async/tango/device_controllers/__init__.py
+++ src/ophyd_async/tango/device_controllers/__init__.py
@@ -1,6 +1,5 @@
 from ophyd_async.tango.device_controllers.dgg2 import DGG2Timer
 from ophyd_async.tango.device_controllers.omsvme58 import OmsVME58Motor
 from ophyd_async.tango.device_controllers.sis3820 import SIS3820Counter
-from ophyd_async.tango.device_controllers.tango_mover import TangoMover
 
-__all__ = ("OmsVME58Motor", "SIS3820Counter", "DGG2Timer", "TangoMover")
+__all__ = ("OmsVME58Motor", "SIS3820Counter", "DGG2Timer")
