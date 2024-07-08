98e40ee8b3616daaa0d3f940f18e272389273666
Tom Trafford <tom.trafford@diamond.ac.uk>
2024-07-08 14:19:11 +0000

    Make test represent current functionality

--- tests/epics/pmac/test_pmac.py
+++ tests/epics/pmac/test_pmac.py
@@ -1,5 +1,4 @@
 import pytest
-from scanspec.regions import Circle
 from scanspec.specs import Line, fly
 
 from ophyd_async.core import DeviceCollector, set_mock_value
@@ -10,189 +9,63 @@ from ophyd_async.epics.pmac import PmacCSMotor, PmacTrajectory
 async def sim_x_motor():
     async with DeviceCollector(mock=True):
         sim_motor = PmacCSMotor(
-            "BLxxI-MO-TABLE-01:X", "BRICK1.CS1", "x", name="sim_x_motor"
+            "BLxxI-MO-STAGE-01:X", "BRICK1.CS3", "z", name="sim_x_motor"
         )
 
     set_mock_value(sim_motor.motor_egu, "mm")
     set_mock_value(sim_motor.precision, 3)
-    set_mock_value(sim_motor.velocity, 1)
-    set_mock_value(sim_motor.acceleration_time, 1)
-    set_mock_value(sim_motor.max_velocity, 10)
+    set_mock_value(sim_motor.acceleration_time, 0.5)
+    set_mock_value(sim_motor.max_velocity, 5)
+    set_mock_value(sim_motor.velocity, 0.5)
+
     yield sim_motor
 
 
-@pytest.fixture
-async def sim_y_motor():
-    async with DeviceCollector(mock=True):
-        sim_motor = PmacCSMotor(
-            "BLxxI-MO-TABLE-01:Y", "BRICK1.CS1", "y", name="sim_y_motor"
-        )
-
-    set_mock_value(sim_motor.motor_egu, "mm")
-    set_mock_value(sim_motor.precision, 3)
-    set_mock_value(sim_motor.velocity, 1)
-    set_mock_value(sim_motor.acceleration_time, 1)
-    set_mock_value(sim_motor.max_velocity, 10)
-    yield sim_motor
-
-
-async def test_sim_pmac_trajectory(sim_x_motor, sim_y_motor) -> None:
+async def test_sim_pmac_simple_trajectory(sim_x_motor) -> None:
     # Test the generated Trajectory profile from a scanspec
     async with DeviceCollector(mock=True):
         prefix = "BLxxI-MO-STEP-01"
-        motors = [sim_x_motor, sim_y_motor]
-        traj = PmacTrajectory(prefix, "BRICK1.CS1", motors, name="sim_pmac")
-        grid = Line("y", 10, 20, 11) * ~Line("x", 1, 5, 5)
-        spec = fly(grid, 0.4) & Circle("x", "y", 3.0, 15, radius=3)
+        motors = [sim_x_motor]
+        traj = PmacTrajectory(prefix, "BRICK1.CS3", motors, name="sim_pmac")
+        spec = fly(Line("z", 1, 5, 9), 1)
         stack = spec.calculate()
         await traj.prepare(stack)
     assert traj.profile == {
         "duration": [
-            900000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
-            400000,
+            1050000,
+            1000000,
+            1000000,
+            1000000,
+            1000000,
+            1000000,
+            1000000,
+            1000000,
+            1000000,
         ],
-        "x": [
-            3,
-            5,
-            4,
-            3,
-            2,
+        "z": [
             1,
-            1,
+            1.5,
             2,
+            2.5,
             3,
+            3.5,
             4,
+            4.5,
             5,
-            5,
-            4,
-            3,
-            2,
-            1,
-            1,
-            2,
-            3,
-            4,
-            5,
-            5,
-            4,
-            3,
-            2,
-            1,
-            3,
         ],
-        "x_velocity": [
-            5.0,
-            -2.5,
-            -2.5,
-            -2.5,
-            -2.5,
-            0.0,
-            2.5,
-            2.5,
-            2.5,
-            2.5,
-            0.0,
-            -2.5,
-            -2.5,
-            -2.5,
-            -2.5,
-            0.0,
-            2.5,
-            2.5,
-            2.5,
-            2.5,
-            0.0,
-            -2.5,
-            -2.5,
-            -2.5,
-            -2.5,
-            5.0,
-            0,
-        ],
-        "y": [
-            12,
-            13,
-            13,
-            13,
-            13,
-            13,
-            14,
-            14,
-            14,
-            14,
-            14,
-            15,
-            15,
-            15,
-            15,
-            15,
-            16,
-            16,
-            16,
-            16,
-            16,
-            17,
-            17,
-            17,
-            17,
-            17,
-            18,
-        ],
-        "y_velocity": [
-            2.5,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            2.5,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            2.5,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            2.5,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            2.5,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            2.5,
+        "z_velocity": [
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
             0,
         ],
     }
 
-    assert traj.initial_pos["x"] == 1.75
-    assert traj.initial_pos["y"] == 11.6875
+    assert traj.initial_pos["z"] == 0.9875
+
+    await traj.kickoff()
