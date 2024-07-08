1c2c94642326982204a6302a616c9df16568635e
Tom Trafford <tom.trafford@diamond.ac.uk>
2024-07-08 12:56:00 +0000

    Implement complete method and setting of CS

--- src/ophyd_async/epics/pmac/_pmacTrajectory.py
+++ src/ophyd_async/epics/pmac/_pmacTrajectory.py
@@ -1,6 +1,9 @@
+import time
+
 from bluesky.protocols import Flyable, Preparable
 
-from ophyd_async.core.async_status import AsyncStatus
+from ophyd_async.core.async_status import AsyncStatus, WatchableAsyncStatus
+from ophyd_async.core.utils import WatcherUpdate
 from ophyd_async.epics.pmac import Pmac, PmacCSMotor
 
 TICK_S = 0.000001
@@ -16,8 +19,9 @@ class PmacTrajectory(Pmac, Flyable, Preparable):
         self.motors = {}
         for motor in motors:
             self.motors[motor.csAxis] = motor
-
-        super().__init__(prefix, name=name)
+        self._fly_start: float
+        self.cs = cs
+        super().__init__(prefix, cs, name=name)
 
     async def _ramp_up_velocity_pos(
         self, velocity: float, motor: PmacCSMotor, end_velocity
@@ -83,6 +87,7 @@ class PmacTrajectory(Pmac, Flyable, Preparable):
         # Send trajectory to brick
         for axis in scanAxes:
             if axis != "DURATION":
+                self.profile_cs_name.set(self.cs)
                 self.points_to_build.set(scanSize)
                 getattr(self, "use_" + axis.lower()).set(True)
                 getattr(self, axis.lower()).set(self.profile[axis.lower()])
@@ -100,9 +105,20 @@ class PmacTrajectory(Pmac, Flyable, Preparable):
         # Set No Of Points
 
         self.build_profile.set(True)
+        self._fly_start = time.monotonic
 
+    @AsyncStatus.wrap
     async def kickoff(self):
-        self.execute_profile.set(True)
+        await self.execute_profile.set(True)
 
+    @WatchableAsyncStatus.wrap
     async def complete(self):
-        pass
+        yield WatcherUpdate(
+            name=self.name,
+            current=self.scan_percent,
+            initial=0,
+            target=100,
+            unit="%",
+            precision=0,
+            time_elapsed=time.monotonic() - self._fly_start,
+        )
