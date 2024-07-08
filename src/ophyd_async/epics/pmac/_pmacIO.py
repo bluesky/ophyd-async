8a84da83acd730427b921dfa7401eb6016694d2f
Tom Trafford <tom.trafford@diamond.ac.uk>
2024-07-08 09:14:09 +0000

    Correct PV datatypes

--- src/ophyd_async/epics/pmac/_pmacIO.py
+++ src/ophyd_async/epics/pmac/_pmacIO.py
@@ -1,3 +1,5 @@
+import numpy as np
+import numpy.typing as npt
 from bluesky.protocols import Flyable, Preparable
 
 from ophyd_async.core import StandardReadable
@@ -9,31 +11,33 @@ class Pmac(StandardReadable, Flyable, Preparable):
     """Device that moves a PMAC Motor record"""
 
     def __init__(self, prefix: str, name="") -> None:
-        self.timeArray = epics_signal_rw(str, prefix + ":ProfileTimeArray")
-        self.a = epics_signal_rw(str, prefix + ":A")
+        self.timeArray = epics_signal_rw(
+            npt.NDArray[np.float64], prefix + ":ProfileTimeArray"
+        )
+        self.a = epics_signal_rw(npt.NDArray[np.float64], prefix + ":A:Positions")
         self.use_a = epics_signal_rw(bool, prefix + ":A:UseAxis")
-        self.a_vel = epics_signal_rw(str, prefix + ":A:Velocities")
-        self.b = epics_signal_rw(str, prefix + ":B")
+        self.a_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":A:Velocities")
+        self.b = epics_signal_rw(npt.NDArray[np.float64], prefix + ":B:Positions")
         self.use_b = epics_signal_rw(bool, prefix + ":B:UseAxis")
-        self.b_vel = epics_signal_rw(str, prefix + ":B:Velocities")
-        self.c = epics_signal_rw(str, prefix + ":C")
+        self.b_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":B:Velocities")
+        self.c = epics_signal_rw(npt.NDArray[np.float64], prefix + ":C:Positions")
         self.use_c = epics_signal_rw(bool, prefix + ":C:UseAxis")
-        self.c_vel = epics_signal_rw(str, prefix + ":C:Velocities")
-        self.u = epics_signal_rw(str, prefix + ":U")
+        self.c_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":C:Velocities")
+        self.u = epics_signal_rw(npt.NDArray[np.float64], prefix + ":U:Positions")
         self.use_u = epics_signal_rw(bool, prefix + ":U:UseAxis")
-        self.u_vel = epics_signal_rw(str, prefix + ":U:Velocities")
-        self.v = epics_signal_rw(str, prefix + ":V")
+        self.u_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":U:Velocities")
+        self.v = epics_signal_rw(npt.NDArray[np.float64], prefix + ":V:Positions")
         self.use_v = epics_signal_rw(bool, prefix + ":V:UseAxis")
-        self.v_vel = epics_signal_rw(str, prefix + ":V:Velocities")
-        self.w = epics_signal_rw(str, prefix + ":W")
+        self.v_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":V:Velocities")
+        self.w = epics_signal_rw(npt.NDArray[np.float64], prefix + ":W:Positions")
         self.use_w = epics_signal_rw(bool, prefix + ":W:UseAxis")
-        self.w_vel = epics_signal_rw(str, prefix + ":W:Velocities")
-        self.x = epics_signal_rw(str, prefix + ":X")
+        self.w_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":W:Velocities")
+        self.x = epics_signal_rw(npt.NDArray[np.float64], prefix + ":X:Positions")
         self.use_x = epics_signal_rw(bool, prefix + ":X:UseAxis")
-        self.x_vel = epics_signal_rw(str, prefix + ":X:Velocities")
-        self.y = epics_signal_rw(str, prefix + ":Y")
+        self.x_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":X:Velocities")
+        self.y = epics_signal_rw(npt.NDArray[np.float64], prefix + ":Y:Positions")
         self.use_y = epics_signal_rw(bool, prefix + ":Y:UseAxis")
-        self.y_vel = epics_signal_rw(str, prefix + ":Y:Velocities")
-        self.z = epics_signal_rw(str, prefix + ":Z")
+        self.y_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":Y:Velocities")
+        self.z = epics_signal_rw(npt.NDArray[np.float64], prefix + ":Z:Positions")
         self.use_z = epics_signal_rw(bool, prefix + ":Z:UseAxis")
-        self.z_vel = epics_signal_rw(str, prefix + ":Z:Velocities")
+        self.z_vel = epics_signal_rw(npt.NDArray[np.float64], prefix + ":Z:Velocities")
