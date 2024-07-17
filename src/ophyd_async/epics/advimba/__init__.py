107f8b452776cb14f5e3f221d6c68775bcbb2581
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-07-17 16:13:27 +0100

    Update naming and imports in advimba

---
+++ src/ophyd_async/epics/advimba/__init__.py
@@ -0,0 +1,9 @@
+from ._vimba import VimbaDetector
+from ._vimba_controller import VimbaController
+from ._vimba_driver import VimbaDriver
+
+__all__ = [
+    "VimbaDetector",
+    "VimbaController",
+    "VimbaDriver",
+]
--- src/ophyd_async/epics/advimba/_vimba_controller.py
+++ src/ophyd_async/epics/advimba/_vimba_controller.py
@@ -2,17 +2,12 @@ import asyncio
 from typing import Optional
 
 from ophyd_async.core import AsyncStatus, DetectorControl, DetectorTrigger
-from ophyd_async.epics.areadetector.drivers.ad_base import (
-    start_acquiring_driver_and_ensure_status,
-)
+from ophyd_async.epics.areadetector.drivers.ad_base import \
+    start_acquiring_driver_and_ensure_status
 
-from ..drivers.vimba_driver import (
-    VimbaDriver,
-    VimbaExposeOutMode,
-    VimbaOnOff,
-    VimbaTriggerSource,
-)
 from ..utils import ImageMode, stop_busy_record
+from ._vimba_driver import (VimbaDriver, VimbaExposeOutMode, VimbaOnOff,
+                            VimbaTriggerSource)
 
 TRIGGER_MODE = {
     DetectorTrigger.internal: VimbaOnOff.off,
