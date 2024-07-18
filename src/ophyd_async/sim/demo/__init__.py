1320a83df54b7d43f89597e696b094b8f2847849
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-07-18 10:48:57 +0100

    Add sim pattern detector to the init

--- src/ophyd_async/sim/demo/__init__.py
+++ src/ophyd_async/sim/demo/__init__.py
@@ -1,5 +1,14 @@
+from ._pattern_detector import (DATA_PATH, SUM_PATH, PatternGenerator,
+                                SimPatternDetector, SimPatternDetectorControl,
+                                SimPatternDetectorWriter)
 from ._sim_motor import SimMotor
 
 __all__ = [
+    "DATA_PATH",
+    "SUM_PATH",
+    "PatternGenerator",
+    "SimPatternDetector",
+    "SimPatternDetectorControl",
+    "SimPatternDetectorWriter",
     "SimMotor",
 ]
