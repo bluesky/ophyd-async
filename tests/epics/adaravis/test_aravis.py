241b95d3b5c4a8f23b7a73190f3cf52b618cb8fa
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-07-18 12:14:40 +0100

    Update adaravis test imports and refactor

--- tests/epics/adaravis/test_aravis.py
+++ tests/epics/adaravis/test_aravis.py
@@ -3,64 +3,59 @@ import re
 import pytest
 from bluesky.run_engine import RunEngine
 
-from ophyd_async.core import (
-    DetectorTrigger,
-    DeviceCollector,
-    PathProvider,
-    TriggerInfo,
-    set_mock_value,
-)
-from ophyd_async.epics.areadetector.aravis import AravisDetector
+from ophyd_async.core import (DetectorTrigger, DeviceCollector, PathProvider,
+                              TriggerInfo, set_mock_value)
+from ophyd_async.epics import adaravis
 
 
 @pytest.fixture
-async def adaravis(
+async def test_adaravis(
     RE: RunEngine,
     static_path_provider: PathProvider,
-) -> AravisDetector:
+) -> adaravis.AravisDetector:
     async with DeviceCollector(mock=True):
-        adaravis = AravisDetector("ADARAVIS:", static_path_provider)
+        test_adaravis = adaravis.AravisDetector("ADARAVIS:", static_path_provider)
 
-    return adaravis
+    return test_adaravis
 
 
 @pytest.mark.parametrize("exposure_time", [0.0, 0.1, 1.0, 10.0, 100.0])
 async def test_deadtime_invariant_with_exposure_time(
     exposure_time: float,
-    adaravis: AravisDetector,
+    test_adaravis: adaravis.AravisDetector,
 ):
-    assert adaravis.controller.get_deadtime(exposure_time) == 1961e-6
+    assert test_adaravis.controller.get_deadtime(exposure_time) == 1961e-6
 
 
-async def test_trigger_source_set_to_gpio_line(adaravis: AravisDetector):
-    set_mock_value(adaravis.drv.trigger_source, "Freerun")
+async def test_trigger_source_set_to_gpio_line(test_adaravis: adaravis.AravisDetector):
+    set_mock_value(test_adaravis.drv.trigger_source, "Freerun")
 
     async def trigger_and_complete():
-        await adaravis.controller.arm(num=1, trigger=DetectorTrigger.edge_trigger)
+        await test_adaravis.controller.arm(num=1, trigger=DetectorTrigger.edge_trigger)
         # Prevent timeouts
-        set_mock_value(adaravis.drv.acquire, True)
+        set_mock_value(test_adaravis.drv.acquire, True)
 
     # Default TriggerSource
-    assert (await adaravis.drv.trigger_source.get_value()) == "Freerun"
-    adaravis.set_external_trigger_gpio(1)
+    assert (await test_adaravis.drv.trigger_source.get_value()) == "Freerun"
+    test_adaravis.set_external_trigger_gpio(1)
     # TriggerSource not changed by setting gpio
-    assert (await adaravis.drv.trigger_source.get_value()) == "Freerun"
+    assert (await test_adaravis.drv.trigger_source.get_value()) == "Freerun"
 
     await trigger_and_complete()
 
     # TriggerSource changes
-    assert (await adaravis.drv.trigger_source.get_value()) == "Line1"
+    assert (await test_adaravis.drv.trigger_source.get_value()) == "Line1"
 
-    adaravis.set_external_trigger_gpio(3)
+    test_adaravis.set_external_trigger_gpio(3)
     # TriggerSource not changed by setting gpio
     await trigger_and_complete()
-    assert (await adaravis.drv.trigger_source.get_value()) == "Line3"
+    assert (await test_adaravis.drv.trigger_source.get_value()) == "Line3"
 
 
-def test_gpio_pin_limited(adaravis: AravisDetector):
-    assert adaravis.get_external_trigger_gpio() == 1
-    adaravis.set_external_trigger_gpio(2)
-    assert adaravis.get_external_trigger_gpio() == 2
+def test_gpio_pin_limited(adaravis: adaravis.AravisDetector):
+    assert test_adaravis.get_external_trigger_gpio() == 1
+    test_adaravis.set_external_trigger_gpio(2)
+    assert test_adaravis.get_external_trigger_gpio() == 2
     with pytest.raises(
         ValueError,
         match=re.escape(
@@ -68,25 +63,25 @@ def test_gpio_pin_limited(adaravis: AravisDetector):
             "(1, 2, 3, 4) but was asked to use 55"
         ),
     ):
-        adaravis.set_external_trigger_gpio(55)  # type: ignore
+        test_adaravis.set_external_trigger_gpio(55)  # type: ignore
 
 
-async def test_hints_from_hdf_writer(adaravis: AravisDetector):
-    assert adaravis.hints == {"fields": ["adaravis"]}
+async def test_hints_from_hdf_writer(test_adaravis: adaravis.AravisDetector):
+    assert test_adaravis.hints == {"fields": ["adaravis"]}
 
 
-async def test_can_read(adaravis: AravisDetector):
+async def test_can_read(test_adaravis: adaravis.AravisDetector):
     # Standard detector can be used as Readable
-    assert (await adaravis.read()) == {}
+    assert (await test_adaravis.read()) == {}
 
 
-async def test_decribe_describes_writer_dataset(adaravis: AravisDetector):
-    set_mock_value(adaravis._writer.hdf.file_path_exists, True)
-    set_mock_value(adaravis._writer.hdf.capture, True)
+async def test_decribe_describes_writer_dataset(test_adaravis: adaravis.AravisDetector):
+    set_mock_value(test_adaravis._writer.hdf.file_path_exists, True)
+    set_mock_value(test_adaravis._writer.hdf.capture, True)
 
-    assert await adaravis.describe() == {}
-    await adaravis.stage()
-    assert await adaravis.describe() == {
+    assert await test_adaravis.describe() == {}
+    await test_adaravis.stage()
+    assert await test_adaravis.describe() == {
         "adaravis": {
             "source": "mock+ca://ADARAVIS:HDF1:FullFileName_RBV",
             "shape": (0, 0),
@@ -98,15 +93,15 @@ async def test_decribe_describes_writer_dataset(adaravis: AravisDetector):
 
 
 async def test_can_collect(
-    adaravis: AravisDetector, static_path_provider: PathProvider
+    test_adaravis: adaravis.AravisDetector, static_path_provider: PathProvider
 ):
     path_info = static_path_provider()
     full_file_name = path_info.root / path_info.resource_dir / "foo.h5"
-    set_mock_value(adaravis.hdf.full_file_name, str(full_file_name))
-    set_mock_value(adaravis._writer.hdf.file_path_exists, True)
-    set_mock_value(adaravis._writer.hdf.capture, True)
-    await adaravis.stage()
-    docs = [(name, doc) async for name, doc in adaravis.collect_asset_docs(1)]
+    set_mock_value(test_adaravis.hdf.full_file_name, str(full_file_name))
+    set_mock_value(test_adaravis._writer.hdf.file_path_exists, True)
+    set_mock_value(test_adaravis._writer.hdf.capture, True)
+    await test_adaravis.stage()
+    docs = [(name, doc) async for name, doc in test_adaravis.collect_asset_docs(1)]
     assert len(docs) == 2
     assert docs[0][0] == "stream_resource"
     stream_resource = docs[0][1]
@@ -125,12 +120,12 @@ async def test_can_collect(
     assert stream_datum["indices"] == {"start": 0, "stop": 1}
 
 
-async def test_can_decribe_collect(adaravis: AravisDetector):
-    set_mock_value(adaravis._writer.hdf.file_path_exists, True)
-    set_mock_value(adaravis._writer.hdf.capture, True)
-    assert (await adaravis.describe_collect()) == {}
-    await adaravis.stage()
-    assert (await adaravis.describe_collect()) == {
+async def test_can_decribe_collect(test_adaravis: adaravis.AravisDetector):
+    set_mock_value(test_adaravis._writer.hdf.file_path_exists, True)
+    set_mock_value(test_adaravis._writer.hdf.capture, True)
+    assert (await test_adaravis.describe_collect()) == {}
+    await test_adaravis.stage()
+    assert (await test_adaravis.describe_collect()) == {
         "adaravis": {
             "source": "mock+ca://ADARAVIS:HDF1:FullFileName_RBV",
             "shape": (0, 0),
@@ -141,13 +136,13 @@ async def test_can_decribe_collect(adaravis: AravisDetector):
     }
 
 
-async def test_unsupported_trigger_excepts(adaravis: AravisDetector):
+async def test_unsupported_trigger_excepts(adaravis: adaravis.AravisDetector):
     with pytest.raises(
         ValueError,
         # str(EnumClass.value) handling changed in Python 3.11
         match=r"AravisController only supports the following trigger types: .* but",
     ):
-        await adaravis.prepare(
+        await test_adaravis.prepare(
             TriggerInfo(
                 number=1,
                 trigger=DetectorTrigger.variable_gate,
