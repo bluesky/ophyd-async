7236b1a8ef6ef26a10881265748fdec67c1b2339
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-07-18 12:57:01 +0100

    Update advimba test imports and refactor

--- tests/epics/advimba/test_vimba.py
+++ tests/epics/advimba/test_vimba.py
@@ -7,79 +7,78 @@ from ophyd_async.core import (
     PathProvider,
     set_mock_value,
 )
-from ophyd_async.epics.areadetector.vimba import VimbaDetector
-
+from ophyd_async.epics import advimba 
 
 @pytest.fixture
-async def advimba(
+async def test_advimba(
     RE: RunEngine,
     static_path_provider: PathProvider,
-) -> VimbaDetector:
+) -> advimba.VimbaDetector:
     async with DeviceCollector(mock=True):
-        advimba = VimbaDetector("VIMBA:", static_path_provider)
+        test_advimba = advimba.VimbaDetector("VIMBA:", static_path_provider)
 
-    return advimba
+    return test_advimba
 
 
 async def test_get_deadtime(
-    advimba: VimbaDetector,
+    test_advimba: advimba.VimbaDetector,
 ):
     # Currently Vimba controller doesn't support getting deadtime.
-    assert advimba._controller.get_deadtime(0) == 0.001
+    assert test_advimba._controller.get_deadtime(0) == 0.001
 
 
-async def test_arming_trig_modes(advimba: VimbaDetector):
-    set_mock_value(advimba.drv.trig_source, "Freerun")
-    set_mock_value(advimba.drv.trigger_mode, "Off")
-    set_mock_value(advimba.drv.expose_mode, "Timed")
+async def test_arming_trig_modes(test_advimba: advimba.VimbaDetector):
+    set_mock_value(test_advimba.drv.trig_source, "Freerun")
+    set_mock_value(test_advimba.drv.trigger_mode, "Off")
+    set_mock_value(test_advimba.drv.expose_mode, "Timed")
 
     async def setup_trigger_mode(trig_mode: DetectorTrigger):
-        await advimba.controller.arm(num=1, trigger=trig_mode)
+        await test_advimba.controller.arm(num=1, trigger=trig_mode)
         # Prevent timeouts
-        set_mock_value(advimba.drv.acquire, True)
+        set_mock_value(test_advimba.drv.acquire, True)
 
     # Default TriggerSource
-    assert (await advimba.drv.trig_source.get_value()) == "Freerun"
-    assert (await advimba.drv.trigger_mode.get_value()) == "Off"
-    assert (await advimba.drv.expose_mode.get_value()) == "Timed"
+    assert (await test_advimba.drv.trig_source.get_value()) == "Freerun"
+    assert (await test_advimba.drv.trigger_mode.get_value()) == "Off"
+    assert (await test_advimba.drv.expose_mode.get_value()) == "Timed"
 
     await setup_trigger_mode(DetectorTrigger.edge_trigger)
-    assert (await advimba.drv.trig_source.get_value()) == "Line1"
-    assert (await advimba.drv.trigger_mode.get_value()) == "On"
-    assert (await advimba.drv.expose_mode.get_value()) == "Timed"
+    assert (await test_advimba.drv.trig_source.get_value()) == "Line1"
+    assert (await test_advimba.drv.trigger_mode.get_value()) == "On"
+    assert (await test_advimba.drv.expose_mode.get_value()) == "Timed"
 
     await setup_trigger_mode(DetectorTrigger.constant_gate)
-    assert (await advimba.drv.trig_source.get_value()) == "Line1"
-    assert (await advimba.drv.trigger_mode.get_value()) == "On"
-    assert (await advimba.drv.expose_mode.get_value()) == "TriggerWidth"
+    assert (await test_advimba.drv.trig_source.get_value()) == "Line1"
+    assert (await test_advimba.drv.trigger_mode.get_value()) == "On"
+    assert (await test_advimba.drv.expose_mode.get_value()) == "TriggerWidth"
 
     await setup_trigger_mode(DetectorTrigger.internal)
-    assert (await advimba.drv.trig_source.get_value()) == "Freerun"
-    assert (await advimba.drv.trigger_mode.get_value()) == "Off"
-    assert (await advimba.drv.expose_mode.get_value()) == "Timed"
+    assert (await test_advimba.drv.trig_source.get_value()) == "Freerun"
+    assert (await test_advimba.drv.trigger_mode.get_value()) == "Off"
+    assert (await test_advimba.drv.expose_mode.get_value()) == "Timed"
 
     await setup_trigger_mode(DetectorTrigger.variable_gate)
-    assert (await advimba.drv.trig_source.get_value()) == "Line1"
-    assert (await advimba.drv.trigger_mode.get_value()) == "On"
-    assert (await advimba.drv.expose_mode.get_value()) == "TriggerWidth"
+    assert (await test_advimba.drv.trig_source.get_value()) == "Line1"
+    assert (await test_advimba.drv.trigger_mode.get_value()) == "On"
+    assert (await test_advimba.drv.expose_mode.get_value()) == "TriggerWidth"
 
 
-async def test_hints_from_hdf_writer(advimba: VimbaDetector):
-    assert advimba.hints == {"fields": ["advimba"]}
+async def test_hints_from_hdf_writer(test_advimba: advimba.VimbaDetector):
+    assert test_advimba.hints == {"fields": ["advimba"]}
 
 
-async def test_can_read(advimba: VimbaDetector):
+async def test_can_read(test_advimba: advimba.VimbaDetector):
     # Standard detector can be used as Readable
-    assert (await advimba.read()) == {}
+    assert (await test_advimba.read()) == {}
 
 
-async def test_decribe_describes_writer_dataset(advimba: VimbaDetector):
-    set_mock_value(advimba._writer.hdf.file_path_exists, True)
-    set_mock_value(advimba._writer.hdf.capture, True)
+async def test_decribe_describes_writer_dataset(test_advimba: advimba.VimbaDetector):
+    set_mock_value(test_advimba._writer.hdf.file_path_exists, True)
+    set_mock_value(test_advimba._writer.hdf.capture, True)
 
-    assert await advimba.describe() == {}
-    await advimba.stage()
-    assert await advimba.describe() == {
+    assert await test_advimba.describe() == {}
+    await test_advimba.stage()
+    assert await test_advimba.describe() == {
         "advimba": {
             "source": "mock+ca://VIMBA:HDF1:FullFileName_RBV",
             "shape": (0, 0),
@@ -90,14 +89,14 @@ async def test_decribe_describes_writer_dataset(advimba: VimbaDetector):
     }
 
 
-async def test_can_collect(advimba: VimbaDetector, static_path_provider: PathProvider):
+async def test_can_collect(test_advimba: advimba.VimbaDetector, static_path_provider: PathProvider):
     path_info = static_path_provider()
     full_file_name = path_info.root / path_info.resource_dir / "foo.h5"
-    set_mock_value(advimba.hdf.full_file_name, str(full_file_name))
-    set_mock_value(advimba._writer.hdf.file_path_exists, True)
-    set_mock_value(advimba._writer.hdf.capture, True)
-    await advimba.stage()
-    docs = [(name, doc) async for name, doc in advimba.collect_asset_docs(1)]
+    set_mock_value(test_advimba.hdf.full_file_name, str(full_file_name))
+    set_mock_value(test_advimba._writer.hdf.file_path_exists, True)
+    set_mock_value(test_advimba._writer.hdf.capture, True)
+    await test_advimba.stage()
+    docs = [(name, doc) async for name, doc in test_advimba.collect_asset_docs(1)]
     assert len(docs) == 2
     assert docs[0][0] == "stream_resource"
     stream_resource = docs[0][1]
@@ -116,12 +115,12 @@ async def test_can_collect(advimba: VimbaDetector, static_path_provider: PathPro
     assert stream_datum["indices"] == {"start": 0, "stop": 1}
 
 
-async def test_can_decribe_collect(advimba: VimbaDetector):
-    set_mock_value(advimba._writer.hdf.file_path_exists, True)
-    set_mock_value(advimba._writer.hdf.capture, True)
-    assert (await advimba.describe_collect()) == {}
-    await advimba.stage()
-    assert (await advimba.describe_collect()) == {
+async def test_can_decribe_collect(test_advimba: advimba.VimbaDetector):
+    set_mock_value(test_advimba._writer.hdf.file_path_exists, True)
+    set_mock_value(test_advimba._writer.hdf.capture, True)
+    assert (await test_advimba.describe_collect()) == {}
+    await test_advimba.stage()
+    assert (await test_advimba.describe_collect()) == {
         "advimba": {
             "source": "mock+ca://VIMBA:HDF1:FullFileName_RBV",
             "shape": (0, 0),
