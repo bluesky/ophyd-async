50a49d2ccc800a00be922b4ce5e53257739429ac
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-07-18 14:42:38 +0100

    Fix patch path in adsimdetector

--- tests/epics/adsimdetector/test_adsim_controller.py
+++ tests/epics/adsimdetector/test_adsim_controller.py
@@ -16,7 +16,7 @@ async def ad(RE) -> adsimdetector.ADSimController:
 
 
 async def test_ad_controller(RE, ad: adsimdetector.ADSimController):
-    with patch("ophyd_async.core.signal.wait_for_value", return_value=None):
+    with patch("ophyd_async.core._signal.wait_for_value", return_value=None):
         await ad.arm(num=1)
 
     driver = ad.driver
