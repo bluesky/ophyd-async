790f4ef10cbd84be478be271e3d70f49128cf3f1
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-07-18 13:34:38 +0100

    Update docs example imports

--- docs/examples/foo_detector.py
+++ docs/examples/foo_detector.py
@@ -3,22 +3,14 @@ from typing import Optional
 
 from bluesky.protocols import HasHints, Hints
 
-from ophyd_async.core import DirectoryProvider
-from ophyd_async.core.async_status import AsyncStatus
-from ophyd_async.core.detector import DetectorControl, DetectorTrigger, StandardDetector
-from ophyd_async.epics.areadetector.drivers.ad_base import (
-    ADBase,
-    ADBaseShapeProvider,
-    start_acquiring_driver_and_ensure_status,
-)
-from ophyd_async.epics.areadetector.utils import ImageMode, ad_rw, stop_busy_record
-from ophyd_async.epics.areadetector.writers.hdf_writer import HDFWriter
-from ophyd_async.epics.areadetector.writers.nd_file_hdf import NDFileHDF
+from ophyd_async.core import (AsyncStatus, DetectorControl, DetectorTrigger,
+                              DirectoryProvider, StandardDetector)
+from ophyd_async.epics import adcore
 
 
-class FooDriver(ADBase):
+class FooDriver(adcore.ADBase):
     def __init__(self, prefix: str, name: str = "") -> None:
-        self.trigger_mode = ad_rw(str, prefix + "TriggerMode")
+        self.trigger_mode = adcore.ad_rw(str, prefix + "TriggerMode")
         super().__init__(prefix, name)
 
 
@@ -38,20 +30,20 @@ class FooController(DetectorControl):
     ) -> AsyncStatus:
         await asyncio.gather(
             self._drv.num_images.set(num),
-            self._drv.image_mode.set(ImageMode.multiple),
+            self._drv.image_mode.set(adcore.ImageMode.multiple),
             self._drv.trigger_mode.set(f"FOO{trigger}"),
         )
         if exposure is not None:
             await self._drv.acquire_time.set(exposure)
-        return await start_acquiring_driver_and_ensure_status(self._drv)
+        return await adcore.start_acquiring_driver_and_ensure_status(self._drv)
 
     async def disarm(self):
-        await stop_busy_record(self._drv.acquire, False, timeout=1)
+        await adcore.stop_busy_record(self._drv.acquire, False, timeout=1)
 
 
 class FooDetector(StandardDetector, HasHints):
     _controller: FooController
-    _writer: HDFWriter
+    _writer: adcore.HDFWriter
 
     def __init__(
         self,
@@ -63,15 +55,15 @@ class FooDetector(StandardDetector, HasHints):
     ):
         # Must be children to pick up connect
         self.drv = FooDriver(prefix + drv_suffix)
-        self.hdf = NDFileHDF(prefix + hdf_suffix)
+        self.hdf = adcore.NDFileHDF(prefix + hdf_suffix)
 
         super().__init__(
             FooController(self.drv),
-            HDFWriter(
+            adcore.HDFWriter(
                 self.hdf,
                 directory_provider,
                 lambda: self.name,
-                ADBaseShapeProvider(self.drv),
+                adcore.ADBaseShapeProvider(self.drv),
             ),
             config_sigs=(self.drv.acquire_time,),
             name=name,
