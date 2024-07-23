1b79837f4a45856031d424dd587e29c0777c429d
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-07-23 18:00:17 +0100

    Remove hard-dependency on p4p

--- src/ophyd_async/epics/signal/__init__.py
+++ src/ophyd_async/epics/signal/__init__.py
@@ -1,5 +1,8 @@
 from ._common import LimitPair, Limits, get_supported_values
-from ._p4p import PvaSignalBackend
+
+# Import from _epics_transport so that we get a NotImplemented shim in
+# the case where p4p is not available.
+from ._epics_transport import PvaSignalBackend
 from ._signal import (
     epics_signal_r,
     epics_signal_rw,
