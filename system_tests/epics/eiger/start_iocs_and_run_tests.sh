c19cccad75bef9cdf619e28e3edbd98d2300378b
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-08-27 17:39:40 +0100

    Clean up the containers after running them

--- system_tests/epics/eiger/start_iocs_and_run_tests.sh
+++ system_tests/epics/eiger/start_iocs_and_run_tests.sh
@@ -5,11 +5,21 @@ export odin_ioc=ODIN-$host
 
 mkdir /tmp/opi
 
-podman run --rm -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/eiger-fastcs:0.1.0beta5 ioc $eiger_ioc
+if command -v docker &> /dev/null; then
+    DOCKER_COMMAND=docker
+else
+    DOCKER_COMMAND=podman
+fi
 
-podman run --rm -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/odin-fastcs:0.2.0beta2 ioc $odin_ioc
+echo "Using $DOCKER_COMMAND" 
+
+$DOCKER_COMMAND run --rm --name=$eiger_ioc -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/eiger-fastcs:0.1.0beta5 ioc $eiger_ioc
+
+$DOCKER_COMMAND run --rm --name=$odin_ioc -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/odin-fastcs:0.2.0beta2 ioc $odin_ioc
 
 sleep 1
 
 pytest .
 
+podman kill $eiger_ioc
+podman kill $odin_ioc
