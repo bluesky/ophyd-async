76ad527d69e43819bb569afdf1b5077dac0e460b
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-09-02 14:21:54 +0100

    Fix commented out running of container

--- system_tests/epics/eiger/start_iocs_and_run_tests.sh
+++ system_tests/epics/eiger/start_iocs_and_run_tests.sh
@@ -13,13 +13,13 @@ fi
 
 echo "Using $DOCKER_COMMAND" 
 
-#$DOCKER_COMMAND run --rm --name=$eiger_ioc -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/eiger-fastcs:0.1.0beta5 ioc $eiger_ioc
+$DOCKER_COMMAND run --rm --name=$eiger_ioc -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/eiger-fastcs:0.1.0beta5 ioc $eiger_ioc
 
-#$DOCKER_COMMAND run --rm --name=$odin_ioc -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/odin-fastcs:0.2.0beta2 ioc $odin_ioc
+$DOCKER_COMMAND run --rm --name=$odin_ioc -dt --net=host -v /tmp/opi/:/epics/opi ghcr.io/diamondlightsource/odin-fastcs:0.2.0beta2 ioc $odin_ioc
 
 sleep 1
 
 pytest .
 
-#podman kill $eiger_ioc
-#podman kill $odin_ioc
+podman kill $eiger_ioc
+podman kill $odin_ioc
