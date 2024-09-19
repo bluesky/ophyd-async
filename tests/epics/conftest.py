b7acf9153c60f2bcdfb30fe96b4c80f709c34a6a
Jakub Wlodek <jwlodek@bnl.gov>
2024-09-19 18:04:45 -0400

    Overhaul sim detector tests to use ad standard det factory, improve factory with mock put callback

--- tests/epics/conftest.py
+++ tests/epics/conftest.py
@@ -1,3 +1,4 @@
+import os
 from collections.abc import Callable
 
 import pytest
@@ -5,7 +6,7 @@ from bluesky.run_engine import RunEngine
 
 from ophyd_async.core._detector import StandardDetector
 from ophyd_async.core._device import DeviceCollector
-from ophyd_async.core._mock_signal_utils import set_mock_value
+from ophyd_async.core._mock_signal_utils import callback_on_mock_put, set_mock_value
 
 
 @pytest.fixture
@@ -28,10 +29,27 @@ def ad_standard_det_factory(
                 name=f"test_ad{detector_name.lower()}{number}",
             )
 
+        def on_set_file_path_callback(value, **kwargs):
+            if os.path.exists(value):
+                set_mock_value(test_adstandard_det.hdf.file_path_exists, True)
+                set_mock_value(
+                    test_adstandard_det.hdf.full_file_name,
+                    f"{value}/{static_path_provider._filename_provider()}.h5",
+                )
+
+        callback_on_mock_put(
+            test_adstandard_det.hdf.file_path, on_set_file_path_callback
+        )
+
+        # Set some sensible defaults to mimic a real detector setup
+        set_mock_value(test_adstandard_det.drv.acquire_time, (number - 0.2))
+        set_mock_value(test_adstandard_det.drv.acquire_period, float(number))
+        set_mock_value(test_adstandard_det.hdf.capture, True)
+
         # Set number of frames per chunk and frame dimensions to something reasonable
         set_mock_value(test_adstandard_det.hdf.num_frames_chunks, 1)
-        set_mock_value(test_adstandard_det.drv.array_size_x, 10)
-        set_mock_value(test_adstandard_det.drv.array_size_y, 10)
+        set_mock_value(test_adstandard_det.drv.array_size_x, (9 + number))
+        set_mock_value(test_adstandard_det.drv.array_size_y, (9 + number))
 
         return test_adstandard_det
 
