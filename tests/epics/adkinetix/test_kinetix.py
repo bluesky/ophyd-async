e7cef5bebce8f079bf44fb842bb8a26bf2554fa3
Jakub Wlodek <jwlodek@bnl.gov>
2024-09-21 08:49:57 -0400

    Simplify ad standard det tests (#592)
    * Refactor ADPilatus tests to use the ad_standard_det_factory
    
    * Add default signals for sim detector config
    
    * Fix type annotation
    
    * Overhaul sim detector tests to use ad standard det factory, improve factory with mock put callback
    
    * Fix tests for remaining detectors to account for new changes to factory fixture
    
    * Make sure to include config_sigs passed to init
    
    * Add suggestions from review

--- tests/epics/adkinetix/test_kinetix.py
+++ tests/epics/adkinetix/test_kinetix.py
@@ -51,7 +51,7 @@ async def test_trigger_modes(test_adkinetix: adkinetix.KinetixDetector):
 
 
 async def test_hints_from_hdf_writer(test_adkinetix: adkinetix.KinetixDetector):
-    assert test_adkinetix.hints == {"fields": ["test_adkinetix1"]}
+    assert test_adkinetix.hints == {"fields": [test_adkinetix.name]}
 
 
 async def test_can_read(test_adkinetix: adkinetix.KinetixDetector):
@@ -62,9 +62,6 @@ async def test_can_read(test_adkinetix: adkinetix.KinetixDetector):
 async def test_decribe_describes_writer_dataset(
     test_adkinetix: adkinetix.KinetixDetector, one_shot_trigger_info: TriggerInfo
 ):
-    set_mock_value(test_adkinetix._writer.hdf.file_path_exists, True)
-    set_mock_value(test_adkinetix._writer.hdf.capture, True)
-
     assert await test_adkinetix.describe() == {}
     await test_adkinetix.stage()
     await test_adkinetix.prepare(one_shot_trigger_info)
@@ -85,10 +82,8 @@ async def test_can_collect(
     one_shot_trigger_info: TriggerInfo,
 ):
     path_info = static_path_provider()
-    full_file_name = path_info.directory_path / "foo.h5"
-    set_mock_value(test_adkinetix.hdf.full_file_name, str(full_file_name))
-    set_mock_value(test_adkinetix._writer.hdf.file_path_exists, True)
-    set_mock_value(test_adkinetix._writer.hdf.capture, True)
+    full_file_name = path_info.directory_path / f"{path_info.filename}.h5"
+
     await test_adkinetix.stage()
     await test_adkinetix.prepare(one_shot_trigger_info)
     docs = [(name, doc) async for name, doc in test_adkinetix.collect_asset_docs(1)]
@@ -114,8 +109,6 @@ async def test_can_collect(
 async def test_can_decribe_collect(
     test_adkinetix: adkinetix.KinetixDetector, one_shot_trigger_info: TriggerInfo
 ):
-    set_mock_value(test_adkinetix._writer.hdf.file_path_exists, True)
-    set_mock_value(test_adkinetix._writer.hdf.capture, True)
     assert (await test_adkinetix.describe_collect()) == {}
     await test_adkinetix.stage()
     await test_adkinetix.prepare(one_shot_trigger_info)
