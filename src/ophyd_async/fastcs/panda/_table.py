815989e89a224a13f1fff521468692a3a526dda3
Jakub Wlodek <jwlodek@bnl.gov>
2024-11-14 10:13:31 -0500

    Rename hdf5_type to data_type in DatasetTable

--- src/ophyd_async/fastcs/panda/_table.py
+++ src/ophyd_async/fastcs/panda/_table.py
@@ -13,7 +13,7 @@ class PandaHdf5DatasetType(StrictEnum):
 
 class DatasetTable(Table):
     name: Sequence[str]
-    hdf5_type: Sequence[PandaHdf5DatasetType]
+    data_type: Sequence[PandaHdf5DatasetType]
 
 
 class SeqTrigger(StrictEnum):
