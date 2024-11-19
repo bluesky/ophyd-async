666169b1fd3e5ce4602f997d83ccc08ff92268cc
Ollie Copping <oliver.copping@diamond.ac.uk>
2024-11-19 13:14:13 +0000

    Replace get_event_loop() in conftest.py as deprecated in Python 3.12

--- tests/conftest.py
+++ tests/conftest.py
@@ -114,7 +114,12 @@ def fail_test_on_unclosed_tasks(request: FixtureRequest):
     """
 
     fail_count = request.session.testsfailed
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+
+    asyncio.set_event_loop(loop)
     loop.set_debug(True)
 
     request.addfinalizer(
