978dcce2501519dc2a5ac24484016554a51d08aa
tacaswell <tacaswell@users.noreply.github.com>
2025-01-06 16:28:57 +0000

    deploy: 6c42a1facbbf95b0a70df019f8888476a6ac51e8

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.9.0a2.dev7+g3132d297',
+    VERSION: '0.9.0a2.dev8+g6c42a1fa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
