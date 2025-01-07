a1c47c98ccef4c160fd78b4b82189bbe4eb066dc
Niko Kivel <niko.kivel@lightsource.ca>
2025-01-07 16:33:28 +0000

    test raise for setup_ndstats

---
+++ tests/plan_stubs/test_setup.py
@@ -0,0 +1,26 @@
+import re
+
+import pytest
+
+from ophyd_async.plan_stubs import setup_ndstats_sum
+from ophyd_async.testing import ParentOfEverythingDevice
+
+
+@pytest.fixture
+async def parent_device() -> ParentOfEverythingDevice:
+    device = ParentOfEverythingDevice("parent")
+    await device.connect(mock=True)
+    return device
+
+
+def test_setup_ndstats_raises_type_error(RE, parent_device: ParentOfEverythingDevice):
+    detector = parent_device
+    detector_name = "faulty_det"
+    detector.set_name(detector_name)
+    with pytest.raises(
+        TypeError,
+        match=re.escape(
+            f"Expected {detector_name} to have 'hdf' attribute that is an NDFilHDFIO"
+        ),
+    ):
+        RE(setup_ndstats_sum(detector))
