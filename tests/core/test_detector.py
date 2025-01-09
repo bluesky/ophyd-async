1168c3e5d8cb3ac39584478199c76aafdaf731c4
Niko Kivel <niko.kivel@lightsource.ca>
2025-01-09 20:10:19 +0000

    assert removed from StandardDetector.complete added method to ensure existence of TriggerInfo.

--- tests/core/test_detector.py
+++ tests/core/test_detector.py
@@ -6,7 +6,7 @@ from ophyd_async.core import DetectorTrigger, StandardDetector, TriggerInfo
 
 
 @pytest.fixture
-def mock_controller():
+def mock_controller() -> MagicMock:
     controller = MagicMock()
     controller.get_deadtime = MagicMock(return_value=0.5)
     controller.prepare = AsyncMock(return_value={})
@@ -15,7 +15,7 @@ def mock_controller():
 
 
 @pytest.fixture
-def mock_writer():
+def mock_writer() -> MagicMock:
     writer = MagicMock()
     writer.open = AsyncMock(return_value={})
     writer.get_indices_written = AsyncMock(return_value=0)
@@ -23,7 +23,9 @@ def mock_writer():
 
 
 @pytest.fixture
-def standard_detector(mock_controller, mock_writer):
+def standard_detector(
+    mock_controller: MagicMock, mock_writer: MagicMock
+) -> StandardDetector:
     return StandardDetector(
         controller=mock_controller,
         writer=mock_writer,
@@ -32,7 +34,7 @@ def standard_detector(mock_controller, mock_writer):
     )
 
 
-async def test_prepare_internal_trigger(standard_detector):
+async def test_prepare_internal_trigger(standard_detector: StandardDetector) -> None:
     trigger_info = TriggerInfo(
         number_of_triggers=1,
         trigger=DetectorTrigger.INTERNAL,
@@ -44,11 +46,11 @@ async def test_prepare_internal_trigger(standard_detector):
     assert standard_detector._trigger_info == trigger_info
     assert standard_detector._number_of_triggers_iter is not None
     assert standard_detector._initial_frame == 0
-    standard_detector.writer.open.assert_called_once_with(trigger_info.multiplier)
-    standard_detector.controller.prepare.assert_called_once_with(trigger_info)
+    standard_detector.writer.open.assert_called_once_with(trigger_info.multiplier)  # type: ignore
+    standard_detector.controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
 
 
-async def test_prepare_external_trigger(standard_detector):
+async def test_prepare_external_trigger(standard_detector: StandardDetector) -> None:
     trigger_info = TriggerInfo(
         number_of_triggers=1,
         trigger=DetectorTrigger.EDGE_TRIGGER,
@@ -60,12 +62,14 @@ async def test_prepare_external_trigger(standard_detector):
     assert standard_detector._trigger_info == trigger_info
     assert standard_detector._number_of_triggers_iter is not None
     assert standard_detector._initial_frame == 0
-    standard_detector.writer.open.assert_called_once_with(trigger_info.multiplier)
-    standard_detector.controller.prepare.assert_called_once_with(trigger_info)
-    standard_detector.controller.arm.assert_called_once()
+    standard_detector.writer.open.assert_called_once_with(trigger_info.multiplier)  # type: ignore
+    standard_detector.controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
+    standard_detector.controller.arm.assert_called_once()  # type: ignore
 
 
-async def test_prepare_external_trigger_no_deadtime(standard_detector):
+async def test_prepare_external_trigger_no_deadtime(
+    standard_detector: StandardDetector,
+) -> None:
     trigger_info = TriggerInfo(
         number_of_triggers=1,
         trigger=DetectorTrigger.EDGE_TRIGGER,
@@ -80,7 +84,9 @@ async def test_prepare_external_trigger_no_deadtime(standard_detector):
         await standard_detector.prepare(trigger_info)
 
 
-async def test_prepare_external_trigger_insufficient_deadtime(standard_detector):
+async def test_prepare_external_trigger_insufficient_deadtime(
+    standard_detector: StandardDetector,
+) -> None:
     trigger_info = TriggerInfo(
         number_of_triggers=1,
         trigger=DetectorTrigger.EDGE_TRIGGER,
@@ -90,6 +96,26 @@ async def test_prepare_external_trigger_insufficient_deadtime(standard_detector)
     )
     with pytest.raises(
         ValueError,
-        match=r"Detector .* needs at least 0.5s deadtime, but trigger logic provides only",
+        match=r"Detector .* needs at least 0.5s deadtime, but trigger logic provides only",  # noqa: E501
     ):
         await standard_detector.prepare(trigger_info)
+
+
+def test_ensure_trigger_info_exists_success(
+    standard_detector: StandardDetector,
+) -> None:
+    trigger_info = TriggerInfo(number_of_triggers=1)
+    assert isinstance(
+        standard_detector.ensure_trigger_info_exists(trigger_info=trigger_info),
+        TriggerInfo,
+    )
+
+
+def test_ensure_trigger_info_exists_raises(standard_detector: StandardDetector) -> None:
+    with pytest.raises(
+        RuntimeError, match="Trigger info must be set before calling this method."
+    ):
+        assert isinstance(
+            standard_detector.ensure_trigger_info_exists(trigger_info=None),
+            TriggerInfo,
+        )
