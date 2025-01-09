4995cc086bbf5843bee92f2ce399f397c66da794
Niko Kivel <niko.kivel@lightsource.ca>
2025-01-09 22:17:15 +0000

    test case fixed for StandardDetector

--- tests/core/test_detector.py
+++ tests/core/test_detector.py
@@ -46,8 +46,8 @@ async def test_prepare_internal_trigger(standard_detector: StandardDetector) -> 
     assert standard_detector._trigger_info == trigger_info
     assert standard_detector._number_of_triggers_iter is not None
     assert standard_detector._initial_frame == 0
-    standard_detector.writer.open.assert_called_once_with(trigger_info.multiplier)  # type: ignore
-    standard_detector.controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
+    standard_detector._writer.open.assert_called_once_with(trigger_info.multiplier)  # type: ignore
+    standard_detector._controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
 
 
 async def test_prepare_external_trigger(standard_detector: StandardDetector) -> None:
@@ -62,9 +62,9 @@ async def test_prepare_external_trigger(standard_detector: StandardDetector) -> 
     assert standard_detector._trigger_info == trigger_info
     assert standard_detector._number_of_triggers_iter is not None
     assert standard_detector._initial_frame == 0
-    standard_detector.writer.open.assert_called_once_with(trigger_info.multiplier)  # type: ignore
-    standard_detector.controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
-    standard_detector.controller.arm.assert_called_once()  # type: ignore
+    standard_detector._writer.open.assert_called_once_with(trigger_info.multiplier)  # type: ignore
+    standard_detector._controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
+    standard_detector._controller.arm.assert_called_once()  # type: ignore
 
 
 async def test_prepare_external_trigger_no_deadtime(
