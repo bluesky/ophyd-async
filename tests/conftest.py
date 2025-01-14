4296ac6ad7ce8a7cade56ad82e8460825aa94cf1
Eva <evalott100@gmail.com>
2025-01-14 10:39:09 +0000

    skipped tango tests for windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -277,7 +277,7 @@ async def sim_detector(request: FixtureRequest):
 
 
 def pytest_collection_modifyitems(config, items):
-    tango_dir = "tests/tango"
+    tango_dir = os.path.join("tests", "tango")
 
     for item in items:
         if tango_dir in str(item.fspath):
@@ -285,6 +285,15 @@ def pytest_collection_modifyitems(config, items):
                 item.add_marker(
                     pytest.mark.skip(
                         reason="Tango is currently not supported on Python 3.12: https://github.com/bluesky/ophyd-async/issues/681"
+                    )
+                )
+            elif "win" in sys.platform:
+                item.add_marker(
+                    pytest.mark.skip(
+                        reason=(
+                            "Tango tests are currently not supported on Windows: "
+                            "https://github.com/bluesky/ophyd-async/issues/733"
+                        )
                     )
                 )
             else:
