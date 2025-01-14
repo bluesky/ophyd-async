24a384b22e855969a7080d1746615c3a7fe0906a
evalott100 <evalott100@users.noreply.github.com>
2025-01-14 14:50:53 +0000

    deploy: 4658c48295b001d29cfb4bfb4ea0f896be13d86b

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.9.0a2.dev11+g16b794a4',
+    VERSION: '0.9.0a2.dev12+g4658c482',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
