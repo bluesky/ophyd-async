0ae87b89ed9fb15e89e3d64f3f2265ab59af347e
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-01-20 13:45:13 +0000

    Add some arming chain functionality

--- src/ophyd_async/epics/eiger/_eiger.py
+++ src/ophyd_async/epics/eiger/_eiger.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from pydantic import Field
 
 from ophyd_async.core import AsyncStatus, PathProvider, StandardDetector, TriggerInfo
@@ -7,8 +9,18 @@ from ._eiger_io import EigerDriverIO
 from ._odin_io import Odin, OdinWriter
 
 
+@dataclass
+class EigerTimeouts:
+    stale_params_timeout: int = 60
+    general_status_timeout: int = 10
+    meta_file_ready_timeout: int = 30
+    all_frames_timeout: int = 120
+    arming_timeout: int = 60
+
+
 class EigerTriggerInfo(TriggerInfo):
     energy_ev: float = Field(gt=0)
+    exposure_time: float = Field()
 
 
 class EigerDetector(StandardDetector):
@@ -17,7 +29,7 @@ class EigerDetector(StandardDetector):
     """
 
     _controller: EigerController
-    _writer: Odin
+    _writer: OdinWriter
 
     def __init__(
         self,
@@ -29,7 +41,8 @@ class EigerDetector(StandardDetector):
     ):
         self.drv = EigerDriverIO(prefix + drv_suffix)
         self.odin = Odin(prefix + hdf_suffix + "FP:")
-
+        self.detector_params: EigerTriggerInfo | None = None
+        self.timeouts = EigerTimeouts()
         super().__init__(
             EigerController(self.drv),
             OdinWriter(path_provider, lambda: self.name, self.odin),
@@ -40,3 +53,49 @@ class EigerDetector(StandardDetector):
     async def prepare(self, value: EigerTriggerInfo) -> None:  # type: ignore
         await self._controller.set_energy(value.energy_ev)
         await super().prepare(value)
+
+    @AsyncStatus.wrap
+    async def stage(self) -> None:
+        assert self.detector_params
+        detector_params: EigerTriggerInfo = self.detector_params
+        await self._writer.close()
+        await self.set_detector_threshold(detector_params.energy_ev)
+        await self.set_cam_pvs()
+
+    @AsyncStatus.wrap
+    async def set_detector_threshold(self, energy: float, tolerance: float = 0.1):
+        current_energy = await self.drv.photon_energy.get_value()
+        if abs(current_energy - energy) > tolerance:
+            return self.drv.photon_energy.set(
+                energy, timeout=self.timeouts.general_status_timeout
+            )
+
+    @AsyncStatus.wrap
+    async def set_cam_pvs(self):
+        assert self.detector_params
+        await self.drv.acquire_time.set(
+            self.detector_params.exposure_time,
+            timeout=self.timeouts.general_status_timeout,
+        )
+        await self.drv.acquire_period.set(
+            self.detector_params.exposure_time,
+            timeout=self.timeouts.general_status_timeout,
+        )
+        await self.drv.num_exposures.set(
+            1, timeout=self.timeouts.general_status_timeout
+        )
+        # await self.drv.image_mode.set(
+        #     self.cam.ImageMode.MULTIPLE, timeout=self.timeouts.general_status_timeout
+        # )
+        # await self.drv.trigger_mode.set(
+        #     InternalEigerTriggerMode.EXTERNAL_SERIES.value,
+        #     timeout=self.timeouts.general_status_timeout,
+        # )
+
+    @AsyncStatus.wrap
+    async def set_odin_number_of_frame_chunks(self):
+        # assert self.detector_params is not None
+        # self._writer.num_frames_chunks.set(
+        #     1, timeout=self.timeouts.general_status_timeout
+        # )
+        pass
