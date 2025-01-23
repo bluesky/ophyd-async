c17a29e55c613c7246cd8cbc4ec3146e121be294
Jakub Wlodek <jwlodek@bnl.gov>
2025-01-23 12:33:27 -0500

    Port andor new ad format (#724)
    * ignore order of enum elements
    
    * Name signal to match expected PV name
    
    * Modify changes to integrate adandor2 to work with new AD structure
    
    * Fix test with unawaited coro
    
    * Potentially handle different enum values for Andor2
    
    * Use custom data type subset enum class
    
    * Fix incorrect dtype_numpy in tests
    
    * Make AD Image mode a subset enum, use that for andor
    
    * Run linter
    
    ---------
    
    Co-authored-by: Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>

---
+++ src/ophyd_async/epics/adandor/_andor_io.py
@@ -0,0 +1,36 @@
+from ophyd_async.core import StrictEnum, SubsetEnum
+from ophyd_async.epics.adcore import ADBaseIO
+from ophyd_async.epics.core import (
+    epics_signal_r,
+    epics_signal_rw,
+)
+
+
+class Andor2TriggerMode(StrictEnum):
+    INTERNAL = "Internal"
+    EXT_TRIGGER = "External"
+    EXT_START = "External Start"
+    EXT_EXPOSURE = "External Exposure"
+    EXT_FVP = "External FVP"
+    SOFTWARE = "Software"
+
+
+class Andor2DataType(SubsetEnum):
+    UINT16 = "UInt16"
+    UINT32 = "UInt32"
+    FLOAT32 = "Float32"
+    FLOAT64 = "Float64"
+
+
+class Andor2DriverIO(ADBaseIO):
+    """
+    Epics pv for andor model:DU897_BV as deployed on p99
+    """
+
+    def __init__(self, prefix: str, name: str = "") -> None:
+        super().__init__(prefix, name=name)
+        self.trigger_mode = epics_signal_rw(Andor2TriggerMode, prefix + "TriggerMode")
+        self.data_type = epics_signal_r(Andor2DataType, prefix + "DataType_RBV")
+        self.andor_accumulate_period = epics_signal_r(
+            float, prefix + "AndorAccumulatePeriod_RBV"
+        )
