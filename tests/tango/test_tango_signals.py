e056ae11d184cc3a336ef95a995a7bfae5230cab
James Souter <james.souter@diamond.ac.uk>
2025-02-05 09:28:50 +0000

    Use tango everything device more widely in tango tests

--- tests/tango/test_tango_signals.py
+++ tests/tango/test_tango_signals.py
@@ -13,13 +13,19 @@ from test_base_device import TestDevice
 
 from ophyd_async.core import SignalBackend, SignalR, SignalRW, SignalW, SignalX
 from ophyd_async.tango.core import (
+    TangoReadable,
     TangoSignalBackend,
     tango_signal_r,
     tango_signal_rw,
     tango_signal_w,
     tango_signal_x,
 )
-from ophyd_async.testing import MonitorQueue
+from ophyd_async.tango.testing._one_of_everything import (
+    AttributeData,
+    OneOfEverythingTangoDevice,
+    attribute_datas,
+)
+from ophyd_async.testing import MonitorQueue, assert_reading, assert_value
 from tango import AttrDataFormat, AttrWriteType, DevState
 from tango.asyncio import DeviceProxy
 from tango.asyncio_executor import set_global_executor
@@ -35,126 +41,6 @@ def __tango_signal_auto(*args, **kwargs):
 
 
 # --------------------------------------------------------------------
-"""
-Since TangoTest does not support EchoMode, we create our own Device.
-
-"""
-
-
-class TestEnum(IntEnum):
-    __test__ = False
-    A = 0
-    B = 1
-
-
-BASE_TYPES_SET = (
-    # type_name, tango_name,    py_type,    sample_values
-    ("boolean", "DevBoolean", bool, (True, False)),
-    ("short", "DevShort", int, (1, 2, 3, 4, 5)),
-    ("ushort", "DevUShort", int, (1, 2, 3, 4, 5)),
-    ("long", "DevLong", int, (1, 2, 3, 4, 5)),
-    ("ulong", "DevULong", int, (1, 2, 3, 4, 5)),
-    ("long64", "DevLong64", int, (1, 2, 3, 4, 5)),
-    ("char", "DevUChar", int, (1, 2, 3, 4, 5)),
-    ("float", "DevFloat", float, (1.1, 2.2, 3.3, 4.4, 5.5)),
-    ("double", "DevDouble", float, (1.1, 2.2, 3.3, 4.4, 5.5)),
-    # ("string", "DevString", str, ("aaa", "bbb", "ccc")),
-    # ("state", "DevState", DevState, (DevState.ON, DevState.MOVING, DevState.ALARM)),
-    # ("enum", "DevEnum", TestEnum, (TestEnum.A, TestEnum.B)),
-)
-
-
-@dataclass
-class AttributeData(Generic[T]):
-    pv: str
-    tango_type: str
-    d_format: AttrDataFormat
-    py_type: type[T]
-    initial_value: T
-    put_value: T
-
-
-ATTRIBUTES_SET: list[AttributeData] = []
-COMMANDS_SET: list[AttributeData] = []
-
-for type_name, tango_type_name, py_type, values in BASE_TYPES_SET:
-    # pytango test utils currently fail to handle bool pytest.approx
-    if type_name == "boolean":
-        continue
-    ATTRIBUTES_SET.extend(
-        [
-            AttributeData(
-                f"{type_name}_scalar_attr",
-                tango_type_name,
-                AttrDataFormat.SCALAR,
-                py_type,
-                choice(values),
-                choice(values),
-            ),
-            AttributeData(
-                f"{type_name}_spectrum_attr",
-                tango_type_name,
-                AttrDataFormat.SPECTRUM,
-                npt.NDArray[py_type],
-                np.array(
-                    [choice(values), choice(values), choice(values)], dtype=py_type
-                ),
-                np.array(
-                    [choice(values), choice(values), choice(values)], dtype=py_type
-                ),
-            ),
-            AttributeData(
-                f"{type_name}_image_attr",
-                tango_type_name,
-                AttrDataFormat.IMAGE,
-                npt.NDArray[py_type],
-                np.array(
-                    [
-                        [choice(values), choice(values), choice(values)],
-                        [choice(values), choice(values), choice(values)],
-                    ],
-                    dtype=py_type,
-                ),
-                np.array(
-                    [
-                        [choice(values), choice(values), choice(values)],
-                        [choice(values), choice(values), choice(values)],
-                    ],
-                    dtype=py_type,
-                ),
-            ),
-        ]
-    )
-
-    if tango_type_name == "DevUChar":
-        continue
-    else:
-        COMMANDS_SET.append(
-            AttributeData(
-                f"{type_name}_scalar_cmd",
-                tango_type_name,
-                AttrDataFormat.SCALAR,
-                py_type,
-                choice(values),
-                choice(values),
-            )
-        )
-        if tango_type_name in ["DevState", "DevEnum"]:
-            continue
-        else:
-            COMMANDS_SET.append(
-                AttributeData(
-                    f"{type_name}_spectrum_cmd",
-                    tango_type_name,
-                    AttrDataFormat.SPECTRUM,
-                    npt.NDArray[py_type],
-                    [choice(values), choice(values), choice(values)],
-                    [choice(values), choice(values), choice(values)],
-                )
-            )
-
-
-# --------------------------------------------------------------------
 #               TestDevice
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
@@ -164,57 +50,6 @@ def tango_test_device():
         [{"class": TestDevice, "devices": [{"name": "test/device/1"}]}], process=True
     ) as context:
         yield context.get_device_access("test/device/1")
-
-
-# --------------------------------------------------------------------
-#               Echo device
-# --------------------------------------------------------------------
-class EchoDevice(Device):
-    attr_values = {}
-
-    def initialize_dynamic_attributes(self):
-        for attr_data in ATTRIBUTES_SET:
-            attr = attribute(
-                name=attr_data.pv,
-                dtype=attr_data.tango_type,
-                dformat=attr_data.d_format,
-                access=AttrWriteType.READ_WRITE,
-                fget=self.read,
-                fset=self.write,
-                max_dim_x=3,
-                max_dim_y=2,
-                enum_labels=[member.name for member in TestEnum],
-            )
-            self.add_attribute(attr)
-            self.set_change_event(attr_data.pv, True, False)
-
-        for cmd_data in COMMANDS_SET:
-            cmd = command(
-                f=getattr(self, cmd_data.pv),
-                dtype_in=cmd_data.tango_type,
-                dformat_in=cmd_data.d_format,
-                dtype_out=cmd_data.tango_type,
-                dformat_out=cmd_data.d_format,
-            )
-            self.add_command(cmd)
-
-    def read(self, attr):
-        attr.set_value(self.attr_values[attr.get_name()])
-
-    def write(self, attr):
-        new_value = attr.get_write_value()
-        self.attr_values[attr.get_name()] = new_value
-        self.push_change_event(attr.get_name(), new_value)
-
-    echo_command_code = textwrap.dedent(
-        """\
-            def echo_command(self, arg):
-                return arg
-            """
-    )
-
-    for cmd_data in COMMANDS_SET:
-        exec(echo_command_code.replace("echo_command", cmd_data.pv))
 
 
 # --------------------------------------------------------------------
@@ -234,7 +69,8 @@ def assert_enum(initial_value, readout_value):
 @pytest.fixture(scope="module")
 def echo_device():
     with MultiDeviceTestContext(
-        [{"class": EchoDevice, "devices": [{"name": "test/device/1"}]}], process=True
+        [{"class": OneOfEverythingTangoDevice, "devices": [{"name": "test/device/1"}]}],
+        process=True,
     ) as context:
         yield context.get_device_access("test/device/1")
 
@@ -320,18 +156,26 @@ async def assert_monitor_then_put(
 @pytest.mark.asyncio
 async def test_backend_get_put_monitor_attr(echo_device: str):
     try:
-        for attr_data in ATTRIBUTES_SET:
+        for attr_data in attribute_datas:
+            if (
+                "enum" in attr_data.name
+                or "state" in attr_data.name
+                or attr_data.dtype is None
+            ):
+                print("skipping for now", attr_data.name)
+                continue
             # Set a timeout for the operation to prevent it from running indefinitely
+            # TODO: conflict between actual value and max_dim_x...
             await asyncio.wait_for(
                 assert_monitor_then_put(
                     echo_device,
-                    attr_data.pv,
+                    attr_data.name,
                     attr_data.initial_value,
-                    attr_data.put_value,
+                    attr_data.random_value(),
                     get_test_descriptor(
-                        attr_data.py_type, attr_data.initial_value, False
+                        attr_data.dtype, attr_data.initial_value, False
                     ),
-                    attr_data.py_type,
+                    attr_data.dtype,
                 ),
                 timeout=100,  # Timeout in seconds
             )
@@ -373,15 +217,23 @@ async def assert_put_read(
 async def test_backend_get_put_monitor_cmd(
     echo_device: str,
 ):
-    for cmd_data in COMMANDS_SET:
+    for cmd_data in attribute_datas:
+        if (
+            cmd_data.dformat == AttrDataFormat.IMAGE
+            or cmd_data.tango_type == "DevUChar"
+        ):
+            continue
+        if "enum" in cmd_data.name or "state" in cmd_data.name:
+            print("skipping for now", cmd_data.name)
+            continue
         # With the given datatype, check we have the correct initial value
         # and putting works
-        descriptor = get_test_descriptor(cmd_data.py_type, cmd_data.initial_value, True)
-        await assert_put_read(
-            echo_device, cmd_data.pv, cmd_data.put_value, descriptor, cmd_data.py_type
-        )
+        put_value = cmd_data.random_value()
+        name = f"{cmd_data.name}_cmd"
+        descriptor = get_test_descriptor(cmd_data.dtype, cmd_data.initial_value, True)
+        await assert_put_read(echo_device, name, put_value, descriptor, cmd_data.dtype)
         # # With guessed datatype, check we can set it back to the initial value
-        await assert_put_read(echo_device, cmd_data.pv, cmd_data.put_value, descriptor)
+        await assert_put_read(echo_device, name, put_value, descriptor)
         tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
         await asyncio.gather(*tasks)
 
@@ -394,19 +246,23 @@ async def test_tango_signal_r(
     timeout = 0.2
     for use_proxy in [True, False]:
         proxy = await DeviceProxy(echo_device) if use_proxy else None
-        for attr_data in ATTRIBUTES_SET:
-            await prepare_device(echo_device, attr_data.pv, attr_data.initial_value)
-            source = echo_device + "/" + attr_data.pv
+        print("Remember to reset_values between tests!!")
+        for attr_data in attribute_datas:
+            if "enum" in attr_data.name or "state" in attr_data.name:
+                print("skipping for now", attr_data.name)
+                continue
+            source = echo_device + "/" + attr_data.name
             signal = tango_signal_r(
-                datatype=attr_data.py_type,
+                datatype=attr_data.dtype,
                 read_trl=source,
                 device_proxy=proxy,
                 timeout=timeout,
                 name="test_signal",
             )
             await signal.connect()
-            reading = await signal.read()
-            assert_close(reading["test_signal"]["value"], attr_data.initial_value)
+            await assert_reading(
+                signal, {"test_signal": {"value": attr_data.initial_value}}
+            )
 
 
 # --------------------------------------------------------------------
@@ -417,31 +273,34 @@ async def test_tango_signal_w(
     for use_proxy in [True, False]:
         proxy = await DeviceProxy(echo_device) if use_proxy else None
         timeout = 0.2
-        for attr_data in ATTRIBUTES_SET:
-            await prepare_device(echo_device, attr_data.pv, attr_data.initial_value)
-            source = echo_device + "/" + attr_data.pv
-
+        for attr_data in attribute_datas:
+            if "enum" in attr_data.name or "state" in attr_data.name:
+                print("skipping for now", attr_data.name)
+                continue
+            await prepare_device(echo_device, attr_data.name, attr_data.initial_value)
+            source = echo_device + "/" + attr_data.name
             signal = tango_signal_w(
-                datatype=attr_data.py_type,
+                datatype=attr_data.dtype,
                 write_trl=source,
                 device_proxy=proxy,
                 timeout=timeout,
                 name="test_signal",
             )
+            put_value = attr_data.random_value()
             await signal.connect()
-            status = signal.set(attr_data.put_value, wait=True, timeout=timeout)
+            status = signal.set(put_value, wait=True, timeout=timeout)
             await status
             assert status.done is True and status.success is True
 
-            status = signal.set(attr_data.put_value, wait=False, timeout=timeout)
+            status = signal.set(put_value, wait=False, timeout=timeout)
             await status
             assert status.done is True and status.success is True
 
-            status = signal.set(attr_data.put_value, wait=True)
+            status = signal.set(put_value, wait=True)
             await status
             assert status.done is True and status.success is True
 
-            status = signal.set(attr_data.put_value, wait=False)
+            status = signal.set(put_value, wait=False)
             await status
             assert status.done is True and status.success is True
 
@@ -454,12 +313,16 @@ async def test_tango_signal_rw(
     timeout = 0.2
     for use_proxy in [True, False]:
         proxy = await DeviceProxy(echo_device) if use_proxy else None
-        for attr_data in ATTRIBUTES_SET:
-            await prepare_device(echo_device, attr_data.pv, attr_data.initial_value)
-            source = echo_device + "/" + attr_data.pv
+        for attr_data in attribute_datas:
+            if "enum" in attr_data.name or "state" in attr_data.name:
+                print("skipping for now", attr_data.name)
+                continue
+            put_value = attr_data.random_value()
+            await prepare_device(echo_device, attr_data.name, attr_data.initial_value)
+            source = echo_device + "/" + attr_data.name
 
             signal = tango_signal_rw(
-                datatype=attr_data.py_type,
+                datatype=attr_data.dtype,
                 read_trl=source,
                 write_trl=source,
                 device_proxy=proxy,
@@ -468,11 +331,13 @@ async def test_tango_signal_rw(
             )
             await signal.connect()
             reading = await signal.read()
+            # TODO remove this print statement
+            print("assert_close seems to work here but not in the other tests??")
             assert_close(reading["test_signal"]["value"], attr_data.initial_value)
-            await signal.set(attr_data.put_value)
+            await signal.set(put_value)
             location = await signal.locate()
-            assert_close(location["setpoint"], attr_data.put_value)
-            assert_close(location["readback"], attr_data.put_value)
+            assert_close(location["setpoint"], put_value)
+            assert_close(location["readback"], put_value)
 
 
 # --------------------------------------------------------------------
@@ -502,9 +367,9 @@ async def test_tango_signal_auto_attrs(
     timeout = 0.2
     for use_proxy in [True, False]:
         proxy = await DeviceProxy(echo_device) if use_proxy else None
-        for attr_data in ATTRIBUTES_SET:
-            await prepare_device(echo_device, attr_data.pv, attr_data.initial_value)
-            source = echo_device + "/" + attr_data.pv
+        for attr_data in attribute_datas:
+            await prepare_device(echo_device, attr_data.name, attr_data.initial_value)
+            source = echo_device + "/" + attr_data.name
 
             async def _test_signal(dtype, proxy, source, initial_value, put_value):
                 signal = await __tango_signal_auto(
@@ -557,8 +422,8 @@ async def test_tango_signal_auto_cmds(
     timeout = 0.2
     proxy = await DeviceProxy(echo_device) if use_proxy else None
 
-    for cmd_data in COMMANDS_SET:
-        source = echo_device + "/" + cmd_data.pv
+    for cmd_data in attribute_datas:
+        source = echo_device + "/" + cmd_data.name + "_cmd"
 
         async def _test_signal(dtype, proxy, source, put_value):
             signal = await __tango_signal_auto(
@@ -574,7 +439,6 @@ async def test_tango_signal_auto_cmds(
             assert signal
             reading = await signal.read()
             assert reading["test_signal"]["value"] is None
-
             await signal.set(put_value, wait=True, timeout=0.1)
             reading = await signal.read()
             value = reading["test_signal"]["value"]
@@ -582,8 +446,8 @@ async def test_tango_signal_auto_cmds(
                 value = value.tolist()
             assert_close(value, put_value)
 
-        dtype = cmd_data.py_type if use_dtype else None
-        await _test_signal(dtype, proxy, source, cmd_data.put_value)
+        dtype = cmd_data.dtype if use_dtype else None
+        await _test_signal(dtype, proxy, source, cmd_data.random_value())
 
 
 # --------------------------------------------------------------------
