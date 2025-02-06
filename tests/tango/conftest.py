2a3689e3434510bfbab9cd86a6a570d3f91945af
James Souter <james.souter@diamond.ac.uk>
2025-02-06 09:42:45 +0000

    add outline for tango test context in subprocess in tests/tango/conftest.py

--- tests/tango/conftest.py
+++ tests/tango/conftest.py
@@ -1,5 +1,9 @@
 import os
+import pickle
+import socket
+import subprocess
 import sys
+from pathlib import Path
 
 import pytest
 
@@ -32,3 +36,30 @@ def pytest_collection_modifyitems(config, items):
                 )
             else:
                 item.add_marker(pytest.mark.forked)
+
+
+class TangoSubprocessHelper:
+    def __init__(self, args):
+        self._args = args
+
+    def __enter__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(("", 0))
+        port = str(self.sock.getsockname()[1])
+        self.sock.listen(1)
+        subprocess_path = str(Path(__file__).parent / "context_subprocess._py")
+        self.process = subprocess.Popen([sys.executable, subprocess_path, port])
+        self.conn, _ = self.sock.accept()
+        self.conn.send(pickle.dumps(self._args))
+        self.trls = pickle.loads(self.conn.recv(1024))
+        return self
+
+    def __exit__(self, A, B, C):
+        self.conn.close()
+        self.sock.close()
+        self.process.communicate()
+
+
+@pytest.fixture(scope="module")
+def subprocess_helper():
+    return TangoSubprocessHelper
