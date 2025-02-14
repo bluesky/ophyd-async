4c34aa1b2f0257e70d18cc75209620eafe598f1f
Tom Cobb <tom.cobb@diamond.ac.uk>
2025-02-14 13:52:06 +0000

    Add flyable support to SimMotor

--- src/ophyd_async/sim/_motor.py
+++ src/ophyd_async/sim/_motor.py
@@ -4,6 +4,7 @@ import time
 
 import numpy as np
 from bluesky.protocols import Movable, Stoppable
+from pydantic import BaseModel, ConfigDict, Field
 
 from ophyd_async.core import (
     AsyncStatus,
@@ -15,6 +16,37 @@ from ophyd_async.core import (
     soft_signal_rw,
 )
 from ophyd_async.core import StandardReadableFormat as Format
+
+
+class FlySimMotorInfo(BaseModel):
+    """Minimal set of information required to fly a motor:"""
+
+    model_config = ConfigDict(frozen=True)
+
+    cv_start: float
+    """Absolute position of the motor once it finishes accelerating to desired
+    velocity, in motor EGUs"""
+
+    cv_end: float
+    """Absolute position of the motor once it begins decelerating from desired
+    velocity, in EGUs"""
+
+    cv_time: float = Field(gt=0)
+    """Time taken for the motor to get from start_position to end_position, excluding
+    run-up and run-down, in seconds."""
+
+    @property
+    def velocity(self) -> float:
+        """Calculate the velocity of the constant velocity phase."""
+        return (self.cv_end - self.cv_start) / self.cv_time
+
+    def start_position(self, acceleration_time: float) -> float:
+        """Calculate the start position with run-up distance added on."""
+        return self.cv_start - acceleration_time * self.velocity / 2
+
+    def end_position(self, acceleration_time: float) -> float:
+        """Calculate the end position with run-down distance added on."""
+        return self.cv_end + acceleration_time * self.velocity / 2
 
 
 class SimMotor(StandardReadable, Movable, Stoppable):
@@ -33,12 +65,17 @@ class SimMotor(StandardReadable, Movable, Stoppable):
             )
         with self.add_children_as_readables(Format.CONFIG_SIGNAL):
             self.velocity = soft_signal_rw(float, 0 if instant else 1.0)
+            self.acceleration_time = soft_signal_rw(float, 0.5)
             self.units = soft_signal_rw(str, "mm")
         self.user_setpoint = soft_signal_rw(float, 0)
 
         # Whether set() should complete successfully or not
         self._set_success = True
         self._move_status: AsyncStatus | None = None
+        # Stored in prepare
+        self._fly_info: FlySimMotorInfo | None = None
+        # Set on kickoff(), complete when motor reaches end position
+        self._fly_status: WatchableAsyncStatus | None = None
 
         super().__init__(name=name)
 
@@ -47,27 +84,80 @@ class SimMotor(StandardReadable, Movable, Stoppable):
         # Readback should be named the same as its parent in read()
         self.user_readback.set_name(name)
 
-    async def _move(self, old_position: float, new_position: float, move_time: float):
+    @AsyncStatus.wrap
+    async def prepare(self, value: FlySimMotorInfo):
+        """Calculate required velocity and run-up distance, then if motor limits aren't
+        breached, move to start position minus run-up distance"""
+        self._fly_info = value
+        # Move to start as fast as we can
+        await self.velocity.set(0)
+        await self.set(value.start_position(await self.acceleration_time.get_value()))
+        # Set the velocity for the actual move
+        await self.velocity.set(value.velocity)
+
+    @AsyncStatus.wrap
+    async def kickoff(self):
+        """Begin moving motor from prepared position to final position."""
+        if not self._fly_info:
+            msg = "Motor must be prepared before attempting to kickoff"
+            raise RuntimeError(msg)
+        acceleration_time = await self.acceleration_time.get_value()
+        self._fly_status = self.set(self._fly_info.end_position(acceleration_time))
+
+    def complete(self) -> WatchableAsyncStatus:
+        """Mark as complete once motor reaches completed position."""
+        if not self._fly_status:
+            msg = "kickoff not called"
+            raise RuntimeError(msg)
+        return self._fly_status
+
+    async def _move(self, old_position: float, new_position: float, velocity: float):
         start = time.monotonic()
+        acceleration_time = abs(await self.acceleration_time.get_value())
+        velocity = abs(velocity) * np.sign(new_position - old_position)
+        # The total distance to move, and the part of it that is ramp up and down
+        total_distance = new_position - old_position
+        ramp_distance = velocity * acceleration_time / 2
+        if abs(ramp_distance * 2) >= abs(total_distance):
+            # All time is ramp up and down
+            max_velocity = total_distance / ramp_distance * 2 * velocity
+            ramp_time = ramp_distance / max_velocity
+            move_time = 2 * ramp_time
+        else:
+            # Middle segments of constant velocity
+            max_velocity = velocity
+            ramp_time = acceleration_time
+            move_time = ramp_time * 2 + (total_distance - ramp_distance * 2) / velocity
         # Make an array of relative update times at 10Hz intervals
-        update_times = np.arange(0.1, move_time, 0.1)
+        update_times = list(np.arange(0.1, move_time, 0.1, dtype=float))
         # With the end position appended
-        update_times = np.concatenate((update_times, [move_time]))
-        # Interpolate the [old, new] position array with those update times
-        new_positions = np.interp(
-            update_times, [0, move_time], [old_position, new_position]
-        )
-        for update_time, new_position in zip(update_times, new_positions, strict=True):
+        if update_times and np.isclose(update_times[-1], move_time):
+            update_times[-1] = move_time
+        else:
+            update_times.append(move_time)
+        # Iterate through the update times, calculating new position for each
+        for t in update_times:
+            if t <= ramp_time:
+                # Ramp up phase, calculate area under the ramp up triangle
+                current_velocity = t / ramp_time * max_velocity
+                position = old_position + current_velocity * t / 2
+            elif t >= move_time - ramp_time:
+                # Ramp down phase, subtract area under the ramp down triangle
+                time_left = move_time - t
+                current_velocity = time_left / ramp_time * max_velocity
+                position = new_position - current_velocity * time_left / 2
+            else:
+                # Constant velocity phase
+                position = old_position + ramp_distance + (t - ramp_time) * max_velocity
             # Calculate how long to wait to get there
             relative_time = time.monotonic() - start
-            await asyncio.sleep(update_time - relative_time)
+            await asyncio.sleep(t - relative_time)
             # Update the readback position
-            self._user_readback_set(new_position)
+            self._user_readback_set(position)
 
     @WatchableAsyncStatus.wrap
     async def set(self, value: float):
         """Asynchronously move the motor to a new position."""
-        start = time.time()
         new_position = value
         # Make sure any existing move tasks are stopped
         await self.stop()
@@ -80,9 +170,8 @@ class SimMotor(StandardReadable, Movable, Stoppable):
         if velocity == 0:
             self._user_readback_set(new_position)
         else:
-            move_time = abs(new_position - old_position) / velocity
             self._move_status = AsyncStatus(
-                self._move(old_position, new_position, move_time)
+                self._move(old_position, new_position, velocity)
             )
             # If stop is called then this will raise a CancelledError, ignore it
             with contextlib.suppress(asyncio.CancelledError):
@@ -96,7 +185,6 @@ class SimMotor(StandardReadable, Movable, Stoppable):
                         name=self.name,
                         unit=units,
                     )
-        print("Move took", time.time() - start)
         if not self._set_success:
             raise RuntimeError("Motor was stopped")
 
