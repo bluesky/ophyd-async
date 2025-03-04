8055ea84eb2dccec22366581a97c0115d7305e6b
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-03-04 12:21:38 +0000

    Move tests to fastcs eiger module

---
+++ src/ophyd_async/fastcs/eiger/_eiger.py
@@ -0,0 +1,40 @@
+from pydantic import Field
+
+from ophyd_async.core import AsyncStatus, PathProvider, StandardDetector, TriggerInfo
+from ophyd_async.fastcs.odin._odin_io import OdinHdfIO, OdinWriter
+
+from ._eiger_controller import EigerController
+from ._eiger_io import EigerDriverIO
+
+
+class EigerTriggerInfo(TriggerInfo):
+    energy_ev: float = Field(gt=0)
+
+
+class EigerDetector(StandardDetector):
+    """Ophyd-async implementation of an Eiger Detector."""
+
+    _controller: EigerController
+    _writer: OdinWriter
+
+    def __init__(
+        self,
+        prefix: str,
+        path_provider: PathProvider,
+        drv_suffix="-EA-EIGER-01:",
+        hdf_suffix="-EA-ODIN-01:",
+        name="",
+    ):
+        self.drv = EigerDriverIO(prefix + drv_suffix)
+        self.odin = OdinHdfIO(prefix + hdf_suffix + "FP:")
+
+        super().__init__(
+            EigerController(self.drv),
+            OdinWriter(path_provider, lambda: self.name, self.odin),
+            name=name,
+        )
+
+    @AsyncStatus.wrap
+    async def prepare(self, value: EigerTriggerInfo) -> None:  # type: ignore
+        await self._controller.set_energy(value.energy_ev)
+        await super().prepare(value)
---
+++ tests/fastcs/eiger/test_eiger_detector.py
@@ -0,0 +1,35 @@
+from unittest.mock import ANY, AsyncMock, MagicMock
+
+import pytest
+
+from ophyd_async.core import DetectorTrigger, init_devices
+from ophyd_async.fastcs.eiger import EigerDetector, EigerTriggerInfo
+from ophyd_async.testing import get_mock_put
+
+
+@pytest.fixture
+def detector(RE):
+    with init_devices(mock=True):
+        detector = EigerDetector("BL03I", MagicMock())
+    return detector
+
+
+def test_when_detector_initialised_then_driver_and_odin_have_expected_prefixes(
+    detector,
+):
+    assert "BL03I-EA-EIGER-01:" in detector.drv.arm.source
+    assert "BL03I-EA-ODIN-01:FP:" in detector.odin.acquisition_id.source
+
+
+async def test_when_prepared_with_energy_then_energy_set_on_detector(detector):
+    detector._controller.arm = AsyncMock()
+    await detector.prepare(
+        EigerTriggerInfo(
+            frame_timeout=None,
+            number_of_triggers=1,
+            trigger=DetectorTrigger.INTERNAL,
+            energy_ev=10000,
+        )
+    )
+
+    get_mock_put(detector.drv.photon_energy).assert_called_once_with(10000, wait=ANY)
