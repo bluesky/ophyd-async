343ec32bafc65063b7980df335bc87e334375989
Shihab S <162436767+shihab-dls@users.noreply.github.com>
2025-03-07 14:37:58 +0000

    Update ADSimDetector to match ADAravis (#808)
    * Add to doc strings and seperate logic
    
    * Add to doc strings and seperate logic
    
    * Change module name back to adsimdetector
    
    * Change module name back to adsimdetector

--- src/ophyd_async/epics/adsimdetector/__init__.py
+++ src/ophyd_async/epics/adsimdetector/__init__.py
@@ -1,4 +1,11 @@
-from ._sim import SimController, SimDetector, SimDriverIO
+"""Support for the ADAravis areaDetector driver.
+
+https://github.com/areaDetector/ADSimDetector
+"""
+
+from ._sim import SimDetector
+from ._sim_controller import SimController
+from ._sim_io import SimDriverIO
 
 __all__ = [
     "SimDriverIO",
