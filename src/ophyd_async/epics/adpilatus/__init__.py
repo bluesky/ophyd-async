b7bc124d0a4cf415b9fcd009d77e9aaef0f4eeb1
Paula Martin Gomez <141016361+paula-mg@users.noreply.github.com>
2025-03-13 13:05:40 +0000

    Update ADPilatus to match ADAravis (#810)
    * Change PilatusDriverIO class
    
    * Add link to Github documentation
    
    * Add armed field
    
    * Add _RBV to PVSuffix for SignalR
    
    * Fix variable declaration in the IO class
    
    * Update PilatusController description
    
    * Update PilatusDriverIO desc to include link to docs
    
    Co-authored-by: Shihab S <162436767+shihab-dls@users.noreply.github.com>
    
    * Update docs to replace ADAravis for ADPilatus
    
    * Add brief description of PilatusDriver
    
    Co-authored-by: Shihab S <162436767+shihab-dls@users.noreply.github.com>
    
    * Fix lint errors
    
    ---------
    
    Co-authored-by: Shihab S <162436767+shihab-dls@users.noreply.github.com>

--- src/ophyd_async/epics/adpilatus/__init__.py
+++ src/ophyd_async/epics/adpilatus/__init__.py
@@ -1,3 +1,8 @@
+"""Support for the ADPilatus areaDetector driver.
+
+https://github.com/areaDetector/ADPilatus
+"""
+
 from ._pilatus import PilatusDetector
 from ._pilatus_controller import PilatusController, PilatusReadoutTime
 from ._pilatus_io import PilatusDriverIO, PilatusTriggerMode
