334c82921635be4e77e0d8c8e109e3ed589213c6
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-03-20 17:41:53 +0000

    Fix docs by exposing eiger monitor stream and detector

--- src/ophyd_async/fastcs/eiger/__init__.py
+++ src/ophyd_async/fastcs/eiger/__init__.py
@@ -1,5 +1,13 @@
 from ._eiger import EigerDetector, EigerTriggerInfo
 from ._eiger_controller import EigerController
-from ._eiger_io import EigerDriverIO
+from ._eiger_io import EigerDetectorIO, EigerDriverIO, EigerMonitorIO, EigerStreamIO
 
-__all__ = ["EigerDetector", "EigerController", "EigerDriverIO", "EigerTriggerInfo"]
+__all__ = [
+    "EigerDetector",
+    "EigerController",
+    "EigerDriverIO",
+    "EigerTriggerInfo",
+    "EigerDetectorIO",
+    "EigerMonitorIO",
+    "EigerStreamIO",
+]
