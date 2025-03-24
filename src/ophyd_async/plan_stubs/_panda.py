546ba1f87f5c999f49ea70b58ee89bc719c197e7
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-03-24 12:56:25 +0000

    Remove use of p4p.Context in fastcs/panda/writer

--- src/ophyd_async/plan_stubs/_panda.py
+++ src/ophyd_async/plan_stubs/_panda.py
@@ -1,17 +1,13 @@
-import typing
-
 from bluesky.utils import MsgGenerator, plan
 
 from ophyd_async.core import Settings
+from ophyd_async.fastcs import panda
 
 from ._settings import apply_settings
 
-if typing.TYPE_CHECKING:
-    from ophyd_async.fastcs import panda
-
 
 @plan
-def apply_panda_settings(settings: "Settings[panda.HDFPanda]") -> MsgGenerator[None]:
+def apply_panda_settings(settings: Settings[panda.HDFPanda]) -> MsgGenerator[None]:
     """Apply given settings to a panda device."""
     units, others = settings.partition(lambda signal: signal.name.endswith("_units"))
     yield from apply_settings(units)
