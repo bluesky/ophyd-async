8f31ccd8adcef5a6909695045cfb1949acd63373
eir17846 <victor.rogalev@diamond.ac.uk>
2025-04-02 18:02:30 +0100

    Add more tests

--- tests/core/test_single_derived_signal.py
+++ tests/core/test_single_derived_signal.py
@@ -2,7 +2,6 @@ import asyncio
 import re
 from unittest.mock import call
 from unittest.mock import patch
-import typing
 
 import pytest
 
@@ -10,6 +9,9 @@ from ophyd_async.core import (
     derived_signal_r,
     soft_signal_rw,
 )
+from ophyd_async.core._derived_signal import DerivedSignalFactory, _get_first_arg_datatype, _get_return_datatype, _make_factory, derived_signal_rw
+from ophyd_async.core._signal import SignalR, SignalRW
+from ophyd_async.core._table import Table
 from ophyd_async.testing import (
     BeamstopPosition,
     Exploder,
@@ -21,7 +23,9 @@ from ophyd_async.testing import (
     get_mock,
 )
 
-from ophyd_async.core._derived_signal_backend import validate_by_type
+from ophyd_async.core._derived_signal_backend import DerivedSignalBackend, SignalTransformer, Transform, validate_by_type
+from ophyd_async.core._utils import StrictEnumMeta
+from bluesky.protocols import Reading
 
 @pytest.mark.parametrize(
     "x, y, position",
@@ -72,20 +76,26 @@ async def test_monitoring_position(cls: type[ReadOnlyBeamstop | MovableBeamstop]
     assert await results.get() == BeamstopPosition.IN_POSITION
     assert results.empty()
 
+@pytest.fixture
+def movable_beamstop() -> MovableBeamstop:
+    return  MovableBeamstop("device")
 
-async def test_setting_position():
-    inst = MovableBeamstop("inst")
+@pytest.fixture
+def readonly_beamstop() -> ReadOnlyBeamstop:
+    return  ReadOnlyBeamstop("device")
+
+async def test_setting_position(movable_beamstop:MovableBeamstop):
     # Connect in mock mode so we can see what would have been set
-    await inst.connect(mock=True)
-    m = get_mock(inst)
-    await inst.position.set(BeamstopPosition.OUT_OF_POSITION)
+    await movable_beamstop.connect(mock=True)
+    m = get_mock(movable_beamstop)
+    await movable_beamstop.position.set(BeamstopPosition.OUT_OF_POSITION)
     assert m.mock_calls == [
         call.position.put(BeamstopPosition.OUT_OF_POSITION, wait=True),
         call.x.put(3, wait=True),
         call.y.put(5, wait=True),
     ]
     m.reset_mock()
-    await inst.position.set(BeamstopPosition.IN_POSITION)
+    await movable_beamstop.position.set(BeamstopPosition.IN_POSITION)
     assert m.mock_calls == [
         call.position.put(BeamstopPosition.IN_POSITION, wait=True),
         call.x.put(0, wait=True),
@@ -122,13 +132,13 @@ def test_mismatching_args():
             _get_position, foo=soft_signal_rw(float), bar=soft_signal_rw(float)
         )
 
-@patch("typing.TYPE_CHECKING", True)
-def test_validate_by_type():
-    movable_beamstop = MovableBeamstop("mvb")
+@patch("ophyd_async.core._derived_signal_backend.TYPE_CHECKING", True)
+def test_validate_by_type(
+    movable_beamstop:MovableBeamstop,
+    readonly_beamstop: ReadOnlyBeamstop
+) -> None:
     movable_beamstop2 = MovableBeamstop("mvb2")
-    readable_beamstop = ReadOnlyBeamstop("rdb")
-    
-    invalid_devices_dict = {device.name: device for device in [movable_beamstop, readable_beamstop]}
+    invalid_devices_dict = {device.name: device for device in [movable_beamstop, readonly_beamstop]}
     with pytest.raises(Exception) as e_info:
         validate_by_type(invalid_devices_dict, MovableBeamstop)
     assert type(e_info.value) == TypeError
@@ -139,4 +149,111 @@ def test_validate_by_type():
     with pytest.raises(Exception) as e_info:
         validate_by_type({movable_beamstop.name: movable_beamstop}, ReadOnlyBeamstop)
     assert type(e_info.value) == TypeError
+
+@pytest.fixture
+def transformer() -> SignalTransformer:
+    return  SignalTransformer(Transform, None, None)
+
+@pytest.fixture
+def new_readings() -> dict[str, Reading]:
+    return  {"device-position": Reading(value=0.0,timestamp=0.0)}
+
+@pytest.fixture
+def derived_signal_backend(transformer: SignalTransformer) -> DerivedSignalBackend:
+    return DerivedSignalBackend(Table, "derived_backend", transformer)
+
+async def test_set_derived_not_initialized(
+    transformer : SignalTransformer
+) -> None:
+    with pytest.raises(Exception) as e_info:
+        await transformer.set_derived("name",None)
+    assert type(e_info.value) == RuntimeError
+
+async def test_get_transform_cached(
+    transformer : SignalTransformer,
+    new_readings: dict[str, Reading]
+) -> None:
+    with patch.object(transformer, '_cached_readings', new_readings):
+        with patch.object(transformer, 'raw_and_transform_subscribables', {"device":SignalR}):
+            assert transformer._cached_readings == new_readings
+            r = await transformer.get_transform()
+            assert isinstance(r,Transform)
+
+def test_update_cached_reading_non_initialized(
+    transformer : SignalTransformer,
+    new_readings: dict[str, Reading]
+) -> None:
+    with pytest.raises(Exception) as e_info:
+        transformer._update_cached_reading(new_readings)
+    assert type(e_info.value) == RuntimeError
+
+def test_update_cached_reading_initialized(
+    transformer : SignalTransformer,
+    new_readings: dict[str, Reading]
+) -> None:
+    transformer._cached_readings = {}
+    transformer._update_cached_reading(new_readings)
+    assert transformer._cached_readings == new_readings
+
+@patch("ophyd_async.core._utils.Callback")
+def test_set_callback_already_set(
+    mock_class,
+    transformer : SignalTransformer
+) -> None:
+    device_name = "device"
+    with patch.object(transformer, '_derived_callbacks', {device_name:mock_class}):
+        with pytest.raises(Exception) as e_info:
+            transformer.set_callback(device_name,mock_class)
+        assert type(e_info.value) == RuntimeError
+
+async def test_derived_signal_backend_connect_pass(
+    derived_signal_backend:DerivedSignalBackend
+) -> None:
+    result = await derived_signal_backend.connect(0.0)
+    assert result == None
+
+def test_derived_signal_backend_set_value(
+    derived_signal_backend:DerivedSignalBackend
+) -> None:
+    with pytest.raises(Exception) as e_info:
+        derived_signal_backend.set_value(0.0)
+    assert type(e_info.value) == RuntimeError
+
+async def test_derived_signal_backend_put_fails(
+    derived_signal_backend:DerivedSignalBackend
+) -> None:
+    with pytest.raises(Exception) as e_info:
+        await derived_signal_backend.put(value = None, wait = False)
+    assert type(e_info.value) == RuntimeError
     
+    with pytest.raises(Exception) as e_info:
+        await derived_signal_backend.put(value = None, wait = True)
+    assert type(e_info.value) == RuntimeError
+
+@patch("ophyd_async.core._derived_signal.get_type_hints", return_value={})
+def test_get_return_datatype_no_type(movable_beamstop:MovableBeamstop):
+    with pytest.raises(Exception) as e_info:
+        _get_return_datatype(movable_beamstop._get_position)
+    assert type(e_info.value) == TypeError
+
+def test_get_return_datatype(movable_beamstop:MovableBeamstop):
+    result = _get_return_datatype(movable_beamstop._get_position)
+    assert isinstance(result,StrictEnumMeta)
+
+@patch("ophyd_async.core._derived_signal.get_type_hints", return_value={})
+def test_get_first_arg_datatype_no_type(movable_beamstop:MovableBeamstop):
+    with pytest.raises(Exception) as e_info:
+        _get_first_arg_datatype(movable_beamstop._set_from_position)
+    assert type(e_info.value) == TypeError
+
+def test_derived_signal_rw_type_error(movable_beamstop:MovableBeamstop):
+    with patch.object(movable_beamstop, '_set_from_position', movable_beamstop._get_position):
+        with pytest.raises(Exception) as e_info:
+            derived_signal_rw(movable_beamstop._get_position,movable_beamstop._set_from_position)
+        assert type(e_info.value) == TypeError
+
+def test_make_rw_signal_type_mismatch(movable_beamstop:MovableBeamstop):
+    factory = _make_factory(movable_beamstop._get_position, None, {"x":movable_beamstop.x, "y":movable_beamstop.y})
+    with pytest.raises(Exception) as e_info:
+        factory._make_signal(signal_cls=SignalRW, datatype=BeamstopPosition, name="")
+    assert type(e_info.value) == ValueError
