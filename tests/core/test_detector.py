bfbbbb7659903c7504f794aabb86f87a286a4e25
paula-mg <paulamartingmz@gmail.com>
2025-04-03 15:25:52 +0000

    Fix prepare calls test

--- tests/core/test_detector.py
+++ tests/core/test_detector.py
@@ -49,11 +49,9 @@ async def test_prepare_internal_trigger(standard_detector: StandardDetector) -> 
     assert standard_detector._initial_frame == 0
     assert standard_detector._name == "test_detector"
     standard_detector._writer.open.assert_called_once_with(
-        standard_detector.name, trigger_info.multiplier
+        standard_detector._name, trigger_info.multiplier
     )  # type: ignore
-    standard_detector._controller.prepare.assert_called_once_with(
-        standard_detector.name, trigger_info
-    )  # type: ignore
+    standard_detector._controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
 
 
 async def test_prepare_external_trigger(standard_detector: StandardDetector) -> None:
@@ -71,9 +69,7 @@ async def test_prepare_external_trigger(standard_detector: StandardDetector) -> 
     standard_detector._writer.open.assert_called_once_with(
         standard_detector.name, trigger_info.multiplier
     )  # type: ignore
-    standard_detector._controller.prepare.assert_called_once_with(
-        standard_detector.name, trigger_info
-    )  # type: ignore
+    standard_detector._controller.prepare.assert_called_once_with(trigger_info)  # type: ignore
     standard_detector._controller.arm.assert_called_once()  # type: ignore
 
 
