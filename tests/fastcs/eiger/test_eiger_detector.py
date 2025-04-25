a6e46fb03ac5ea58c5c0d12e69c9b397ad8ed907
Shihab S <162436767+shihab-dls@users.noreply.github.com>
2025-04-25 10:43:41 +0100

    Use ADOdin with FastCS-Eiger (#863)
    * Add fastcs eiger but use old odin
    
    * Use ADOdin with fastcs-eiger
    
    * Reorder signal type matching
    
    * Get value from initial_values response
    
    * Add test for order of signal sets and fix odin nodes
    
    ---------
    
    Co-authored-by: Dominic Oram <dominic.oram@diamond.ac.uk>

--- tests/fastcs/eiger/test_eiger_detector.py
+++ tests/fastcs/eiger/test_eiger_detector.py
@@ -4,13 +4,16 @@ import pytest
 
 from ophyd_async.core import DetectorTrigger, init_devices
 from ophyd_async.fastcs.eiger import EigerDetector, EigerTriggerInfo
-from ophyd_async.testing import get_mock_put
+from ophyd_async.testing import get_mock_put, set_mock_value
 
 
 @pytest.fixture
 def detector(RE):
     with init_devices(mock=True):
         detector = EigerDetector("BL03I", MagicMock())
+    set_mock_value(detector.odin.meta_active, "Active")
+    set_mock_value(detector.odin.capture_rbv, "Capturing")
+    set_mock_value(detector.odin.meta_writing, "Writing")
     return detector
 
 
