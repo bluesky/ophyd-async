bca4481347eac3a41ee742e50c6b9307852636d7
Villtord <Villtord@users.noreply.github.com>
2025-04-30 13:07:43 +0000

    deploy: 6e6169cce2987f2e04e657c6ad197b01afdb7faf

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.10.0a4.dev3+g80e70fc31',
+    VERSION: '0.10.0a4.dev4+g6e6169cce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
