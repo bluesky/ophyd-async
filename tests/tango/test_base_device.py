e1b4f179f05b0280efd709d26baa6046dd3bc577
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-05-27 15:10:44 +0000

    increase long test time again

--- tests/tango/test_base_device.py
+++ tests/tango/test_base_device.py
@@ -345,7 +345,7 @@ async def test_with_bluesky(tango_test_device):
 
 # --------------------------------------------------------------------
 @pytest.mark.asyncio
-@pytest.mark.timeout(10.5)
+@pytest.mark.timeout(12.5)
 async def test_tango_sim(sim_test_context_trls):
     detector = TangoDetector(
         name="detector",
