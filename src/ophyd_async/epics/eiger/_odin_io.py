5ce557359aa685f6bc7e87a9d945fc530b6f8117
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-06-03 15:53:57 +0000

    Add odin FAN PV

--- src/ophyd_async/epics/eiger/_odin_io.py
+++ src/ophyd_async/epics/eiger/_odin_io.py
@@ -75,6 +75,8 @@ class Odin(Device):
         self.meta_file_name = epics_signal_r(str, f"{prefix}META:FileName_RBV")
         self.meta_stop = epics_signal_rw(bool, f"{prefix}META:Stop")
 
+        self.fan_ready = epics_signal_rw(float, f"{prefix}FAN:StateReady_RBV")
+
         self.data_type = epics_signal_rw_rbv(str, f"{prefix}DataType")
 
         super().__init__(name)
@@ -97,22 +99,23 @@ class OdinWriter(DetectorWriter):
         info = self._path_provider(device_name=name)
         self._exposures_per_event = exposures_per_event
 
+        print(await self._drv.id.get_value())
         await asyncio.gather(
-            self._drv.file_path.set(str(info.directory_path)),
-            self._drv.file_name.set(info.filename),
             self._drv.data_type.set(f"UInt{await self._eiger_bit_depth().get_value()}"),
             self._drv.num_to_capture.set(0),
         )
 
+        await self._drv.file_path.set(str(info.directory_path))
+        await self._drv.file_name.set(info.filename)
+
+        await wait_for_value(
+            self._drv.meta_file_name, info.filename, timeout=DEFAULT_TIMEOUT
+        )
+        await wait_for_value(self._drv.id, info.filename, timeout=DEFAULT_TIMEOUT)
+
         await wait_for_value(self._drv.meta_active, "Active", timeout=DEFAULT_TIMEOUT)
 
-        await asyncio.gather(
-            wait_for_value(self._drv.meta_active, "Active", timeout=DEFAULT_TIMEOUT),
-            wait_for_value(self._drv.id, info.filename, timeout=DEFAULT_TIMEOUT),
-            wait_for_value(
-                self._drv.meta_file_name, info.filename, timeout=DEFAULT_TIMEOUT
-            ),
-        )
+        print(await self._drv.id.get_value())
 
         self._capture_status = await set_and_wait_for_value(
             self._drv.capture, Writing.CAPTURE, wait_for_set_completion=False
