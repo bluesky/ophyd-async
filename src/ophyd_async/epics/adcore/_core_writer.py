f62c576f5be3add206d8e2997243d0cd15a9769f
Jakub Wlodek <jwlodek@bnl.gov>
2025-07-10 09:54:16 -0400

    Don't use os.sep, since separator must be defined by IOC host

--- src/ophyd_async/epics/adcore/_core_writer.py
+++ src/ophyd_async/epics/adcore/_core_writer.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 from collections.abc import AsyncGenerator, AsyncIterator
 from typing import Generic, TypeVar, get_args
 
@@ -93,9 +92,17 @@ class ADWriter(DetectorWriter, Generic[NDFileIOT]):
         # when directory path PV is processed.
         await self.fileio.create_directory.set(path_info.create_dir_depth)
 
+        # Need to ensure that trailing separator is added to the directory path.
+        # When setting the path for windows based AD IOCs, a '/' is added rather than
+        # a '\\', which will cause the readback to never register the same value.
+        dir_path_as_str = str(path_info.directory_path)
+        separator = "/"
+        if "\\" in dir_path_as_str:
+            separator = "\\"
+
         await asyncio.gather(
             # See https://github.com/bluesky/ophyd-async/issues/122
-            self.fileio.file_path.set(str(path_info.directory_path) + os.sep),
+            self.fileio.file_path.set(dir_path_as_str + separator),
             self.fileio.file_name.set(path_info.filename),
             self.fileio.file_write_mode.set(ADFileWriteMode.STREAM),
             # For non-HDF file writers, use AD file templating mechanism
