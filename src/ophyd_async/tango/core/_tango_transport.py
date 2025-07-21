a025c087b78819e07ef1d35a886d19534ff65f37
Devin Burke <devin.burke@desy.de>
2025-07-21 11:11:10 +0200

    lint fix

--- src/ophyd_async/tango/core/_tango_transport.py
+++ src/ophyd_async/tango/core/_tango_transport.py
@@ -485,6 +485,7 @@ class AttributeProxy(TangoProxy):
 
 class CommandProxyReadCharacter(StrictEnum):
     """Enum to carry the read/write character of the CommandProxy."""
+
     READ = "READ"
     WRITE = "WRITE"
     READ_WRITE = "READ_WRITE"
