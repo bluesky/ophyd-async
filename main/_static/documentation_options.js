fda6172d925fe5db85febd133d9951fba58476d4
coretl <coretl@users.noreply.github.com>
2025-07-28 08:46:47 +0000

    deploy: 1ee1de994ea9e108e54ba97d7b0fe678a2bb40f0

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.12.2.dev1+g5a84d9293',
+    VERSION: '0.12.3.dev1+g1ee1de994',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
