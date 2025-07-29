37137214cdfe8402e3792576a66d2465a192c086
Grace Kalu-Anyah <Grace.Kalu-Anyah@diamond.ac.uk>
2025-07-29 07:49:57 +0000

    Make pmac util file with PmacMotorInfo class

---
+++ src/ophyd_async/epics/pmac/_util.py
@@ -0,0 +1,78 @@
+import asyncio
+from collections.abc import Sequence
+from dataclasses import dataclass
+
+from ophyd_async.core._utils import gather_dict
+from ophyd_async.epics.motor import Motor
+from ophyd_async.epics.pmac._pmac_io import PmacIO
+
+
+@dataclass
+class _PmacMotorInfo:
+    cs_port: str
+    cs_number: int
+    motor_cs_index: dict[Motor, int]
+    motor_acceleration_rate: dict[Motor, float]
+
+    @classmethod
+    async def from_motors(
+        cls, pmac: PmacIO, motors: Sequence[Motor]
+    ) -> "_PmacMotorInfo":
+        assignments = {
+            motor: pmac.assignment[pmac.motor_assignment_index[motor]]
+            for motor in motors
+        }
+
+        cs_ports, cs_numbers, cs_axes, velocities, accls = await asyncio.gather(
+            gather_dict(
+                {motor: assignments[motor].cs_port.get_value() for motor in motors}
+            ),
+            gather_dict(
+                {motor: assignments[motor].cs_number.get_value() for motor in motors}
+            ),
+            gather_dict(
+                {
+                    motor: assignments[motor].cs_axis_letter.get_value()
+                    for motor in motors
+                }
+            ),
+            gather_dict({motor: motor.max_velocity.get_value() for motor in motors}),
+            gather_dict(
+                {motor: motor.acceleration_time.get_value() for motor in motors}
+            ),
+        )
+
+        # check if the values in cs_port and cs_number are the same
+        cs_ports = set(cs_ports.values())
+
+        if len(cs_ports) != 1:
+            raise RuntimeError(
+                "Failed to fetch common CS port."
+                "Motors passed are assigned to multiple CS ports:"
+                f"{list(cs_ports)}"
+            )
+
+        cs_port = cs_ports.pop()
+
+        cs_numbers = set(cs_numbers.values())
+        if len(cs_numbers) != 1:
+            raise RuntimeError(
+                "Failed to fetch common CS number."
+                "Motors passed are assigned to multiple CS numbers:"
+                f"{list(cs_numbers)}"
+            )
+
+        cs_number = cs_numbers.pop()
+
+        motor_cs_index = {}
+        for motor in cs_axes:
+            motor_cs_index[motor] = "ABCUVWXYZ".index(cs_axes[motor])
+
+        motor_acceleration_rate = {
+            motor: float(velocities[motor]) / float(accls[motor])
+            for motor in velocities
+        }
+
+        return _PmacMotorInfo(
+            cs_port, cs_number, motor_cs_index, motor_acceleration_rate
+        )
---
+++ tests/epics/pmac/test_util.py
@@ -0,0 +1,69 @@
+import pytest
+
+from ophyd_async.core import init_devices
+from ophyd_async.epics.motor import Motor
+from ophyd_async.epics.pmac import PmacIO
+from ophyd_async.epics.pmac._util import _PmacMotorInfo  # noqa: PLC2701
+from ophyd_async.testing import set_mock_value
+
+
+@pytest.fixture
+async def sim_motors():
+    sim_x_motor = Motor("BLxxI-MO-STAGE-01:X", name="sim_x_motor")
+    sim_y_motor = Motor("BLxxI-MO-STAGE-01:Y", name="sim_y_motor")
+    async with init_devices(mock=True):
+        sim_pmac = PmacIO(
+            prefix="Test_PMAC",
+            name="sim_pmac",
+            raw_motors=[sim_x_motor, sim_y_motor],
+            coord_nums=[],
+        )
+    await sim_x_motor.connect(mock=True)
+    await sim_y_motor.connect(mock=True)
+    pmac_x = sim_pmac.assignment[sim_pmac.motor_assignment_index[sim_x_motor]]
+    pmac_y = sim_pmac.assignment[sim_pmac.motor_assignment_index[sim_y_motor]]
+    set_mock_value(pmac_x.cs_port, "CS1")
+    set_mock_value(pmac_x.cs_number, 1)
+    set_mock_value(pmac_x.cs_axis_letter, "X")
+    set_mock_value(pmac_y.cs_port, "CS1")
+    set_mock_value(pmac_y.cs_number, 1)
+    set_mock_value(pmac_y.cs_axis_letter, "Y")
+    set_mock_value(sim_x_motor.acceleration_time, 0.5)
+    set_mock_value(sim_x_motor.max_velocity, 5)
+    set_mock_value(sim_y_motor.acceleration_time, 0.5)
+    set_mock_value(sim_y_motor.max_velocity, 10)
+
+    yield (sim_pmac, sim_x_motor, sim_y_motor)
+
+
+async def test_motor_info_from_motors(sim_motors: tuple[PmacIO, Motor, Motor]):
+    sim_pmac, sim_x_motor, sim_y_motor = sim_motors
+    motor_info = await _PmacMotorInfo.from_motors(sim_pmac, [sim_x_motor, sim_y_motor])
+    assert motor_info.cs_port == "CS1"
+    assert motor_info.cs_number == 1
+    assert motor_info.motor_acceleration_rate == {sim_x_motor: 10, sim_y_motor: 20}
+    assert motor_info.motor_cs_index == {sim_x_motor: 6, sim_y_motor: 7}
+
+
+async def test_multiple_cs_port_raises_runtime_error(
+    sim_motors: tuple[PmacIO, Motor, Motor],
+):
+    sim_pmac, sim_x_motor, sim_y_motor = sim_motors
+    set_mock_value(
+        sim_pmac.assignment[sim_pmac.motor_assignment_index[sim_x_motor]].cs_port, "CS2"
+    )
+
+    with pytest.raises(RuntimeError, match="multiple CS ports"):
+        await _PmacMotorInfo.from_motors(sim_pmac, [sim_x_motor, sim_y_motor])
+
+
+async def test_multiple_cs_number_raises_runtime_error(
+    sim_motors: tuple[PmacIO, Motor, Motor],
+):
+    sim_pmac, sim_x_motor, sim_y_motor = sim_motors
+    set_mock_value(
+        sim_pmac.assignment[sim_pmac.motor_assignment_index[sim_x_motor]].cs_number, 2
+    )
+
+    with pytest.raises(RuntimeError, match="multiple CS numbers"):
+        await _PmacMotorInfo.from_motors(sim_pmac, [sim_x_motor, sim_y_motor])
