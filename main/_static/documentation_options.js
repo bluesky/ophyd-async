22901605a962b3905eb8f9ff14afeeaa86778b20
gkalua <gkalua@users.noreply.github.com>
2025-07-31 16:05:53 +0000

    deploy: 9054d714cba4563fc3fff341a2b82fc553173ab9

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.12.3.dev3+ga67ef2aca',
+    VERSION: '0.12.4.dev1+g9054d714c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
