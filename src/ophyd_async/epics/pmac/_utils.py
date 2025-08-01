a66e8969e212e78efef0dd63b61db60d7dc48f16
Shihab S <162436767+shihab-dls@users.noreply.github.com>
2025-08-01 16:59:38 +0100

    Parse gapless pmac trajectory from slice (#986)
    * feat: parse gapless pmac trajectory from slice
    
    * chore: add scanspec branch as dep
    
    * refactor: exclude ramping events
    
    * docs: add comment for TICK_S
    
    * tests: amend tests to check pure trajectory no ramps
    
    * refactor: address review comments
    
    * wip: center velocity calculations and add initial point
    
    * refactor: handle middle and upper vels seperately
    
    * tests: add spiral test
    
    * chore: make Trajectory private
    
    ---------
    
    Co-authored-by: Tom C (DLS) <101418278+coretl@users.noreply.github.com>

--- src/ophyd_async/epics/pmac/_utils.py
+++ src/ophyd_async/epics/pmac/_utils.py
@@ -4,12 +4,105 @@ import asyncio
 from collections.abc import Sequence
 from dataclasses import dataclass
 
+import numpy as np
+import numpy.typing as npt
 from scanspec.core import Slice
 
-from ophyd_async.core import gather_dict
+from ophyd_async.core import error_if_none, gather_dict
 from ophyd_async.epics.motor import Motor
 
 from ._pmac_io import CS_LETTERS, PmacIO
+
+# PMAC durations are in milliseconds
+# We must convert from scanspec durations (seconds) to milliseconds
+# PMAC motion program multiples durations by 0.001
+# (see https://github.com/DiamondLightSource/pmac/blob/afe81f8bb9179c3a20eff351f30bc6cfd1539ad9/pmacApp/pmc/trajectory_scan_code_ppmac.pmc#L241)
+# Therefore, we must divide scanspec durations by 10e-6
+TICK_S = 0.000001
+
+
+@dataclass
+class _Trajectory:
+    positions: dict[Motor, np.ndarray]
+    velocities: dict[Motor, np.ndarray]
+    user_programs: npt.NDArray[np.int32]
+    durations: npt.NDArray[np.float64]
+
+    @classmethod
+    def from_slice(cls, slice: Slice[Motor], ramp_up_time: float) -> _Trajectory:
+        """Parse a trajectory with no gaps from a slice.
+
+        :param slice: Information about a series of scan frames along a number of axes
+        :param ramp_up_duration: Time required to ramp up to speed
+        :param ramp_down: Booleon representing if we ramp down or not
+        :returns Trajectory: Data class representing our parsed trajectory
+        :raises RuntimeError: Slice must have no gaps and a duration array
+        """
+        slice_duration = error_if_none(slice.duration, "Slice must have a duration")
+
+        # Check if any gaps other than initial gap.
+        if any(slice.gap[1:]):
+            raise RuntimeError(
+                f"Cannot parse trajectory with gaps. Slice has gaps: {slice.gap}"
+            )
+
+        scan_size = len(slice)
+        motors = slice.axes()
+
+        positions: dict[Motor, npt.NDArray[np.float64]] = {}
+        velocities: dict[Motor, npt.NDArray[np.float64]] = {}
+
+        # Initialise arrays
+        positions = {motor: np.empty(2 * scan_size + 1, float) for motor in motors}
+        velocities = {motor: np.empty(2 * scan_size + 1, float) for motor in motors}
+        durations: npt.NDArray[np.float64] = np.empty(2 * scan_size + 1, float)
+        user_programs: npt.NDArray[np.int32] = np.ones(2 * scan_size + 1, float)
+        user_programs[-1] = 8
+
+        # Ramp up time for start of collection window
+        durations[0] = int(ramp_up_time / TICK_S)
+        # Half the time per point
+        durations[1:] = np.repeat(slice_duration / (2 * TICK_S), 2)
+
+        # Fill profile assuming no gaps
+        # Excluding starting points, we begin at our next frame
+        half_durations = slice_duration / 2
+        for motor in motors:
+            # Set the first position to be lower bound, then
+            # alternate mid and upper as the upper and lower
+            # bounds of neighbouring points are the same as gap is false
+            positions[motor][0] = slice.lower[motor][0]
+            positions[motor][1::2] = slice.midpoints[motor]
+            positions[motor][2::2] = slice.upper[motor]
+            # For velocities we will need the relative distances
+            mid_to_upper_velocities = (
+                slice.upper[motor] - slice.midpoints[motor]
+            ) / half_durations
+            lower_to_mid_velocities = (
+                slice.midpoints[motor] - slice.lower[motor]
+            ) / half_durations
+            # First velocity is the lower -> mid of first point
+            velocities[motor][0] = lower_to_mid_velocities[0]
+            # For the midpoints, we take the average of the
+            # lower -> mid and mid-> upper velocities of the same point
+            velocities[motor][1::2] = (
+                lower_to_mid_velocities + mid_to_upper_velocities
+            ) / 2
+            # For the upper points, we need to take the average of the
+            # mid -> upper velocity of the previous point and
+            # lower -> mid velocity of the current point
+            velocities[motor][2:-1:2] = (
+                mid_to_upper_velocities[:-1] + lower_to_mid_velocities[1:]
+            ) / 2
+            # For the last velocity take the mid to upper velocity
+            velocities[motor][-1] = mid_to_upper_velocities[-1]
+
+        return cls(
+            positions=positions,
+            velocities=velocities,
+            user_programs=user_programs,
+            durations=durations,
+        )
 
 
 @dataclass
--- tests/epics/pmac/test_pmac_utils.py
+++ tests/epics/pmac/test_pmac_utils.py
@@ -1,6 +1,6 @@
 import pytest
 from scanspec.core import Path
-from scanspec.specs import Fly, Line
+from scanspec.specs import Fly, Line, Spiral
 
 from ophyd_async.core import init_devices
 from ophyd_async.epics.motor import Motor
@@ -9,6 +9,7 @@ from ophyd_async.epics.pmac import (
 )
 from ophyd_async.epics.pmac._utils import (
     _PmacMotorInfo,  # noqa: PLC2701
+    _Trajectory,  # noqa: PLC2701
     calculate_ramp_position_and_duration,  # noqa: PLC2701
 )
 from ophyd_async.testing import set_mock_value
@@ -41,7 +42,189 @@ async def sim_motors():
     yield (sim_pmac, sim_x_motor, sim_y_motor)
 
 
-async def test_calculate_ramp_position_and_duration(sim_motors):
+async def test_line_trajectory_from_slice(sim_motors: tuple[PmacIO, Motor, Motor]):
+    _, sim_x_motor, _ = sim_motors
+    spec = Fly(2.0 @ Line(sim_x_motor, 1, 5, 9))
+    slice = Path(spec.calculate()).consume()
+
+    trajectory = _Trajectory.from_slice(slice, 2)
+
+    assert trajectory.positions[sim_x_motor] == pytest.approx(
+        [
+            0.75,
+            1.0,
+            1.25,
+            1.5,
+            1.75,
+            2.0,
+            2.25,
+            2.5,
+            2.75,
+            3.0,
+            3.25,
+            3.5,
+            3.75,
+            4.0,
+            4.25,
+            4.5,
+            4.75,
+            5.0,
+            5.25,
+        ]
+    )
+
+    assert trajectory.velocities[sim_x_motor] == pytest.approx(
+        [
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+            0.25,
+        ]
+    )
+
+    assert (
+        trajectory.user_programs
+        == [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            8,
+        ]
+    ).all()
+
+    assert (
+        trajectory.durations
+        == [
+            2000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+            1000000.0,
+        ]
+    ).all()
+
+
+async def test_spiral_trajectory_from_slice(sim_motors: tuple[PmacIO, Motor, Motor]):
+    _, sim_x_motor, sim_y_motor = sim_motors
+    spec = Spiral(sim_x_motor, sim_y_motor, 0, 0, 5, 5, 3)
+    slice = Path(Fly(2.0 @ spec).calculate()).consume()
+
+    trajectory = _Trajectory.from_slice(slice, 2)
+    assert trajectory.positions == {
+        sim_x_motor: pytest.approx(
+            [
+                0.0,
+                0.60538,
+                -0.56648101,
+                -1.64763742,
+                -1.94954837,
+                -1.43181015,
+                -0.35683972,
+            ]
+        ),
+        sim_y_motor: pytest.approx(
+            [
+                0.0,
+                -0.82169442,
+                -1.32756642,
+                -0.64053956,
+                0.60491969,
+                1.77714744,
+                2.47440203,
+            ]
+        ),
+    }
+
+    assert trajectory.velocities == {
+        sim_x_motor: pytest.approx(
+            [
+                0.60538,
+                -0.28324051,
+                -1.12650871,
+                -0.69153368,
+                0.10791364,
+                0.79635432,
+                1.07497043,
+            ]
+        ),
+        sim_y_motor: pytest.approx(
+            [
+                -0.82169442,
+                -0.66378321,
+                0.09057743,
+                0.96624305,
+                1.2088435,
+                0.93474117,
+                0.69725459,
+            ]
+        ),
+    }
+
+    assert trajectory.durations == pytest.approx(
+        [2000000.0, 1000000.0, 1000000.0, 1000000.0, 1000000.0, 1000000.0, 1000000.0]
+    )
+
+    assert trajectory.user_programs == pytest.approx([1, 1, 1, 1, 1, 1, 8])
+
+
+async def test_trajectory_from_slice_raises_runtime_error_if_gap(
+    sim_motors: tuple[PmacIO, Motor, Motor],
+):
+    _, sim_x_motor, sim_y_motor = sim_motors
+    spec = Line(sim_y_motor, 10, 12, 3) * ~Line(sim_x_motor, 1, 5, 5)
+    slice = Path(Fly(2.0 @ spec).calculate()).consume()
+
+    with pytest.raises(RuntimeError, match="Slice has gaps"):
+        _Trajectory.from_slice(slice, 2)
+
+
+async def test_calculate_ramp_position_and_duration(
+    sim_motors: tuple[PmacIO, Motor, Motor],
+):
     sim_pmac, sim_x_motor, sim_y_motor = sim_motors
     spec = Fly(1.0 @ (Line(sim_y_motor, 10, 12, 3) * ~Line(sim_x_motor, 1, 5, 5)))
     slice = Path(spec.calculate()).consume()
