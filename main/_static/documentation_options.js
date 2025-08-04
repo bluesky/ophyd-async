8d39e1d5b552eadc824d7b5b82724ace0feaa948
GDYendell <GDYendell@users.noreply.github.com>
2025-08-04 15:44:26 +0000

    deploy: 8a3013daf19518b475548d4a85b617f8408e90dc

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.12.4.dev8+g3e0c3c25c',
+    VERSION: '0.12.4.dev9+g8a3013daf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
