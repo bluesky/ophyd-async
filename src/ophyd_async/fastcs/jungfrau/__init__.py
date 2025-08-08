8be2abbc2efeceb415688d674b4a203a45fe4eb4
oliver.silvester@diamond.ac.uk <oliver.silvester@diamond.ac.uk>
2025-08-08 15:54:20 +0000

    Fix docs again

--- src/ophyd_async/fastcs/jungfrau/__init__.py
+++ src/ophyd_async/fastcs/jungfrau/__init__.py
@@ -1,8 +1,6 @@
 from ._controller import JungfrauController
 from ._jungfrau import Jungfrau
-from ._signals import (
-    DetectorStatus,
-)
+from ._signals import DetectorStatus, JungfrauDriverIO, JungfrauTriggerMode
 from ._utils import (
     create_jungfrau_external_triggering_info,
     create_jungfrau_internal_triggering_info,
@@ -14,4 +12,6 @@ __all__ = [
     "create_jungfrau_external_triggering_info",
     "create_jungfrau_internal_triggering_info",
     "JungfrauController",
+    "JungfrauDriverIO",
+    "JungfrauTriggerMode",
 ]
