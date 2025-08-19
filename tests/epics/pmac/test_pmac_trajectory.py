a4d7f4a292454ec1fa3850aba1157c4fa43fcaa5
Grace Kalu-Anyah <Grace.Kalu-Anyah@diamond.ac.uk>
2025-08-19 11:00:19 +0000

    test pmac_trajectory

--- tests/epics/pmac/test_pmac_trajectory.py
+++ tests/epics/pmac/test_pmac_trajectory.py
@@ -43,43 +43,21 @@ async def sim_motors():
     set_mock_value(sim_y_motor.acceleration_time, 0.5)
     set_mock_value(sim_y_motor.velocity, 1)
     set_mock_value(sim_y_motor.max_velocity, 10)
-    set_mock_value(sim_x_motor.velocity, 1)
     yield (sim_pmac, sim_x_motor, sim_y_motor)
 
 
 async def test_pmac_prepare(sim_motors: tuple[PmacIO, Motor, Motor]):
     pmacIO, sim_x_motor, sim_y_motor = sim_motors
-    spec = Fly(1.0 @ Line(sim_x_motor, 1, 5, 9))
+    spec = Fly(2.0 @ Line(sim_x_motor, 1, 5, 2))
     trigger_logic = PmacTriggerLogic(spec=spec)
     pmac_trajectory = PmacTrajectoryTriggerLogic(pmacIO)
     await pmac_trajectory.prepare(trigger_logic)
 
-    assert await sim_x_motor.user_setpoint.get_value() == 0.7375
-    assert pmac_trajectory.scantime == 9100000
+    # assert await sim_x_motor.user_setpoint.get_value() == -1.2
+    assert await pmacIO.coord[1].cs_axis_setpoint[7].get_value() == -1.2
 
     assert await pmacIO.trajectory.positions[7].get_value() == pytest.approx(
-        [
-            0.75,
-            1.0,
-            1.25,
-            1.5,
-            1.75,
-            2.0,
-            2.25,
-            2.5,
-            2.75,
-            3.0,
-            3.25,
-            3.5,
-            3.75,
-            4.0,
-            4.25,
-            4.5,
-            4.75,
-            5.0,
-            5.25,
-            5.2625,
-        ]
+        [-1.0, 1.0, 3.0, 5.0, 7.0, 7.2]
     )
     assert pmac_trajectory.scantime == 4400000
 
@@ -158,81 +136,6 @@ async def test_pmac_move_to_start(sim_motors: tuple[PmacIO, Motor, Motor]):
         {"wait": True},
     )
     assert coord_mock_calls[3] == call.defer_moves.put(False, wait=True)
-    assert await pmacIO.trajectory.velocities[7].get_value() == pytest.approx(
-        [
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.5,
-            0.0,
-        ]
-    )
-
-    assert await pmacIO.trajectory.time_array.get_value() == pytest.approx(
-        [
-            50000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            500000,
-            50000,
-        ]
-    )
-
-    assert await pmacIO.trajectory.user_array.get_value() == pytest.approx(
-        [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            8,
-        ]
-    )
-
-    await pmac_trajectory.kickoff()
 
 
 async def test_pmac_trajectory_kickoff(sim_motors: tuple[PmacIO, Motor, Motor]):
