7eee369a97351d280d7ddd6d49c0f132ca3d53bb
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-09-04 14:04:21 +0000

    FIX: wrong number of points set in the trajectory

--- src/ophyd_async/epics/pmac/_pmac_trajectory.py
+++ src/ophyd_async/epics/pmac/_pmac_trajectory.py
@@ -72,7 +72,6 @@ class PmacTrajectoryTriggerLogic(FlyerController):
         self.scantime = np.sum(trajectory.durations)
         use_axis = {axis + 1: False for axis in range(len(CS_LETTERS))}
 
-        size = 0
         for motor, number in motor_info.motor_cs_index.items():
             use_axis[number + 1] = True
             await self.pmac.trajectory.positions[number + 1].set(
@@ -81,13 +80,12 @@ class PmacTrajectoryTriggerLogic(FlyerController):
             await self.pmac.trajectory.velocities[number + 1].set(
                 trajectory.velocities[motor]
             )
-            size += len(trajectory.positions[motor])
 
         coros = [
             self.pmac.trajectory.profile_cs_name.set(motor_info.cs_port),
             self.pmac.trajectory.time_array.set(trajectory.durations),
             self.pmac.trajectory.user_array.set(trajectory.user_programs),
-            self.pmac.trajectory.points_to_build.set(size),
+            self.pmac.trajectory.points_to_build.set(len(trajectory.user_programs)),
             self.pmac.trajectory.calculate_velocities.set(False),
         ] + [
             self.pmac.trajectory.use_axis[number].set(use)
