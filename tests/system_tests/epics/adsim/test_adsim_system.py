04278237431b3741ca2b54b6f09c2863ade16892
Hiran Wijesinghe <hiran.wijesinghe@diamond.ac.uk>
2025-09-22 13:00:46 +0000

    manually patch the env

--- tests/system_tests/epics/adsim/test_adsim_system.py
+++ tests/system_tests/epics/adsim/test_adsim_system.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from typing import Any
-from unittest.mock import ANY, patch
+from unittest.mock import ANY
 
 import bluesky.plan_stubs as bps
 import bluesky.plans as bp
@@ -35,17 +35,35 @@ from ophyd_async.plan_stubs import (
 TIMEOUT = 10.0
 
 
+# @pytest.fixture(scope="module")
+# def with_env():
+#     with patch.dict(
+#         os.environ,
+#         {
+#             "EPICS_CA_NAME_SERVERS": "127.0.0.1:9064",
+#             "EPICS_PVA_NAME_SERVERS": "127.0.0.1:9075",
+#         },
+#         clear=True,
+#     ):
+#         yield
+
+
 @pytest.fixture(scope="module")
 def with_env():
-    with patch.dict(
-        os.environ,
-        {
-            "EPICS_CA_NAME_SERVERS": "127.0.0.1:9064",
-            "EPICS_PVA_NAME_SERVERS": "127.0.0.1:9075",
-        },
-        clear=True,
-    ):
+    target_env = {
+        "EPICS_CA_NAME_SERVERS": "127.0.0.1:9064",
+        "EPICS_PVA_NAME_SERVERS": "127.0.0.1:9075",
+    }
+    original_env = {key: os.environ.get(key, "") for key in target_env}
+
+    # Set the test values
+    os.environ.update(target_env)
+
+    try:
         yield
+    finally:
+        # Restore original values (or delete if originally unset)
+        os.environ.update(original_env)
 
 
 @pytest.fixture
