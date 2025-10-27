6e875b968d12a2dc26d533460feb928fb43eaff5
DominicOram <DominicOram@users.noreply.github.com>
2025-10-27 17:05:49 +0000

    deploy: c1626aa9913dc5019abcb77a0ca3075f66c94008

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.13.5.dev20+gbb7c98987',
+    VERSION: '0.13.6.dev1+gc1626aa99',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
