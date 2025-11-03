8659ea3a74aa07cdcdc85729f3137165deebf213
Grace Kalu-Anyah <Grace.Kalu-Anyah@diamond.ac.uk>
2025-11-03 16:14:13 +0000

    Restructures code and tests to derive cs_number from cs_lookup table

--- tests/unit_tests/epics/pmac/test_pmac_utils.py
+++ tests/unit_tests/epics/pmac/test_pmac_utils.py
@@ -37,17 +37,28 @@ async def test_calculate_ramp_position_and_duration(
     assert ramp_down_time == 0.1
 
 
-async def test_from_motors(sim_motors: tuple[PmacIO, Motor, Motor]):
-    pmac, sim_x_motor, sim_y_motor = sim_motors
-    motor_info = await _PmacMotorInfo.from_motors(pmac, [sim_x_motor, sim_y_motor])
+async def test_motor_info_from_cs_motors(sim_cs_motors: tuple[PmacIO, Motor, Motor]):
+    pmac, sim_cs_x_motor, sim_cs_y_motor = sim_cs_motors
+    motor_info = await _PmacMotorInfo.from_motors(
+        pmac, [sim_cs_x_motor, sim_cs_y_motor]
+    )
     expected_motor_info = _PmacMotorInfo(
-        "CS1",
-        1,
-        {sim_x_motor: 6, sim_y_motor: 7},
-        {sim_x_motor: 10.0, sim_y_motor: 20.0},
-        {sim_x_motor: 5.0, sim_y_motor: 10.0},
+        "CS5",
+        5,
+        {sim_cs_x_motor: 6, sim_cs_y_motor: 7},
+        {sim_cs_x_motor: 10.0, sim_cs_y_motor: 20.0},
+        {sim_cs_x_motor: 5.0, sim_cs_y_motor: 10.0},
     )
     assert motor_info == expected_motor_info
+
+
+async def test_motor_info_from_both_motors(
+    sim_both_motors: tuple[PmacIO, Motor, Motor, Motor, Motor],
+):
+    sim_pmac, sim_x_motor, sim_y_motor, sim_cs_x_motor, sim_cs_y_motor = sim_both_motors
+
+    with pytest.raises(ValueError, match="raw motors and CS motors"):
+        await _PmacMotorInfo.from_motors(sim_pmac, [sim_x_motor, sim_cs_y_motor])
 
 
 async def test_motor_info_from_motors(sim_motors: tuple[PmacIO, Motor, Motor]):
