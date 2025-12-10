451f853210819289db90d31c833d0aab374b9d7e
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-12-10 12:12:42 +0000

    chore: reimplement OdinHDFIO for Jungfrau

--- src/ophyd_async/fastcs/jungfrau/_jungfrau.py
+++ src/ophyd_async/fastcs/jungfrau/_jungfrau.py
@@ -1,10 +1,20 @@
-from ophyd_async.core import (
-    PathProvider,
-    StandardDetector,
-)
+from ophyd_async.core import Device, PathProvider, StandardDetector
+from ophyd_async.fastcs.core import fastcs_connector
 from ophyd_async.fastcs.jungfrau._controller import JungfrauController
 from ophyd_async.fastcs.jungfrau._signals import JungfrauDriverIO
-from ophyd_async.fastcs.odin import OdinHdfIO, OdinWriter
+from ophyd_async.fastcs.odin import OdinWriter
+from ophyd_async.fastcs.odin._odin_io import FrameProcessorIO, MetaWriterIO
+
+
+# TODO: Delete this duplicate device, once FastCS Jungfrau
+# has top level 'detector' and 'odin', after which, follow
+# EigerDetector as an example of correct structure
+class OdinHdfIO(Device):
+    fp: FrameProcessorIO
+    mw: MetaWriterIO
+
+    def __init__(self, uri: str, name: str = ""):
+        super().__init__(name=name, connector=fastcs_connector(self, uri))
 
 
 class Jungfrau(StandardDetector[JungfrauController, OdinWriter]):
@@ -22,7 +32,7 @@ class Jungfrau(StandardDetector[JungfrauController, OdinWriter]):
         self.odin = OdinHdfIO(prefix + hdf_suffix)
         writer = OdinWriter(
             path_provider,
-            self.odin,
+            self.odin,  # type: ignore
             self.drv.bit_depth,
         )
         controller = JungfrauController(self.drv)
