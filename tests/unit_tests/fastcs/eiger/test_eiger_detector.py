010a7b01287f9652f0c89c0fb8ff8ffdf1ec8ea7
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-12-11 09:09:16 +0000

    tests: amend odin reference in eiger detector tests

--- tests/unit_tests/fastcs/eiger/test_eiger_detector.py
+++ tests/unit_tests/fastcs/eiger/test_eiger_detector.py
@@ -19,21 +19,21 @@ def detector(RE):
         detector = EigerDetector("BL03I", MagicMock())
 
     def set_meta_filename_and_id(value, *args, **kwargs):
-        set_mock_value(detector.odin.mw.file_prefix, value)
-        set_mock_value(detector.odin.mw.acquisition_id, value)
+        set_mock_value(detector.od.mw.file_prefix, value)
+        set_mock_value(detector.od.mw.acquisition_id, value)
 
-    callback_on_mock_put(detector.odin.fp.file_prefix, set_meta_filename_and_id)
+    callback_on_mock_put(detector.od.fp.file_prefix, set_meta_filename_and_id)
 
     detector._writer._path_provider.return_value.filename = "filename.h5"  # type: ignore
 
-    set_mock_value(detector.odin.fp.writing, True)
+    set_mock_value(detector.od.fp.writing, True)
     return detector
 
 
 async def test_when_prepared_eiger_bit_depth_is_passed_and_set_in_odin(detector):
     detector._controller.arm = AsyncMock()
     expected_datatype = 16
-    set_mock_value(detector.drv.detector.bit_depth_image, expected_datatype)
+    set_mock_value(detector.detector.bit_depth_image, expected_datatype)
 
     await detector.prepare(
         TriggerInfo(
@@ -44,6 +44,6 @@ async def test_when_prepared_eiger_bit_depth_is_passed_and_set_in_odin(detector)
     )
 
     # Assert that odin datatype is set to the eiger bit depth during detector prepare
-    get_mock_put(detector.odin.fp.data_datatype).assert_called_once_with(
+    get_mock_put(detector.od.fp.data_datatype).assert_called_once_with(
         f"uint{expected_datatype}", wait=True
     )
