77063c3af7aa25b02958f3d8bb372623c6808770
Alan Greer <alan.greer@diamond.ac.uk>
2026-01-27 15:55:36 +0000

    Added a script that allows a Tango Device Server to be spawned in a subprocess.

--- src/ophyd_async/tango/testing/__init__.py
+++ src/ophyd_async/tango/testing/__init__.py
@@ -2,6 +2,7 @@ from ._one_of_everything import (
     ExampleStrEnum,
     OneOfEverythingTangoDevice,
 )
+from ._device_server import TangoSubprocessDeviceServer
 from ._test_config import TestConfig
 
-__all__ = ["ExampleStrEnum", "OneOfEverythingTangoDevice", "TestConfig"]
+__all__ = ["ExampleStrEnum", "OneOfEverythingTangoDevice", "TangoSubprocessDeviceServer", "TestConfig"]
