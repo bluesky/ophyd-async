a531257ec86e60d926417b8a01ad833c69424244
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-02-27 15:46:22 +0000

    Test to still if fixes test

--- src/ophyd_async/core/_movable.py
+++ src/ophyd_async/core/_movable.py
@@ -63,7 +63,7 @@ class MovableLogic(Generic[SignalDatatypeT]):
         async with move_status:
             async for current_position in observe_value(
                 self.readback,
-                done_status=move_status,
+                # done_status=move_status,
             ):
                 yield WatcherUpdate[SignalDatatypeT](
                     current=current_position,
