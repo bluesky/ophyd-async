2605b8c5573e11a33eab635f1931ad8aaabd24f4
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-04-16 13:02:09 +0000

    Adding custom _data_logic

--- src/ophyd_async/fastcs/xspress/_detector.py
+++ src/ophyd_async/fastcs/xspress/_detector.py
@@ -2,6 +2,7 @@ from ophyd_async.core import PathProvider, StandardDetector
 from ophyd_async.fastcs.core import fastcs_connector
 
 from ._arm_logic import XspressArmLogic
+from ._data_logic import XspressOdinDataLogic
 from ._io import XspressDetectorIO
 from ._trigger_logic import XspressTriggerLogic
 from ._xsp_odin_io import XspressOdinIO
@@ -19,20 +20,14 @@ class XspressDetector(StandardDetector):
         path_provider: PathProvider,
         name="",
     ):
-        # Need to do this first so the type hints are filled in
-        # drv_connector = fastcs_connector(prefix + "Xspress:")
-        # self.xspress = XspressDetectorIO(connector=drv_connector)
-        # drv_connector.create_children_from_annotations(self.xspress)
-        # self.od = XspressOdinIO(connector=fastcs_connector(prefix + "OD:"))
         connector = fastcs_connector(prefix, self)
 
         self.add_detector_logics(
             XspressTriggerLogic(self.xspress),
             XspressArmLogic(self.xspress),
-            # OdinDataLogic(
-            #     path_provider=path_provider,
-            #     odin=self.od,
-            #     detector_bit_depth=None,
-            # ),
+            XspressOdinDataLogic(
+                path_provider=path_provider,
+                odin=self.od,
+            ),
         )
         super().__init__(name=name, connector=connector)
