e3b2debfc2fccaf7d6aaf2ed3c4cec3f7f7ba98f
sligara7 <sligara7@users.noreply.github.com>
2026-09-11 20:38:18 +0000

    deploy: 81e14afee27eda1dc0c25cd7e4635cb66800ead5

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.21.3.dev5+ge98262cdf',
+    VERSION: '0.21.3.dev6+g81e14afee',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
